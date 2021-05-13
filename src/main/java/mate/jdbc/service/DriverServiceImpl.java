172ad252fff1234c9028445eba42bf0c3747c413
vuyko23 <mykolasakulych@gmail.com>
2021-05-13 17:48:50 +0300

    aded Exception

--- src/main/java/mate/jdbc/service/DriverServiceImpl.java
+++ src/main/java/mate/jdbc/service/DriverServiceImpl.java
@@ -1,6 +1,7 @@
 package mate.jdbc.service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import mate.jdbc.dao.DriverDao;
 import mate.jdbc.lib.Inject;
 import mate.jdbc.lib.Service;
@@ -18,7 +19,7 @@ public class DriverServiceImpl implements DriverService {
 
     @Override
     public Driver get(Long id) {
-        return dao.get(id).orElseThrow(() -> new RuntimeException(
+        return dao.get(id).orElseThrow(() -> new NoSuchElementException(
                 "Cant find Driver by id " + id));
     }
 
--- src/main/java/mate/jdbc/service/ManufacturerServiceImpl.java
+++ src/main/java/mate/jdbc/service/ManufacturerServiceImpl.java
@@ -1,6 +1,7 @@
 package mate.jdbc.service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import mate.jdbc.dao.ManufacturerDao;
 import mate.jdbc.lib.Inject;
 import mate.jdbc.lib.Service;
@@ -18,7 +19,7 @@ public class ManufacturerServiceImpl implements ManufacturerService {
 
     @Override
     public Manufacturer get(Long id) {
-        return dao.get(id).orElseThrow(() -> new RuntimeException(
+        return dao.get(id).orElseThrow(() -> new NoSuchElementException(
                 "Can`t find Manufacturer by id " + id));
     }
 
