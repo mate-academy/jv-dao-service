bdfdae90aa2fafd2af7997e3ffeef08c5f559417
Nazar-yo <bilaknazar@gmail.com>
2021-05-14 16:53:38 +0300

    all done

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,48 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerService manufacturerService
+                = (ManufacturerService) injector.getInstance(ManufacturerService.class);
+        Manufacturer tesla = new Manufacturer("Tesla", "United States");
+        Manufacturer samsung = new Manufacturer("Samsung", "England");
+        manufacturerService.create(tesla);
+        manufacturerService.create(samsung);
+        System.out.println("Initial manufacturers DB: ");
+        manufacturerService.getAll().forEach(System.out::println);
+        samsung.setCountry("Switzerland");
+        manufacturerService.update(samsung);
+        System.out.println("Manufacturers DB after updating: ");
+        manufacturerService.getAll().forEach(System.out::println);
+        System.out.println("Manufacturer by index 1: ");
+        System.out.println(manufacturerService.get(1L));
+        manufacturerService.delete(1L);
+        System.out.println("Manufacturers DB after deleting: ");
+        manufacturerService.getAll().forEach(System.out::println);
+        DriverService driverService
+                = (DriverService) injector.getInstance(DriverService.class);
+        Driver bob = new Driver("Bob", "AD09093209");
+        Driver alice = new Driver("Alice", "LI88372912");
+        driverService.create(bob);
+        driverService.create(alice);
+        System.out.println("Initial drivers DB: ");
+        driverService.getAll().forEach(System.out::println);
+        alice.setLicenseNumber("PO872719972");
+        driverService.update(alice);
+        System.out.println("Drivers DB after updating: ");
+        driverService.getAll().forEach(System.out::println);
+        System.out.println("Driver by index 1: ");
+        System.out.println(driverService.get(1L));
+        driverService.delete(1L);
+        System.out.println("Drivers DB after deleting: ");
+        driverService.getAll().forEach(System.out::println);
     }
 }
---
+++ src/main/resources/init_db.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE `homework` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT CHAR SET ='N' */;
+CREATE TABLE `manufacturers` (
+                                 `name` varchar(250) NOT NULL,
+                                 `country` varchar(250) NOT NULL,
+                                 `id` bigint NOT NULL AUTO_INCREMENT,
+                                 `is_deleted` tinyint NOT NULL DEFAULT '0',
+                                 PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `drivers` (
+                           `id` bigint NOT NULL AUTO_INCREMENT,
+                           `name` varchar(45) NOT NULL,
+                           `license_number` varchar(45) NOT NULL,
+                           `deleted` tinyint NOT NULL DEFAULT '0',
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
