e6452d4acfbd76c794a65e6af16b9eb3ce376c2c
safordog <safordog@gmail.com>
2021-05-15 18:29:30 +0300

    renamed columns

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -2,15 +2,15 @@ CREATE DATABASE `manufacturer_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE
 
 CREATE TABLE `drivers` (
     `id` bigint NOT NULL AUTO_INCREMENT,
-    `deleted` tinyint NOT NULL DEFAULT '0',
+    `is_deleted` tinyint NOT NULL DEFAULT '0',
     `name` varchar(255) DEFAULT NULL,
-    `licenseNumber` varchar(255) DEFAULT NULL,
+    `license_number` varchar(255) DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `manufacturers` (
     `id` bigint NOT NULL AUTO_INCREMENT,
-    `deleted` tinyint NOT NULL DEFAULT '0',
+    `is_deleted` tinyint NOT NULL DEFAULT '0',
     `name` varchar(255) DEFAULT NULL,
     `country` varchar(255) DEFAULT NULL,
     PRIMARY KEY (`id`)
