91869e882094512af71701409fc26cc98af2e729
safordog <safordog@gmail.com>
2021-05-15 16:16:32 +0300

    changed names

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -10,18 +10,18 @@ public class Main {
     private static Injector injector = Injector.getInstance("mate.jdbc");
 
     public static void main(String[] args) {
-        Manufacturer sony = new Manufacturer("Sony", "Japan");
-        Manufacturer asus = new Manufacturer("Asus", "Taiwan");
+        Manufacturer seat = new Manufacturer("Seat", "Spain");
+        Manufacturer acura = new Manufacturer("Acura", "Japan");
         ManufacturerService manufacturerService =
                 (ManufacturerService) injector.getInstance(ManufacturerService.class);
 
-        System.out.println(manufacturerService.create(sony));
-        System.out.println(manufacturerService.create(asus));
-        System.out.println(manufacturerService.get(asus.getId()));
+        System.out.println(manufacturerService.create(seat));
+        System.out.println(manufacturerService.create(acura));
+        System.out.println(manufacturerService.get(acura.getId()));
         manufacturerService.getAll().forEach(System.out::println);
-        sony.setCountry("JPN");
-        System.out.println(manufacturerService.update(sony));
-        System.out.println(manufacturerService.delete(sony.getId()));
+        seat.setCountry("SPN");
+        System.out.println(manufacturerService.update(seat));
+        System.out.println(manufacturerService.delete(seat.getId()));
 
         Driver bob = new Driver("Bob", "AV322");
         Driver mark = new Driver("Mark", "RW015");
