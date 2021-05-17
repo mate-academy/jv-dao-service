7fd39bcaf6592001398040167a9065af7f993287
Veronika <koraf9712@gmail.com>
2021-05-17 12:30:47 +0300

    Added solution

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,48 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerService manufacturerService =
+                (ManufacturerService) injector.getInstance(ManufacturerService.class);
+
+        Manufacturer manufacturerTesla = new Manufacturer("Tesla", "USA");
+        manufacturerService.create(manufacturerTesla);
+        Manufacturer manufacturerKia = new Manufacturer("Kia", "Korea");
+        manufacturerService.create(manufacturerKia);
+        Manufacturer manufacturerDS = new Manufacturer("Citroen", "France");
+        manufacturerService.create(manufacturerDS);
+        manufacturerService.getAll().forEach(System.out::println);
+
+        System.out.println(manufacturerService.get(manufacturerTesla.getId()));
+        manufacturerDS.setName("DS");
+        manufacturerService.update(manufacturerDS);
+        manufacturerService.delete(manufacturerKia.getId());
+        manufacturerService.getAll().forEach(System.out::println);
+
+        DriverService driverService =
+                (DriverService) injector.getInstance(DriverService.class);
+
+        Driver driverOscar = new Driver("Oscar", "1ldlvik3");
+        driverService.create(driverOscar);
+        Driver driverSanta = new Driver("Santa", "ldk4mc6p");
+        driverService.create(driverSanta);
+        Driver driverKevin = new Driver("Kevin", "tandkf86");
+        driverService.create(driverKevin);
+        driverService.getAll().forEach(System.out::println);
+
+        System.out.println(driverService.get(driverSanta.getId()));
+        driverOscar.setName("Oscarino");
+        driverService.update(driverOscar);
+        driverService.delete(driverKevin.getId());
+        driverService.getAll().forEach(System.out::println);
+
     }
 }
---
+++ src/main/resources/init_db.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE `manufacturers_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+CREATE TABLE `manufacturers` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(225) DEFAULT NULL,
+  `country` varchar(225) DEFAULT NULL,
+  `is_deleted` tinyint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `drivers` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(225) DEFAULT NULL,
+  `license_number` varchar(225) DEFAULT NULL,
+  `is_deleted` tinyint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
