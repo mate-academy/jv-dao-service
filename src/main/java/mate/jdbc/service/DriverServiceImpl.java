b18e4007ffe9fe9ffb78007da3abd2d86b38fc90
Kostiantyn <konkos2505@gmail.com>
2021-05-17 14:39:55 +0300

    fix method get and add message to orElseThrow

--- src/main/java/mate/jdbc/service/DriverServiceImpl.java
+++ src/main/java/mate/jdbc/service/DriverServiceImpl.java
@@ -18,7 +18,8 @@ public class DriverServiceImpl implements DriverService {
 
     @Override
     public Driver get(Long id) {
-        return driverDao.get(id).get();
+        return driverDao.get(id).orElseThrow(() -> new RuntimeException("Couldn't get "
+                + "driver by id = " + id));
     }
 
     @Override
