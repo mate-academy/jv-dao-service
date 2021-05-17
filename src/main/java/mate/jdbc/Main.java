9713f021cab73a015f510d9ac79b611abbc11605
anufriiev <dimaanufriev1012@gmail.com>
2021-05-17 11:07:53 +0300

    Fixes

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -12,17 +12,18 @@ public class Main {
     public static void main(String[] args) {
         ManufacturerService manufacturerService
                 = (ManufacturerService) injector.getInstance(ManufacturerService.class);
-        Manufacturer mercedes = new Manufacturer("Mercedes", "Germany");
         System.out.println(manufacturerService.get(5L));
         DriverService driverService
                 = (DriverService) injector.getInstance(DriverService.class);
-        Driver driver = new Driver("Ivan", "4365345");
+
+        Driver driver = new Driver("George", "4365345");
         System.out.println(driverService.create(driver));
         System.out.println(driverService.get(2L));
         System.out.println(driverService.getAll());
-        Driver driverVasyl = new Driver("Vasyl", "3542424");
-        driverVasyl.setId(11L);
-        System.out.println(driverService.update(driverVasyl));
+
+        Driver driverJohn = new Driver("John", "3542424");
+        driverJohn.setId(11L);
+        System.out.println(driverService.update(driverJohn));
         System.out.println(driverService.delete(15L));
     }
 }
