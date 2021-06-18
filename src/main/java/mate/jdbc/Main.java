719f3cfbf90c3a3104a5ebeaeff102cdf92a402c
mbelm <mbelmasg@gmail.com>
2021-06-18 21:10:59 +0300

    still working

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,20 @@
 package mate.jdbc;
 
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerService manufacturerService =
+                (ManufacturerService) injector.getInstance(ManufacturerService.class);
+        DriverService driverService =
+                (DriverService) injector.getInstance(DriverService.class);
+        System.out.println(manufacturerService.getAll());
+        System.out.println(driverService.create(new Driver("eblan", "VVB 113456")));
     }
 }
