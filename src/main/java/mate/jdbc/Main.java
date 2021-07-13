a5ccba4f3a1a846f6232969492e50fa0c1a767c2
Tamara-Paskalova <forptv@gmail.com>
2021-07-13 16:30:36 +0300

    created classes Driver, DriverDao, DriverDaoImpl, DriverService, DriverServiceImpl, implemented all requested methods

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,25 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriverService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        Driver billDriver = new Driver(null, "Walter", "36522114789");
+        System.out.println("*********Create Driver***********");
+        driverService.create(billDriver);
+        driverService.getAll().forEach(System.out::println);
+        System.out.println("*********Delete Driver***********");
+        driverService.delete(4L);
+        driverService.getAll().forEach(System.out::println);
+        Driver driver = driverService.get(6L);
+        driver.setName("Alice");
+        System.out.println("*********Update Driver***********");
+        driverService.update(driver);
+        driverService.getAll().forEach(System.out::println);
     }
 }
