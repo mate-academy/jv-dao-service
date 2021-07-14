17d617e7847aa9294f3f43f25f60f6ceefff7f61
PashaTerletskyi <p.terletsky@gmail.com>
2021-07-14 14:04:13 +0300

    fixed checkstyle

--- src/main/java/mate/jdbc/dao/impl/DriverDaoImpl.java
+++ src/main/java/mate/jdbc/dao/impl/DriverDaoImpl.java
@@ -51,7 +51,7 @@ public class DriverDaoImpl implements DriverDao {
             }
             return Optional.ofNullable(driver);
         } catch (SQLException throwable) {
-            throw new DataProcessingException("Couldn't get driver by id " + id + " ",
+            throw new DataProcessingException("Couldn't get driver by id " + id,
                     throwable);
         }
     }
@@ -89,7 +89,7 @@ public class DriverDaoImpl implements DriverDao {
             return driver;
         } catch (SQLException throwable) {
             throw new DataProcessingException("Couldn't update a driver "
-                    + driver + " ", throwable);
+                    + driver, throwable);
         }
     }
 
@@ -102,7 +102,7 @@ public class DriverDaoImpl implements DriverDao {
             deleteDriverStatement.setLong(1, id);
             return deleteDriverStatement.executeUpdate() > 0;
         } catch (SQLException throwable) {
-            throw new DataProcessingException("Couldn't delete a driver by id " + id + " ",
+            throw new DataProcessingException("Couldn't delete a driver by id " + id,
                     throwable);
         }
     }
