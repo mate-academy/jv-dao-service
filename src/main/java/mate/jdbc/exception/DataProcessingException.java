7a4b5e822261e3a187dda6809e95c98c7a83bcb9
docer <vlasovengineer@gmail.com>
2021-07-14 12:33:14 +0300

    fix problem with exception

--- src/main/java/mate/jdbc/exception/DataProcessingException.java
+++
@@ -1,7 +0,0 @@
-package mate.jdbc.exception;
-
-public class DataProcessingException extends RuntimeException {
-    public DataProcessingException(String massage, Throwable exception) {
-        super(massage, exception);
-    }
-}
