57075c8cdbbd1cefd430fced97cd12fe2985b465
EugeneSinica <eugenesinica@gmail.com>
2021-07-14 14:16:35 +0300

    removed some empty lines

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -13,23 +13,18 @@ public class Main {
                 (DriverService) injector.getInstance(DriverService.class);
         Driver arsen = new Driver("Arsen", "2281337");
         driverService.create(arsen);
-
         Driver sergey = new Driver("Sergey", "4347332");
         driverService.create(sergey);
-
         Driver andrew = new Driver("Andrew", "7777777");
         driverService.create(andrew);
-
         Driver muhamed = new Driver("Muhamed", "0101010");
         driverService.create(muhamed);
 
         Optional<Driver> fourthRow = Optional.ofNullable(driverService.get(4L));
         System.out.println(fourthRow);
-
         System.out.println();
 
         driverService.getAll().forEach(System.out::println);
-
         System.out.println();
 
         Driver muhamedIii = new Driver("Muhamed II", "1010101");
@@ -43,10 +38,8 @@ public class Main {
         System.out.println(muhamedIii.getId());
         System.out.println(muhamedIii.getName());
         System.out.println(muhamedIii.getLicenseNumber());
-
         System.out.println();
 
         System.out.println(driverService.delete(muhamedIii.getId()));
-
     }
 }
