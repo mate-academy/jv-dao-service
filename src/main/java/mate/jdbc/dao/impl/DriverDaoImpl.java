8cf68bae8f78d872772f3a74878f243bd498be92
Vova Bilodon <vovabilodon@gmail.com>
2021-07-14 16:24:24 +0300

    changed exception message, renamed id variable

--- src/main/java/mate/jdbc/dao/impl/DriverDaoImpl.java
+++ src/main/java/mate/jdbc/dao/impl/DriverDaoImpl.java
@@ -32,7 +32,7 @@ public class DriverDaoImpl implements DriverDao {
             }
             return driver;
         } catch (SQLException throwable) {
-            throw new DataProcessingException("Couldn't create driver. " + driver + " ",
+            throw new DataProcessingException("Couldn't create driver. " + driver,
                     throwable);
         }
     }
@@ -51,7 +51,7 @@ public class DriverDaoImpl implements DriverDao {
             }
             return Optional.ofNullable(driver);
         } catch (SQLException throwable) {
-            throw new DataProcessingException("Couldn't get driver by id " + id + " ",
+            throw new DataProcessingException("Couldn't get driver by id " + id,
                     throwable);
         }
     }
@@ -89,7 +89,7 @@ public class DriverDaoImpl implements DriverDao {
             return driver;
         } catch (SQLException throwable) {
             throw new DataProcessingException("Couldn't update a driver "
-                    + driver + " ", throwable);
+                    + driver, throwable);
         }
     }
 
@@ -102,18 +102,18 @@ public class DriverDaoImpl implements DriverDao {
             deleteDriverStatement.setLong(1, id);
             return deleteDriverStatement.executeUpdate() > 0;
         } catch (SQLException throwable) {
-            throw new DataProcessingException("Couldn't delete a driver by id " + id + " ",
+            throw new DataProcessingException("Couldn't delete a driver by id " + id,
                     throwable);
         }
     }
 
     private Driver getDriver(ResultSet resultSet) {
         try {
-            Long newId = resultSet.getObject("id", Long.class);
+            Long id = resultSet.getObject("id", Long.class);
             String name = resultSet.getString("name");
             String licenseNumber = resultSet.getString("license_number");
             Driver driver = new Driver(name, licenseNumber);
-            driver.setId(newId);
+            driver.setId(id);
             return driver;
         } catch (SQLException throwable) {
             throw new DataProcessingException("Cant get Driver from ResultSet "
