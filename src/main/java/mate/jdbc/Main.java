08519546cef63c32a3a0b88966c3c39cf6a94556
Sinelife <ya.serzhan@gmail.com>
2021-09-13 22:52:50 +0300

    add realization of Dao and services for manufacturer and driver classes

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,67 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+import mate.jdbc.service.impl.DriverServiceImpl;
+
 public class Main {
+    private static final String PACKAGE_NAME = "mate.jdbc";
+    private static Injector injector = Injector.getInstance(PACKAGE_NAME);
+
     public static void main(String[] args) {
-        // test your code here
+        driverTest();
+//        manufacturerTest();
+    }
+
+    private static void driverTest() {
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        System.out.println("\t\tTESTS FOR DRIVERS");
+        System.out.println("\nTest for getAll()");
+        System.out.println("DriverList: " + driverService.getAll());
+
+        System.out.println("\nTest for create");
+        Driver andrey = new Driver("Andrey", "1234567");
+        System.out.println(driverService.create(andrey));
+
+        System.out.println("\nTest for update");
+        Long id = 1L;
+        Driver alexander = new Driver("Alexander", "4534222");
+        alexander.setId(id);
+        System.out.println(driverService.update(alexander));
+
+        System.out.println("\nTest for get");
+        System.out.println(driverService.get(id));
+
+        System.out.println("\nTest for delete");
+        id = 2L;
+        System.out.println(driverService.delete(id));
+        System.out.println(driverService.get(id));
+    }
+
+    private static void manufacturerTest() {
+        ManufacturerService manufacturerService = (ManufacturerService) injector.getInstance(ManufacturerService.class);
+        System.out.println("\t\tTESTS FOR MANUFACTURER");
+        System.out.println("Test for getAll()");
+        System.out.println("DriverList: " + manufacturerService.getAll());
+
+        System.out.println("\nTest for create");
+        Manufacturer company1 = new Manufacturer("Company1", "USA");
+        System.out.println(manufacturerService.create(company1));
+
+        System.out.println("\nTest for update");
+        Long id = 4L;
+        Manufacturer company111 = new Manufacturer("Company111", "USA");
+        company111.setId(id);
+        System.out.println(manufacturerService.update(company111));
+
+        System.out.println("\nTest for get");
+        System.out.println(manufacturerService.get(id));
+
+        System.out.println("\nTest for delete");
+        id = 6L;
+        System.out.println(manufacturerService.delete(id));
     }
 }
---
+++ src/main/java/mate/jdbc/dao/impl/DriverDaoImpl.java
@@ -0,0 +1,129 @@
+package mate.jdbc.dao.impl;
+
+import mate.jdbc.dao.DriverDao;
+import mate.jdbc.lib.Dao;
+import mate.jdbc.lib.Inject;
+import mate.jdbc.model.Driver;
+import mate.jdbc.util.ConnectionUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Dao
+public class DriverDaoImpl implements DriverDao {
+    private static final String INSERT_DRIVER_REQUEST
+            = "INSERT INTO drivers(name, licence_number) values(?,?);";
+    private static final String GET_ALL_DRIVERS_REQUEST
+            = "SELECT * FROM drivers WHERE is_deleted = false";
+    private static final String UPDATE_DRIVER_REQUEST
+            = "UPDATE drivers SET name=?,licence_number=? WHERE id=?";
+    private static final String DELETE_DRIVER_REQUEST
+            = "UPDATE drivers SET is_deleted = true WHERE id = ?";
+    private static final String GET_DRIVER_REQUEST
+            = "SELECT * FROM drivers WHERE is_deleted = false AND id = ?";
+    private static final String CANT_GET_ALL_MESSAGE = "Can't get all drivers from DB.";
+    private static final String CANT_CREATE_MESSAGE = "Can't insert driver to DB.";
+    private static final String CANT_UPDATE_MESSAGE = "Can't update driver in DB.";
+    private static final String CANT_DELETE_MESSAGE = "Can't delete driver from DB.";
+    private static final String CANT_GET_MESSAGE = "Can't get driver by id from DB.";
+    private static final String NAME = "name";
+    private static final String LICENCE_NUMBER = "licence_number";
+    private static final String ID = "id";
+
+    @Override
+    public List<Driver> getAll() {
+        List<Driver> driverList = new ArrayList<>();
+        try (Connection connection = ConnectionUtil.getConnection();
+             Statement getAllDriversStatement = connection.createStatement();) {
+            ResultSet resultSet = getAllDriversStatement
+                    .executeQuery(GET_ALL_DRIVERS_REQUEST);
+            while (resultSet.next()) {
+                String name = resultSet.getString(NAME);
+                String licenceNumber = resultSet.getString(LICENCE_NUMBER);
+                Long id = resultSet.getObject(ID, Long.class);
+                Driver driver = new Driver();
+                driver.setId(id);
+                driver.setName(name);
+                driver.setLicenseNumber(licenceNumber);
+                driverList.add(driver);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(CANT_GET_ALL_MESSAGE, e);
+        }
+        return driverList;
+    }
+
+    @Override
+    public Driver create(Driver driver) {
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement createDriverStatement =
+                     connection.prepareStatement(INSERT_DRIVER_REQUEST,
+                             Statement.RETURN_GENERATED_KEYS);) {
+            createDriverStatement.setString(1, driver.getName());
+            createDriverStatement.setString(2, driver.getLicenseNumber());
+            createDriverStatement.executeUpdate();
+            ResultSet generatedKeys = createDriverStatement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                Long id = generatedKeys.getObject(1, Long.class);
+                driver.setId(id);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(CANT_CREATE_MESSAGE, e);
+        }
+        return driver;
+    }
+
+    @Override
+    public Driver update(Driver driver) {
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement updateDriverStatement =
+                     connection.prepareStatement(UPDATE_DRIVER_REQUEST,
+                             Statement.RETURN_GENERATED_KEYS);) {
+            updateDriverStatement.setString(1, driver.getName());
+            updateDriverStatement.setString(2, driver.getLicenseNumber());
+            updateDriverStatement.setObject(3, driver.getId());
+            updateDriverStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(CANT_UPDATE_MESSAGE, e);
+        }
+        return driver;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement deleteDriverStatement =
+                     connection.prepareStatement(DELETE_DRIVER_REQUEST,
+                             Statement.RETURN_GENERATED_KEYS);) {
+            deleteDriverStatement.setLong(1, id);
+            return deleteDriverStatement.executeUpdate() >= 1;
+        } catch (SQLException e) {
+            throw new RuntimeException(CANT_DELETE_MESSAGE, e);
+        }
+    }
+
+    @Override
+    public Optional<Driver> get(Long id) {
+        Driver driver = null;
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement getDriverStatement =
+                     connection.prepareStatement(GET_DRIVER_REQUEST,
+                             Statement.RETURN_GENERATED_KEYS);) {
+            getDriverStatement.setLong(1, id);
+            ResultSet resultSet = getDriverStatement.executeQuery();
+            while (resultSet.next()) {
+                driver = new Driver();
+                String name = resultSet.getString(NAME);
+                String licenceNumber = resultSet.getString(LICENCE_NUMBER);
+                driver.setId(id);
+                driver.setName(name);
+                driver.setLicenseNumber(licenceNumber);
+            }
+            return Optional.ofNullable(driver);
+        } catch (SQLException e) {
+            throw new RuntimeException(CANT_GET_MESSAGE, e);
+        }
+    }
+}
