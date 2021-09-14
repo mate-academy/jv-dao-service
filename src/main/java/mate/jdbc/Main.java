7f0857f0ba0e2a489bf1b9cc8279a532b33e134d
Oleksandr Rozvazhaiev <alexrozv@gmail.com>
2021-09-14 14:29:02 +0300

    implemented service for Manufacturer, created Driver model, implemented dao and service for Driver, added table config to init_db.sql, tested CRUD in main

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,41 @@
 package mate.jdbc;
 
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerService manufacturerService = (ManufacturerService) injector
+                .getInstance(ManufacturerService.class);
+        DriverService driverService = (DriverService) injector
+                .getInstance(DriverService.class);
+        Manufacturer manufacturer = new Manufacturer("Honda", "Japan");
+        Driver driver = new Driver("Bob", "12345");
+        manufacturerService.create(manufacturer);
+        driverService.create(driver);
+        List<Manufacturer> manufacturers = manufacturerService.getAll();
+        manufacturers.forEach(System.out::println);
+        List<Driver> drivers = driverService.getAll();
+        drivers.forEach(System.out::println);
+        Manufacturer updatedManufacturer = new Manufacturer("Toyota", "Japan");
+        updatedManufacturer.setId(1L);
+        manufacturerService.update(updatedManufacturer);
+        Driver updatedDriver = new Driver("Alice", "54321");
+        updatedDriver.setId(1L);
+        driverService.update(updatedDriver);
+        System.out.println(manufacturerService.get(1L));
+        System.out.println(driverService.get(1L));
+        manufacturerService.delete(1L);
+        driverService.delete(1L);
+        manufacturers = manufacturerService.getAll();
+        manufacturers.forEach(System.out::println);
+        drivers = driverService.getAll();
+        drivers.forEach(System.out::println);
     }
 }
