585c46a0bc362c4dae9312bae409d387e48e9d82
Sinelife <ya.serzhan@gmail.com>
2021-09-15 15:12:14 +0300

    rewrited tests for dao methods in Main.class

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -18,50 +18,71 @@ public class Main {
     private static void driverTest() {
         DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
         System.out.println("\t\tTESTS FOR DRIVERS");
+
+        System.out.println("\nTest for create");
+        Driver andrey = driverService.create(new Driver("Andrey", "1234567"));
+        Driver anna = driverService.create(new Driver("Anna", "6666666"));
+        Driver lena = driverService.create(new Driver("Lena", "121212121"));
+        Driver anton = driverService.create(new Driver("Anton", "111"));
+        System.out.println(andrey);
+        System.out.println(anna);
+        System.out.println(lena);
+        System.out.println(anton);
+
         System.out.println("\nTest for getAll()");
         System.out.println("DriverList: " + driverService.getAll());
 
-        System.out.println("\nTest for create");
-        Driver andrey = new Driver("Andrey", "1234567");
-        System.out.println(driverService.create(andrey));
-
         System.out.println("\nTest for update");
-        Long id = 1L;
-        Driver alexander = new Driver("Alexander", "4534222");
-        alexander.setId(id);
-        System.out.println(driverService.update(alexander));
+        System.out.println("Before update: " + anna);
+        anna.setLicenseNumber("666");
+        System.out.println(driverService.update(anna));
+        System.out.println("After update: " + anna);
 
         System.out.println("\nTest for get");
-        System.out.println(driverService.get(id));
+        System.out.println(driverService.get(andrey.getId()));
+        System.out.println(driverService.get(lena.getId()));
 
         System.out.println("\nTest for delete");
-        id = 2L;
-        System.out.println(driverService.delete(id));
-        System.out.println(driverService.get(id));
+        System.out.println("deleting " + anton);
+        driverService.delete(anton.getId());
+        System.out.println("DriverList: " + driverService.getAll());
     }
 
     private static void manufacturerTest() {
         ManufacturerService manufacturerService =
                 (ManufacturerService) injector.getInstance(ManufacturerService.class);
-        System.out.println("\t\tTESTS FOR MANUFACTURER");
-        System.out.println("Test for getAll()");
-        System.out.println("DriverList: " + manufacturerService.getAll());
+        System.out.println("\t\tTESTS FOR MANUFACTURERS");
 
         System.out.println("\nTest for create");
-        Manufacturer company1 = new Manufacturer("Company1", "USA");
-        System.out.println(manufacturerService.create(company1));
+        Manufacturer bentley
+                = manufacturerService.create(new Manufacturer("Bentley", "Great Britain"));
+        Manufacturer ferrari
+                = manufacturerService.create(new Manufacturer("Ferrari", "Italy"));
+        Manufacturer mercedes
+                = manufacturerService.create(new Manufacturer("Mercedes", "Germany"));
+        Manufacturer hyundai
+                = manufacturerService.create(new Manufacturer("Hyundai", "South Korea"));
+        System.out.println(bentley);
+        System.out.println(ferrari);
+        System.out.println(mercedes);
+        System.out.println(hyundai);
+
+        System.out.println("\nTest for getAll()");
+        System.out.println("Manufacturer List: " + manufacturerService.getAll());
 
         System.out.println("\nTest for update");
-        Long id = 4L;
-        Manufacturer company111 = new Manufacturer("Company111", "USA");
-        company111.setId(id);
-        System.out.println(manufacturerService.update(company111));
+        System.out.println("Before update: " + hyundai);
+        hyundai.setName("Hyundai Motor Company");
+        System.out.println(manufacturerService.update(hyundai));
+        System.out.println("After update: " + hyundai);
 
         System.out.println("\nTest for get");
-        System.out.println(manufacturerService.get(id));
+        System.out.println(manufacturerService.get(ferrari.getId()));
+        System.out.println(manufacturerService.get(bentley.getId()));
 
         System.out.println("\nTest for delete");
-        id = 6L;
-        System.out.println(manufacturerService.delete(id));
+        System.out.println("deleting " + ferrari);
+        manufacturerService.delete(ferrari.getId());
+        System.out.println("Manufacturer List: " + manufacturerService.getAll());
     }
 }
