9d19615924cef6234f4e399930cc0a4a01b1ca6c
Viktor Yukhno <pingvinomania@gmail.com>
2021-09-18 12:59:11 +0300

    added Driver model, created Driver DAO and implemented Manufacturer and Driver services

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,39 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        Manufacturer manufacturer = new Manufacturer("Ford", "USA");
+        Manufacturer manufacturer1 = new Manufacturer("Volvo","Sweden");
+        ManufacturerService manufacturerService = (ManufacturerService) injector
+                .getInstance(ManufacturerService.class);
+        manufacturer = manufacturerService.create(manufacturer);
+        manufacturer1 = manufacturerService.create(manufacturer1);
+        System.out.println(manufacturerService.get(manufacturer.getId()));
+        manufacturer1.setName("Tesla");
+        manufacturer1.setCountry("USA");
+        manufacturerService.update(manufacturer1);
+        System.out.println(manufacturerService.get(manufacturer1.getId()));
+        manufacturerService.delete(manufacturer1.getId());
+        manufacturerService.getAll().forEach(System.out::println);
+
+        Driver driver = new Driver("Viktor", "01234");
+        Driver driver1 = new Driver("Kyrylo", "56789");
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        driver = driverService.create(driver);
+        driver1 = driverService.create(driver1);
+        System.out.println(driverService.get(driver.getId()));
+        driver.setLicenseNumber("43210");
+        driverService.update(driver);
+        System.out.println(driverService.get(driver.getId()));
+        driverService.delete(driver.getId());
+        driverService.getAll().forEach(System.out::println);
     }
 }
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,9 +1,16 @@
-CREATE SCHEMA IF NOT EXISTS `taxi` DEFAULT CHARACTER SET utf8;
-USE `taxi`;
-
+CREATE DATABASE `taxi` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE TABLE `drivers` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `licenseNumber` varchar(255) COLLATE utf8_bin NOT NULL,
+  `is_deleted` tinyint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
 CREATE TABLE `manufacturers` (
-                                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                        `name` VARCHAR(225) NOT NULL,
-                                        `country` VARCHAR(225) NOT NULL,
-                                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                        PRIMARY KEY (`id`));
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `country` varchar(255) COLLATE utf8_bin NOT NULL,
+  `is_deleted` tinyint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
+
