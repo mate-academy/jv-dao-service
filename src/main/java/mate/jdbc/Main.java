c12be3636c58646e0ace6fe4740bebd1c8978df2
Roman Kozhevnikow <roman.kozhevnikow@gmail.com>
2021-09-30 18:00:20 +0300

    added solution

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,35 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriverService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
         // test your code here
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+
+        Driver driver = Driver.builder()
+                .name("Bob")
+                .licenseNumber("3856")
+                .build();
+
+        driver = driverService.create(driver);
+        driver = driverService.get(driver.getId()).orElse(new Driver());
+        driver.setName("Alice");
+        driver = driverService.update(driver);
+        driverService.delete(driver.getId());
+
+        Driver driverTom = Driver.builder()
+                .name("Tom")
+                .licenseNumber("3856")
+                .build();
+
+        driverService.create(driverTom);
+        driverService.create(driverTom);
+        driverService.getAll().forEach(System.out::println);
+
     }
 }
