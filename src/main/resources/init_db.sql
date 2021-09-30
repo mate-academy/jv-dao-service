83d7e8604e6ec4a008015f53bf5f1ca6453d3322
Yehor Zapara <egor.zapara1994@gmail.com>
2021-09-30 14:29:16 +0300

    whitespaces were deleted

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -13,6 +13,3 @@ CREATE TABLE `drivers` (
                            `license_number` VARCHAR(225) NOT NULL,
                            `is_deleted` TINYINT NOT NULL DEFAULT 0,
                            PRIMARY KEY (`id`));
-
-
-
