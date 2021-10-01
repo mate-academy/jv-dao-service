457f006473bd3dc33a62998f3d5f9c55c5affbc2
Evgen Krasnenkov <krasnenkov.evgen@gmail.com>
2021-10-01 12:36:58 +0300

    MR with dao inport with service interface changed schema

--- src/main/java/mate/jdbc/sevice/DriverService.java
+++ src/main/java/mate/jdbc/sevice/DriverService.java
@@ -3,7 +3,7 @@ package mate.jdbc.sevice;
 import java.util.List;
 import mate.jdbc.model.Driver;
 
-public interface DriverService extends Service<Driver> {
+public interface DriverService extends GenericService<Driver> {
     Driver create(Driver driver);
 
     Driver get(Long id);
