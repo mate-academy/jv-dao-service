4f8feb1f47c910a9132db22b6cc8b7fd204aaf37
Denys Stefanko <denys.stefanko@gmail.com>
2021-10-03 14:07:03 +0300

    Fixed main class

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,10 +1,10 @@
 package mate.jdbc;
 
-import mate.jdbc.dao.ManufacturerDao;
 import mate.jdbc.lib.Injector;
 import mate.jdbc.model.Driver;
 import mate.jdbc.model.Manufacturer;
 import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
 
 public class Main {
     public static final Injector injector = Injector.getInstance("mate.jdbc");
@@ -26,8 +26,8 @@ public class Main {
         driverService.delete(2L);
         driverService.getAll().forEach(System.out::println);
 
-        ManufacturerDao manufacturerService =
-                (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+        ManufacturerService manufacturerService =
+                (ManufacturerService) injector.getInstance(ManufacturerService.class);
         Manufacturer porscheManufacturer = new Manufacturer("Porsche", "Germany");
         Manufacturer mustangManufacturer = new Manufacturer("Mustang", "USA");
         manufacturerService.create(porscheManufacturer);
