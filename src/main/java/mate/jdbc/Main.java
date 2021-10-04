b0507e10d9f28fea1896b1b1c4438c245c1bcae7
Sasha Hordenin <sg743148@gmail.com>
2021-10-04 12:43:44 +0300

    realized jv-dao-service

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,37 @@
 package mate.jdbc;
 
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+    private static final long TEST_ID = 3L;
+    private static final Manufacturer CREATE_METHOD_MANUFACTURER
+            = new Manufacturer("Opel", "Germany");
+    private static final Manufacturer UPDATE_METHOD_MANUFACTURER
+            = new Manufacturer(2L, "Hyundai", "South Korea");
+    private static final Driver CREATE_METHOD_DRIVER
+            = new Driver("John", "45345345");
+    private static final Driver UPDATE_METHOD_DRIVER
+            = new Driver(2L, "Alice", "34343434");
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerService manufacturerService = (ManufacturerService) injector
+                .getInstance(ManufacturerService.class);
+        Manufacturer createdManufacturer = manufacturerService.create(CREATE_METHOD_MANUFACTURER);
+        Manufacturer manufacturerById = manufacturerService.get(TEST_ID);
+        List<Manufacturer> allManufacturersList = manufacturerService.getAll();
+        Manufacturer updatedManufacturer = manufacturerService.update(UPDATE_METHOD_MANUFACTURER);
+        boolean isDeletedManufacturer = manufacturerService.delete(TEST_ID);
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        Driver createdDriver = driverService.create(CREATE_METHOD_DRIVER);
+        Driver driverById = driverService.get(TEST_ID);
+        List<Driver> allDriversList = driverService.getAll();
+        Driver updatedDriver = driverService.update(UPDATE_METHOD_DRIVER);
+        boolean isDeletedDriver = driverService.delete(TEST_ID);
     }
 }
