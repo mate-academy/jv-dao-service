2b46e64663f67f748a18bd822321296b0a07dfc5
Rostyslav Fostyk <rostyslavfostykjob@gmail.com>
2021-10-04 18:48:31 +0300

    implemented driverdao, driverservice, manufacturerservice

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,18 @@
 package mate.jdbc;
 
+import mate.jdbc.dao.DriverDao;
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerDao manufacturerDao = (ManufacturerDao) injector
+                .getInstance(ManufacturerDao.class);
+        DriverDao driverDao = (DriverDao) injector
+                .getInstance(DriverDao.class);
+        driverDao.getAll().forEach(System.out::println);
+        System.out.println(driverDao.get(1L));
     }
 }
