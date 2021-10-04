82b1752054d4d0a08d2d77690c85eafa789116a7
Stas Shepet <milka33349@gmail.com>
2021-10-05 01:40:14 +0300

    first commit

---
+++ src/main/java/mate/jdbc/dao/DriverDaoImpl.java
@@ -0,0 +1,33 @@
+package mate.jdbc.dao;
+
+import mate.jdbc.model.Driver;
+
+import java.util.List;
+import java.util.Optional;
+
+public class DriverDaoImpl implements DriverDao{
+    @Override
+    public Driver create(Driver driver) {
+        return null;
+    }
+
+    @Override
+    public Optional<Driver> get(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Driver> getAll() {
+        return null;
+    }
+
+    @Override
+    public Driver update(Driver driver) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+}
