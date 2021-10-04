8e416bbf5615d958d69d73f48c16a10c95d0b6f5
Yuliya Senchenko <usencenko70@gmail.com>
2021-10-04 20:16:32 +0300

    Created dao-service project

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -7,3 +7,10 @@ CREATE TABLE `manufacturers` (
                                         `country` VARCHAR(225) NOT NULL,
                                         `is_deleted` TINYINT NOT NULL DEFAULT 0,
                                         PRIMARY KEY (`id`));
+CREATE TABLE `drivers` (
+                                     `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                                     `name` VARCHAR(255) NOT NULL,
+                                     `license_number` VARCHAR(255) NOT NULL,
+                                     `is_deleted` TINYINT NOT NULL DEFAULT 0,
+                                     PRIMARY KEY (`id`));
+
