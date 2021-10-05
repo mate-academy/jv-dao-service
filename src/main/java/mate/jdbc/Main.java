8dde902d1d096c3c1184fe5f30a12896b842dbe8
Roman Dandali <Dandali2018@gmail.com>
2021-10-05 11:40:04 +0300

    Created class Driver and implementations

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,54 @@
 package mate.jdbc;
 
+import java.util.List;
+import java.util.Optional;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        DriverService driverService = (DriverService)
+                injector.getInstance(DriverService.class);
+        ManufacturerService manufacturerService = (ManufacturerService)
+                injector.getInstance(ManufacturerService.class);
+        Driver driver = new Driver("Pavlo", "SS 333");
+        Manufacturer manufacturer =
+                new Manufacturer("Opel", "Ukraine");
+        System.out.println("Created method: ");
+        Driver createdDriver = driverService.create(driver);
+        Manufacturer createdManufacturer =
+                manufacturerService.create(manufacturer);
+        System.out.println(createdDriver);
+        System.out.println(createdManufacturer);
+        System.out.println("Get all method: ");
+        List<Driver> driverList = driverService.getAll();
+        driverList.forEach(System.out::println);
+        List<Manufacturer> manufacturerList = manufacturerService.getAll();
+        manufacturerList.forEach(System.out::println);
+        System.out.println("Get method: ");
+        Optional<Driver> driverOptional = driverService.get(createdDriver.getId());
+        driverOptional.ifPresent(System.out::println);
+        Optional<Manufacturer> optionalManufacturer =
+                manufacturerService.get(createdManufacturer.getId());
+        optionalManufacturer.ifPresent(System.out::println);
+        System.out.println("Update method: ");
+        Driver driverForUpdate = new Driver("Slavik", "TT 555");
+        driverForUpdate.setId(2L);
+        createdDriver = driverService.update(driverForUpdate);
+        System.out.println(createdDriver);
+        Manufacturer manufacturerForUpdate =
+                new Manufacturer("Kia", "Russia");
+        manufacturerForUpdate.setId(10L);
+        createdManufacturer = manufacturerService.update(manufacturerForUpdate);
+        System.out.println(createdManufacturer);
+        System.out.println("Delete method: ");
+        System.out.println(driverService.delete(createdDriver.getId()));
+        System.out.println(manufacturerService
+                .delete(createdManufacturer.getId()));
     }
 }
