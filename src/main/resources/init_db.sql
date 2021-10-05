24013f1dcc2309dcb39cc23ea61c16b7abfad3ee
Dmytro Heleta <dmytroheleta@gmail.com>
2021-10-05 13:21:21 +0300

    added tabulation to init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -8,9 +8,9 @@ CREATE TABLE `manufacturers` (
                                         `is_deleted` TINYINT NOT NULL DEFAULT 0,
                                         PRIMARY KEY (`id`));
 CREATE TABLE `drivers` (
-                           `id` int NOT NULL AUTO_INCREMENT,
-                           `name` varchar(255) NOT NULL,
-                           `license_number` varchar(255) NOT NULL,
-                           `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                           PRIMARY KEY (`id`)
+                                        `id` int NOT NULL AUTO_INCREMENT,
+                                        `name` varchar(255) NOT NULL,
+                                        `license_number` varchar(255) NOT NULL,
+                                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
+                                        PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
