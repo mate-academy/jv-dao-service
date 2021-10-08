cbcbd9f7bf08f2cccded6aac889f9cc60909c4e3
Vladimir Nedashkovskiy <vo-v4ik@hotmail.com>
2021-10-08 11:46:51 +0300

    added DriverDao

---
+++ src/main/java/mate/jdbc/dao/DriverDaoImpl.java
@@ -0,0 +1,34 @@
+package mate.jdbc.dao;
+
+import java.util.List;
+import java.util.Optional;
+import mate.jdbc.lib.Dao;
+import mate.jdbc.model.Driver;
+
+@Dao
+public class DriverDaoImpl implements DriverDao {
+    @Override
+    public Driver create(Driver driver) {
+        return null;
+    }
+
+    @Override
+    public Optional<Driver> get(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Driver> getAll() {
+        return null;
+    }
+
+    @Override
+    public Driver update(Driver driver) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+}
