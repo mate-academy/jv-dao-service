b7d7e44183ba04829e51c5f4ce5e14503884fd4a
Vladimir Nedashkovskiy <vo-v4ik@hotmail.com>
2021-10-08 11:40:32 +0300

    implemented ManufacturerServiceImpl

--- src/main/java/mate/jdbc/service/ManufacturerServiceImpl.java
+++ src/main/java/mate/jdbc/service/ManufacturerServiceImpl.java
@@ -1,31 +1,40 @@
 package mate.jdbc.service;
 
 import java.util.List;
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Inject;
+import mate.jdbc.lib.Service;
 import mate.jdbc.model.Manufacturer;
 
+@Service
 public class ManufacturerServiceImpl implements ManufacturerService {
+    @Inject
+    ManufacturerDao manufacturerDao;
+
     @Override
     public Manufacturer create(Manufacturer manufacturer) {
-        return null;
+        return manufacturerDao.create(manufacturer);
     }
 
     @Override
     public Manufacturer get(Long id) {
-        return null;
+        return manufacturerDao.get(id).orElseThrow(() ->
+                new RuntimeException("The manufacturer was not found by id: " + id +
+                        " Check request details"));
     }
 
     @Override
     public List<Manufacturer> getAll() {
-        return null;
+        return manufacturerDao.getAll();
     }
 
     @Override
     public Manufacturer update(Manufacturer manufacturer) {
-        return null;
+        return manufacturerDao.update(manufacturer);
     }
 
     @Override
     public boolean delete(Long id) {
-        return false;
+        return manufacturerDao.delete(id);
     }
 }
