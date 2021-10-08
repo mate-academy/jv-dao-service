c076c0d0ff2d399f629303713b7d3cac83a62c78
Vladimir Nedashkovskiy <vo-v4ik@hotmail.com>
2021-10-08 12:17:27 +0300

    implemented DriverDaoImpl

--- src/main/java/mate/jdbc/model/Driver.java
+++ src/main/java/mate/jdbc/model/Driver.java
@@ -1,9 +1,19 @@
 package mate.jdbc.model;
 
+import java.util.Objects;
+
 public class Driver {
     private Long id;
     private String name;
     private String licenseNumber;
+
+    public Driver() {
+    }
+
+    public Driver(String name, String licenseNumber) {
+        this.name = name;
+        this.licenseNumber = licenseNumber;
+    }
 
     public Long getId() {
         return id;
@@ -27,5 +37,27 @@ public class Driver {
 
     public void setLicenseNumber(String licenseNumber) {
         this.licenseNumber = licenseNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Driver driver = (Driver) o;
+        return Objects.equals(id, driver.id) && Objects.equals(name, driver.name) && Objects.equals(licenseNumber, driver.licenseNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, licenseNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "Driver{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", licenseNumber='" + licenseNumber + '\'' +
+                '}';
     }
 }
