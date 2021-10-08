cbc8874b1629bfb6ea19e4c410b14371d1621656
Vladimir Nedashkovskiy <vo-v4ik@hotmail.com>
2021-10-08 11:10:45 +0300

    added services and model driver

---
+++ src/main/java/mate/jdbc/service/ManufacturerServiceImpl.java
@@ -0,0 +1,31 @@
+package mate.jdbc.service;
+
+import java.util.List;
+import mate.jdbc.model.Manufacturer;
+
+public class ManufacturerServiceImpl implements ManufacturerService {
+    @Override
+    public Manufacturer create(Manufacturer manufacturer) {
+        return null;
+    }
+
+    @Override
+    public Manufacturer get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Manufacturer> getAll() {
+        return null;
+    }
+
+    @Override
+    public Manufacturer update(Manufacturer manufacturer) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+}
