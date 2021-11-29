285d211097040ae0bf7b6dcb5165b9fd34781410
Oleg Shiriaev <mystidreamer@gmail.com>
2021-11-29 23:21:10 +0200

    driver dao and all services was created

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,50 @@
 package mate.jdbc;
 
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        DriverService driverService = (DriverService) injector
+                .getInstance(DriverService.class);
+        List<Driver> drivers = List.of(new Driver("Aaron", "fe2606"),
+                new Driver("Tommy", "au4419"),
+                new Driver("Bob", "tr9090"),
+                new Driver("Shon", "no7822"));
+        for (Driver driver : drivers) {
+            driverService.create(driver);
+        }
+        System.out.println(driverService.get(36L));
+        Driver driver = new Driver("Stewe", "fm3280");
+        driverService.create(driver);
+        driver.setLicenseNumber("tt8608");
+        driverService.update(driver);
+        driverService.getAll().forEach(System.out::println);
+        driverService.delete(33L);
+        driverService.delete(35L);
+
+        ManufacturerService manufacturerService = (ManufacturerService) injector
+                .getInstance(ManufacturerService.class);
+        List<Manufacturer> manufacturers = List.of(new Manufacturer("Mercedes", "Germany"),
+                new Manufacturer("Renault", "France"),
+                new Manufacturer("Honda", "Japan"),
+                new Manufacturer("Daewoo", "Ukraine"));
+        for (Manufacturer manufacturer : manufacturers) {
+            manufacturerService.create(manufacturer);
+        }
+        System.out.println(manufacturerService.get(8L));
+        Manufacturer manufacturer = new Manufacturer("Hyundai", "North Korea");
+        manufacturerService.create(manufacturer);
+        manufacturer.setCountry("South Korea");
+        manufacturerService.update(manufacturer);
+        manufacturerService.getAll().forEach(System.out::println);
+        manufacturerService.delete(7L);
+        manufacturerService.delete(10L);
     }
 }
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,9 +1,19 @@
-CREATE SCHEMA IF NOT EXISTS `taxi` DEFAULT CHARACTER SET utf8;
-USE `taxi`;
+CREATE DATABASE `taxi` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
 
 CREATE TABLE `manufacturers` (
-                                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                        `name` VARCHAR(225) NOT NULL,
-                                        `country` VARCHAR(225) NOT NULL,
-                                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                        PRIMARY KEY (`id`));
+                                 `id` bigint NOT NULL,
+                                 `name` varchar(255) DEFAULT NULL,
+                                 `country` varchar(255) DEFAULT NULL,
+                                 `is_deleted` tinyint NOT NULL DEFAULT '0',
+                                 PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `manufacturers` (
+                                 `id` bigint NOT NULL,
+                                 `name` varchar(255) DEFAULT NULL,
+                                 `country` varchar(255) DEFAULT NULL,
+                                 `is_deleted` tinyint NOT NULL DEFAULT '0',
+                                 PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
