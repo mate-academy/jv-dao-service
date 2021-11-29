17ff30cc65dd77a3a65017af4a99278978c73c89
Oleg Shiriaev <mystidreamer@gmail.com>
2021-11-29 23:25:19 +0200

    little changes in file init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -10,10 +10,11 @@ CREATE TABLE `manufacturers` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `manufacturers` (
-                                 `id` bigint NOT NULL,
+                                 `id` bigint NOT NULL AUTO_INCREMENT,
                                  `name` varchar(255) DEFAULT NULL,
                                  `country` varchar(255) DEFAULT NULL,
                                  `is_deleted` tinyint NOT NULL DEFAULT '0',
                                  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
