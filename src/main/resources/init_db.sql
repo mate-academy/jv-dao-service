c87d4fe22b7940c4ae2b014174a81cbbd2ae4c4c
Oleg Shiriaev <mystidreamer@gmail.com>
2021-11-29 23:28:36 +0200

    one more change in sql file

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,13 +1,14 @@
 CREATE DATABASE `taxi` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 
-CREATE TABLE `manufacturers` (
-                                 `id` bigint NOT NULL,
-                                 `name` varchar(255) DEFAULT NULL,
-                                 `country` varchar(255) DEFAULT NULL,
-                                 `is_deleted` tinyint NOT NULL DEFAULT '0',
-                                 PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `drivers` (
+                           `id` bigint NOT NULL AUTO_INCREMENT,
+                           `name` varchar(255) DEFAULT NULL,
+                           `licenseNumber` varchar(255) DEFAULT NULL,
+                           `is_deleted` tinyint NOT NULL DEFAULT '0',
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
 CREATE TABLE `manufacturers` (
                                  `id` bigint NOT NULL AUTO_INCREMENT,
