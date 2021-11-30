9a9d7d185b6e746b20a2da1f13f0868c7d3f0cf9
Roman Polulikh <polilikh.r@gmail.com>
2021-11-30 19:22:15 +0200

    checked ManufactureService implementation in main

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -3,6 +3,7 @@ package mate.jdbc;
 import java.util.List;
 import mate.jdbc.lib.Injector;
 import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
 import mate.jdbc.service.DriverService;
 import mate.jdbc.service.ManufacturerService;
 
@@ -12,6 +13,16 @@ public class Main {
     public static void main(String[] args) {
         ManufacturerService manufacturerService
                 = (ManufacturerService) injector.getInstance(ManufacturerService.class);
+        Manufacturer bmwManufacturer = new Manufacturer("BMW", "Germany");
+        manufacturerService.create(bmwManufacturer);
+        Long manufacturerIdToFind = 6L;
+        Manufacturer foundedManufacturer = manufacturerService.get(manufacturerIdToFind);
+        System.out.println(foundedManufacturer);
+        List<Manufacturer> manufacturers = manufacturerService.getAll();
+        manufacturers.forEach(System.out::println);
+        foundedManufacturer.setCountry("USA");
+        manufacturerService.update(foundedManufacturer);
+        manufacturerService.delete(manufacturerIdToFind);
         DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
         Driver bobDriver = new Driver("Bob", "DL8362748");
         driverService.create(bobDriver);
