a6f170bbbf207f21531e0fabe7b1fc02cdf9963c
Roman Polulikh <polilikh.r@gmail.com>
2021-11-30 16:27:49 +0200

    removed redundant constructors, refactored services

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,19 +1,19 @@
-CREATE SCHEMA IF NOT EXISTS `taxi` DEFAULT CHARACTER SET utf8;
-USE `taxi`;
+CREATE SCHEMA IF NOT EXISTS `taxi_service_db` DEFAULT CHARACTER SET utf8;
+USE `taxi_service_db`;
 
 CREATE TABLE `manufacturers` (
-                                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                        `name` VARCHAR(225) NOT NULL,
-                                        `country` VARCHAR(225) NOT NULL,
-                                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                        PRIMARY KEY (`id`));
+  `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(225) NOT NULL,
+  `country` VARCHAR(225) NOT NULL,
+  `is_deleted` TINYINT NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`));
 
 CREATE TABLE `drivers` (
-                           `id` bigint NOT NULL,
-                           `name` varchar(45) NOT NULL,
-                           `license_number` varchar(45) NOT NULL,
-                           `is_deleted` tinyint NOT NULL DEFAULT '0',
-                           PRIMARY KEY (`id`),
-                           UNIQUE KEY `id_UNIQUE` (`id`)
+  `id` BIGINT(11) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `license_number` VARCHAR(45) NOT NULL,
+  `is_deleted` TINYINT NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
