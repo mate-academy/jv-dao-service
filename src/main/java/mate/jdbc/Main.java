944da5335f363fd5bfde4826650ffc9cbe0c91a2
Andriy-dz <andriydzdz@gmail.com>
2021-12-01 17:15:14 +0200

    add tests for ManufacturerDaoImpl

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,9 @@
 package mate.jdbc;
 
+import mate.jdbc.dao.ManufacturerDao;
 import mate.jdbc.lib.Injector;
 import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
 import mate.jdbc.service.DriverService;
 
 public class Main {
@@ -25,5 +27,33 @@ public class Main {
         System.out.println(driverService.update(driverFirst));
 
         System.out.println(driverService.delete(2L));
+
+        Manufacturer manufacturerFirst = new Manufacturer();
+        manufacturerFirst.setName("First");
+        manufacturerFirst.setCountry("First");
+
+        Manufacturer manufacturerSecond = new Manufacturer();
+        manufacturerSecond.setName("Second");
+        manufacturerSecond.setCountry("Second");
+
+        Manufacturer manufacturerThird = new Manufacturer();
+        manufacturerThird.setName("Third");
+        manufacturerThird.setCountry("Third");
+
+        ManufacturerDao manufacturerDao =
+                (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+        manufacturerFirst = manufacturerDao.create(manufacturerFirst);
+        manufacturerSecond = manufacturerDao.create(manufacturerSecond);
+        manufacturerThird = manufacturerDao.create(manufacturerThird);
+
+        System.out.println(manufacturerDao.get(4L).orElse(manufacturerThird));
+
+        System.out.println(manufacturerDao.getAll());
+
+        manufacturerSecond.setName("newSecond");
+        manufacturerSecond.setCountry("newSecond");
+        System.out.println(manufacturerDao.update(manufacturerSecond));
+
+        manufacturerDao.delete(6L);
     }
 }
