8e317a758cd50fa95fa03154572e5d220a372e37
Olha Tankevych <freakystyley85@gmail.com>
2021-12-01 17:13:34 +0200

    created services, DriverDao and tested them in main

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,33 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerService manufacturerService = (ManufacturerService) injector
+                .getInstance(ManufacturerService.class);
+        Manufacturer firstManufacturer = new Manufacturer("Mazda", "Japan");
+        manufacturerService.create(firstManufacturer);
+        Manufacturer secondManufacturer = manufacturerService.get(firstManufacturer.getId());
+        secondManufacturer.setName("Mazda");
+        secondManufacturer.setCountry("Japan");
+        manufacturerService.update(secondManufacturer);
+        manufacturerService.delete(firstManufacturer.getId());
+        manufacturerService.getAll().forEach(System.out::println);
+        Driver firstDriver = new Driver("Ivanov", "123453");
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        driverService.create(firstDriver);
+        Driver secondDriver = driverService.get(firstDriver.getId());
+        secondDriver.setName("Savin");
+        secondDriver.setLicenseNumber("555555");
+        driverService.update(secondDriver);
+        driverService.delete(secondDriver.getId());
+        driverService.getAll().forEach(System.out::println);
     }
 }
