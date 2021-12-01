47075d9b5fdc06b0621d5794721ee78c184c3a8c
Olha Tankevych <freakystyley85@gmail.com>
2021-12-01 17:51:52 +0200

    added emptyline

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -7,6 +7,7 @@ CREATE TABLE `manufacturers` (
                                         `country` VARCHAR(225) NOT NULL,
                                         `is_deleted` TINYINT NOT NULL DEFAULT 0,
                                         PRIMARY KEY (`id`));
+
 CREATE TABLE `drivers` (
                            `id` bigint NOT NULL AUTO_INCREMENT,
                            `name` varchar(225) NOT NULL,
