f5e87a58b4bf88e9da38e29ecb5dfb22e17f92c6
Zachar Chorny <ufumoveldar@gmail.com>
2021-12-01 14:57:37 +0200

    edits made(2)

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -13,5 +13,3 @@ CREATE TABLE `drivers` (
                                         `is_deleted` tinyint NOT NULL DEFAULT '0',
                                          PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
-
-
