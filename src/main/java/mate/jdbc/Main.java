a3845b5272f15be959c0a890ecb302d63d662811
VitaliiMarchenko <vitalbka.01@gmail.com>
2021-12-02 18:36:28 +0200

    initial commit

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,25 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriverService;
+
 public class Main {
+    private static Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        DriverService driverService =
+                (DriverService) injector.getInstance(DriverService.class);
+
+        driverService.getAll()
+                .forEach(System.out::println);
+        Driver driverFromDB = driverService.get(1L);
+        Driver createdDriver = driverService.create(new Driver("Bob", "3123"));
+
+        driverFromDB.setName("updatedName");
+        driverFromDB.setLicenseNumber("updatedLicense");
+        Driver updateDriver = driverService.update(driverFromDB);
+
+        boolean deleteDriver = driverService.delete(1L);
     }
 }
