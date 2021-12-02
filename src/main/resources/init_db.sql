d41fa71b3543971423137cce95f935bdcffbbc40
SonyaPakhomova <88778908+SonyaPakhomova@users.noreply.github.com>
2021-12-02 22:24:03 +0200

    create service layer

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,15 +1,14 @@
-CREATE DATABASE `taxi_db`;
+CREATE DATABASE `taxi_db` DEFAULT CHARACTER SET utf8 DEFAULT ENCRYPTION='N';
 CREATE TABLE `manufacturers` (
                                  `id` bigint NOT NULL AUTO_INCREMENT,
                                  `name` varchar(255) DEFAULT NULL,
                                  `country` varchar(255) DEFAULT NULL,
                                  `is_deleted` tinyint NOT NULL DEFAULT '0',
-                                 PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
+                                 PRIMARY KEY (`id`));
 CREATE TABLE `drivers` (
                            `id` bigint NOT NULL AUTO_INCREMENT,
                            `name` varchar(45) DEFAULT NULL,
                            `license_number` varchar(45) DEFAULT NULL,
                            `is_deleted` tinyint DEFAULT NULL,
-                           PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
+                           PRIMARY KEY (`id`))
+ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
