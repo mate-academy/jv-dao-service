eb2d8b6dab6b2d77573a0b182efd5e308c32a8cb
VitaliiMarchenko <vitalbka.01@gmail.com>
2021-12-02 19:21:15 +0200

    added Manufacturer test in main

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -2,24 +2,36 @@ package mate.jdbc;
 
 import mate.jdbc.lib.Injector;
 import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
 import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
 
 public class Main {
     private static Injector injector = Injector.getInstance("mate.jdbc");
 
     public static void main(String[] args) {
-        DriverService driverService =
-                (DriverService) injector.getInstance(DriverService.class);
+        DriverService driverService = (DriverService)
+                injector.getInstance(DriverService.class);
+        final ManufacturerService manufacturerService = (ManufacturerService)
+                injector.getInstance(ManufacturerService.class);
 
         driverService.getAll()
                 .forEach(System.out::println);
-        Driver driverFromDB = driverService.get(1L);
+        Driver driverFromDB = driverService.get(2L);
         Driver createdDriver = driverService.create(new Driver("Bob", "3123"));
-
         driverFromDB.setName("updatedName");
         driverFromDB.setLicenseNumber("updatedLicense");
         Driver updateDriver = driverService.update(driverFromDB);
-
         boolean deleteDriver = driverService.delete(1L);
+
+        manufacturerService.getAll()
+                .forEach(System.out::println);
+        Manufacturer manufacturerFromDB = manufacturerService.get(2L);
+        Manufacturer createdManufacturer =
+                manufacturerService.create(new Manufacturer("BMW", "Germany"));
+        manufacturerFromDB.setName("Ferrari");
+        manufacturerFromDB.setCountry("Italy");
+        Manufacturer updateManufacturer = manufacturerService.update(manufacturerFromDB);
+        boolean deleteManufacturer = manufacturerService.delete(1L);
     }
 }
