6602031e6dea4053bc72b3e598c9d2e259ea959a
SarumanIsAlive <90073141+SarumanIsAlive@users.noreply.github.com>
2021-12-02 20:09:17 +0200

    --> Everything was done according to the terms of the assignment.

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,41 @@
 package mate.jdbc;
 
+import java.util.Random;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+    private static final int RANDOM_RANGE = 9999;
+
     public static void main(String[] args) {
-        // test your code here
+        DriverService driverService =
+                (DriverService) injector.getInstance(DriverService.class);
+        driverService.create(new Driver("Maria",
+                String.valueOf(new Random().nextInt(RANDOM_RANGE))));
+        driverService.create(new Driver("Alice",
+                String.valueOf(new Random().nextInt(RANDOM_RANGE))));
+        Driver updateDriver = new Driver("J.Bond", "007");
+        updateDriver.setId(4L);
+        System.out.println(driverService.get(4L));
+        driverService.update(updateDriver);
+        System.out.println(driverService.get(8L));
+        driverService.delete(5L);
+        driverService.getAll().forEach(System.out::println);
+        System.out.println();
+        ManufacturerService manufacturerService =
+                (ManufacturerService) injector.getInstance(ManufacturerService.class);
+        manufacturerService.create(new Manufacturer("BMW","Germany"));
+        manufacturerService.create(new Manufacturer("Audi","Germany"));
+        Manufacturer updateManufacturer = new Manufacturer("Tesla","USA");
+        updateManufacturer.setId(2L);
+        System.out.println(manufacturerService.get(4L));
+        manufacturerService.update(updateManufacturer);
+        System.out.println(manufacturerService.get(5L));
+        manufacturerService.delete(1L);
+        manufacturerService.getAll().forEach(System.out::println);
     }
 }
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,9 +1,16 @@
-CREATE SCHEMA IF NOT EXISTS `taxi` DEFAULT CHARACTER SET utf8;
-USE `taxi`;
+CREATE DATABASE `taxi_db` DEFAULT CHARACTER SET utf8 DEFAULT ENCRYPTION='N';
 
 CREATE TABLE `manufacturers` (
-                                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                        `name` VARCHAR(225) NOT NULL,
-                                        `country` VARCHAR(225) NOT NULL,
-                                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                        PRIMARY KEY (`id`));
+                                 `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                                 `name` VARCHAR(225) NOT NULL,
+                                 `country` VARCHAR(225) NOT NULL,
+                                 `is_deleted` TINYINT NOT NULL DEFAULT 0,
+                                 PRIMARY KEY (`id`));
+
+CREATE TABLE `drivers` (
+                           `id` bigint NOT NULL AUTO_INCREMENT,
+                           `name` varchar(255) DEFAULT NULL,
+                           `license_number` varchar(255) DEFAULT NULL,
+                           `is_deleted` tinyint NOT NULL DEFAULT '0',
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoD
