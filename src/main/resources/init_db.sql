6cf648be8b1688fa6199e26e87a7a51f3a685fbe
Panchenko Volodymyr <volodymyr.panchenko147@egmail.com>
2021-12-03 10:21:11 +0200

    init_db was changed and other fixes

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE `taxi_service` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE `taxi_service` DEFAULT CHARACTER SET utf8 DEFAULT ENCRYPTION='N' */;
 
 CREATE TABLE `drivers` (
                            `id` bigint NOT NULL AUTO_INCREMENT,
@@ -6,7 +6,7 @@ CREATE TABLE `drivers` (
                            `licenseNumber` varchar(255) DEFAULT NULL,
                            `is_deleted` tinyint NOT NULL DEFAULT '0',
                            PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3;
 
 CREATE TABLE `manufacturers` (
                                  `id` bigint NOT NULL AUTO_INCREMENT,
@@ -14,4 +14,4 @@ CREATE TABLE `manufacturers` (
                                  `country` varchar(255) DEFAULT NULL,
                                  `is_deleted` tinyint NOT NULL DEFAULT '0',
                                  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3;
