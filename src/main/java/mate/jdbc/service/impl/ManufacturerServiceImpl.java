86ffffa425db7c6020dae92bd15e9ed16f2b5bcc
VitaliiMarchenko <vitalbka.01@gmail.com>
2021-12-03 10:04:17 +0200

    fixed get in services

--- src/main/java/mate/jdbc/service/impl/ManufacturerServiceImpl.java
+++ src/main/java/mate/jdbc/service/impl/ManufacturerServiceImpl.java
@@ -1,7 +1,6 @@
 package mate.jdbc.service.impl;
 
 import java.util.List;
-import java.util.Optional;
 import mate.jdbc.dao.ManufacturerDao;
 import mate.jdbc.lib.Inject;
 import mate.jdbc.lib.Service;
@@ -20,8 +19,7 @@ public class ManufacturerServiceImpl implements ManufacturerService {
 
     @Override
     public Manufacturer get(Long id) {
-        Optional<Manufacturer> manufacturer = manufacturerDao.get(id);
-        return manufacturer.orElseGet(() -> null);
+        return manufacturerDao.get(id).orElseThrow();
     }
 
     @Override
