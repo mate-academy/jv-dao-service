a11cc810a26f92675372f6b4c737f043617ac703
Yevheniy Savenkov <yevheniy.savenkov@gmail.com>
2021-12-03 15:57:59 +0200

    second commit of the second commit

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -6,7 +6,7 @@ CREATE TABLE `drivers` (
                            `license_number` varchar(255) DEFAULT NULL,
                            `is_deleted` tinyint DEFAULT '0',
                            PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+);
 CREATE TABLE `manufacturers` (
                                  `id` bigint NOT NULL AUTO_INCREMENT,
                                  `name` varchar(255) DEFAULT NULL,
