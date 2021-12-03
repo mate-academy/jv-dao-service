c71ae2bd4ea807e452fafa030535198874aff288
Volodimir <vova.stepanenko2020@gmail.com>
2021-12-03 14:31:09 +0200

    throw exception

--- src/main/java/mate/jdbc/service/impl/DriverServiceImpl.java
+++ src/main/java/mate/jdbc/service/impl/DriverServiceImpl.java
@@ -19,7 +19,8 @@ public class DriverServiceImpl implements DriverService {
 
     @Override
     public Driver get(Long id) {
-        return driverDao.get(id).orElseThrow();
+        return driverDao.get(id).orElseThrow(
+            () -> new RuntimeException("Empty driver from DAO"));
     }
 
     @Override
