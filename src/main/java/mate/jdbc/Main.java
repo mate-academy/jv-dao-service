ebff8495497e7aa87aaa69009b376a8a51ab5c5b
Evgen Lanovenko <ev.lanovenko@gmail.com>
2022-01-17 18:38:42 +0200

    added model Driver, DriverDao, DriverService

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,21 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriverService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        Driver alice = new Driver(4L,"Alice", "127");
+        Driver ben = new Driver("Ben", "124");
+        driverService.create(ben);
+        driverService.update(alice);
+        driverService.delete(6L);
+        driverService.getAll().forEach(System.out::println);
+        System.out.println(driverService.get(3L));
         // test your code here
     }
 }
