bb354653275022100e883b0305bf1139f53fb6f3
Andriy_Poluhovic <93582635+pikador10@users.noreply.github.com>
2022-02-22 22:39:25 +0200

    Update init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,5 +1,4 @@
 CREATE DATABASE `library_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
-
 CREATE TABLE `manufacturers` (
   `name` varchar(111) DEFAULT NULL,
   `country` varchar(111) DEFAULT NULL,
@@ -7,8 +6,6 @@ CREATE TABLE `manufacturers` (
   `is_deleted` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8;
-
-
 CREATE TABLE `drivers` (
   `name` varchar(211) DEFAULT NULL,
   `license_number` varchar(211) DEFAULT NULL,
@@ -16,4 +13,3 @@ CREATE TABLE `drivers` (
   `is_deleted` tinyint(4) DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
-
