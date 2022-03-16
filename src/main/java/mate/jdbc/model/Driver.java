84df5233f348ccd2601f620b0b7223368ad70643
Oleh Mytiai <mytiai.ov@icloud.com>
2022-03-16 03:34:33 +0200

    created model Driver

---
+++ src/main/java/mate/jdbc/model/Driver.java
@@ -0,0 +1,64 @@
+package mate.jdbc.model;
+
+import java.util.Objects;
+
+public class Driver {
+    private Long id;
+    private String name;
+    private String licenceNumber;
+
+    public Driver() {
+    }
+
+    public Driver(Long id, String name, String licenceNumber) {
+        this.id = id;
+        this.name = name;
+        this.licenceNumber = licenceNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLicenceNumber() {
+        return licenceNumber;
+    }
+
+    public void setLicenceNumber(String licenceNumber) {
+        this.licenceNumber = licenceNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Driver driver = (Driver) o;
+        return Objects.equals(id, driver.id) && Objects.equals(name, driver.name) && Objects.equals(licenceNumber, driver.licenceNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, licenceNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "Driver{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", licenceNumber='" + licenceNumber + '\'' +
+                '}';
+    }
+}
