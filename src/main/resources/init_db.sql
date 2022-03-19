15eb020609e2158fbb955f1ea089c1fa6ccfe41e
Alex Drapikovsky <drapikovskyan@gmail.com>
2022-03-19 14:07:52 +0200

    JDBC-Homework. part2. first commit

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -7,3 +7,12 @@ CREATE TABLE `manufacturers` (
                                         `country` VARCHAR(225) NOT NULL,
                                         `is_deleted` TINYINT NOT NULL DEFAULT 0,
                                         PRIMARY KEY (`id`));
+
+CREATE TABLE `drivers` (
+                           `id` bigint NOT NULL AUTO_INCREMENT,
+                           `name` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+                           `license_number` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+                           `is_deleted` tinyint NOT NULL DEFAULT '0',
+                           PRIMARY KEY (`driver_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
+
