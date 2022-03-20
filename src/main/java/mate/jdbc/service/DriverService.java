f3b79ec73eec0fb1c758fd110686c50e37c7a3bf
DenTima <tymoshchuk.den@gmail.com>
2022-03-20 21:52:24 +0200

    Added Driver service and impl

--- src/main/java/mate/jdbc/service/DriverService.java
+++ src/main/java/mate/jdbc/service/DriverService.java
@@ -1,4 +1,17 @@
 package mate.jdbc.service;
 
+import mate.jdbc.model.Driver;
+
+import java.util.List;
+
 public interface DriverService {
+    Driver create(Driver driver);
+
+    Driver get(Long id);
+
+    List<Driver> getAll();
+
+    Driver update(Driver driver);
+
+    boolean delete(Long id);
 }
