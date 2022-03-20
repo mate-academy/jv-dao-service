a962800c135a667261988fdc6db797727e688dd2
Liubov Chernohor <liubovchernohor@gmail.com>
2022-03-20 22:40:50 +0200

    Classes of Service layer and driverDao class were added to project

---
+++ src/main/java/mate/jdbc/model/Driver.java
@@ -0,0 +1,48 @@
+package mate.jdbc.model;
+
+public class Driver {
+    private Long id;
+    private String name;
+    private String licenseNumber;
+
+    public Driver(String name, String licenseNumber) {
+        this.name = name;
+        this.licenseNumber = licenseNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Driver setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Driver setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public Driver setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Driver{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + ", licenseNumber='" + licenseNumber + '\''
+                + '}';
+    }
+}
