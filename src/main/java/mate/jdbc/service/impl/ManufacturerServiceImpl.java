6204396a847f4115db8a0b37495e00948f4e3303
DenTima <tymoshchuk.den@gmail.com>
2022-03-20 21:22:07 +0200

    Added manufacturer service and impl

---
+++ src/main/java/mate/jdbc/service/impl/ManufacturerServiceImpl.java
@@ -0,0 +1,38 @@
+package mate.jdbc.service.impl;
+
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Inject;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.ManufacturerService;
+
+import java.util.List;
+
+public class ManufacturerServiceImpl implements ManufacturerService {
+    @Inject
+    private ManufacturerDao manufacturerDao;
+
+    @Override
+    public Manufacturer create(Manufacturer manufacturer) {
+        return manufacturerDao.create(manufacturer);
+    }
+
+    @Override
+    public Manufacturer get(Long id) {
+        return manufacturerDao.get(id).get();
+    }
+
+    @Override
+    public List<Manufacturer> getAll() {
+        return manufacturerDao.getAll();
+    }
+
+    @Override
+    public Manufacturer update(Manufacturer manufacturer) {
+        return manufacturerDao.update(manufacturer);
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return manufacturerDao.delete(id);
+    }
+}
