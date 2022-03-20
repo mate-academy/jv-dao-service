9d5fabed944f9defc98fbcdccf38d0dd94a1fc3a
Misha Dzhuzha <dzhuzhamichael@gmail.com>
2022-03-20 13:05:49 +0200

    DriverDao and DriverDaoImpl, model Driver, DriverService and ManufacturerService were implemented

---
+++ src/main/java/mate/jdbc/service/ManufacturerServiceImpl.java
@@ -0,0 +1,40 @@
+package mate.jdbc.service;
+
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Inject;
+import mate.jdbc.lib.Service;
+import mate.jdbc.model.Manufacturer;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+public class ManufacturerServiceImpl implements ManufacturerService {
+    @Inject
+    private ManufacturerDao manufacturerDao;
+
+    @Override
+    public Manufacturer create(Manufacturer manufacturer) {
+        return manufacturerDao.create(manufacturer);
+    }
+
+    @Override
+    public Manufacturer get(Long id) {
+        return manufacturerDao.get(id).orElseThrow(()
+        -> new NoSuchElementException("Couldn't get such element by id: " + id));
+    }
+
+    @Override
+    public List<Manufacturer> getAll() {
+        return manufacturerDao.getAll();
+    }
+
+    @Override
+    public Manufacturer update(Manufacturer manufacturer) {
+        return manufacturerDao.update(manufacturer);
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return manufacturerDao.delete(id);
+    }
+}
