ced2516bc7253f4a9b7527911edcc1ed7046185b
AprioriKS <sounddummies@gmail.com>
2022-03-21 19:02:52 +0200

    implements Service

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,6 +1,12 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriverService;
+
 public class Main {
+    private static Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
         DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
         Driver driver = driverService.create(new Driver("Kostya", "12345"));
