48cef0ad35912096d1ad5fdd029c657f546ca5a2
Alex Drapikovsky <drapikovskyan@gmail.com>
2022-03-21 14:49:02 +0200

    JDBC-Homework. part2. after mentor review

--- src/main/java/mate/jdbc/service/DriverServiceImpl.java
+++ src/main/java/mate/jdbc/service/DriverServiceImpl.java
@@ -2,7 +2,6 @@ package mate.jdbc.service;
 
 import java.util.List;
 import mate.jdbc.dao.DriverDao;
-import mate.jdbc.exception.DataProcessingException;
 import mate.jdbc.lib.Inject;
 import mate.jdbc.lib.Service;
 import mate.jdbc.model.Driver;
@@ -19,7 +18,7 @@ public class DriverServiceImpl implements DriverService {
 
     @Override
     public Driver get(Long id) {
-            return driverDao.get(id).orElseThrow();
+        return driverDao.get(id).orElseThrow();
     }
 
     @Override
