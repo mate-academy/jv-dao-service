ccab7759dc46358e9704948afa7fff1e78701885
Sasha Bachynskyi <allfred12@gmail.com>
2022-03-21 11:23:43 +0200

    modified main class

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,6 +1,5 @@
 package mate.jdbc;
 
-import java.util.ArrayList;
 import java.util.List;
 import mate.jdbc.lib.Injector;
 import mate.jdbc.model.Driver;
@@ -12,70 +11,19 @@ public class Main {
     public static void main(String[] args) {
         DriverService driverService = (DriverService)
                 injector.getInstance(DriverService.class);
-
-        addingDataToDb(driverService);
-
-        createManufacturerInDb(driverService);
-
-        getManufacturerFromDb(driverService);
-
-        getAllManufacturersFromDb(driverService);
-
-        updateManufacturerInDb(driverService);
-
-        deleteManufacturerById(driverService);
-    }
-
-    private static void addingDataToDb(DriverService driverService) {
-        Driver driverBob = new Driver();
-        driverBob.setName("Bob");
-        driverBob.setLicenseNumber("45432");
-        Driver driverAlice = new Driver();
-        driverAlice.setName("Alice");
-        driverAlice.setLicenseNumber("48599");
-        Driver driverJohn = new Driver();
-        driverJohn.setName("John");
-        driverJohn.setLicenseNumber("41211");
-        List<Driver> drivers = new ArrayList<>();
-        drivers.add(driverBob);
-        drivers.add(driverAlice);
-        drivers.add(driverJohn);
-        for (Driver driver : drivers) {
-            driverService.create(driver);
-        }
-    }
-
-    private static void createManufacturerInDb(DriverService driverService) {
-        Driver driver = new Driver();
-        driver.setName("Joe");
-        driver.setLicenseNumber("17799");
-        System.out.println("Created a new driver in DB: "
-                + driverService.create(driver));
-    }
-
-    private static void getManufacturerFromDb(DriverService driverService) {
-        Long driverBobId = 1L;
-        System.out.println("Bob driver from DB: "
-                + driverService.get(driverBobId));
-    }
-
-    private static void getAllManufacturersFromDb(DriverService driverService) {
-        List<Driver> drivers = driverService.getAll();
-        System.out.println("All manufacturers from DB: ");
-        drivers.forEach(System.out::println);
-    }
-
-    private static void updateManufacturerInDb(DriverService driverService) {
-        Long driverAliveId = 2L;
-        Driver newDriverForUpdate =
-                new Driver(driverAliveId, "Alice", "11122");
-        System.out.println("Updated driver in DB: "
-                + driverService.update(newDriverForUpdate));
-    }
-
-    private static void deleteManufacturerById(DriverService driverService) {
-        Long driverBobId = 1L;
-        System.out.println("Deleted driver from DB by ID one: "
-                + driverService.delete(driverBobId));
+        Driver bob = new Driver();
+        bob.setName("Bob");
+        bob.setLicenseNumber("12345");
+        Driver bobFromDb = driverService.create(bob);
+        System.out.println("Created driver from DB: " + bobFromDb);
+        System.out.println("Get Bob driver from DB by ID: " + driverService.get(bob.getId()));
+        List<Driver> allDriversFromDB = driverService.getAll();
+        System.out.println("All drivers from DB: ");
+        allDriversFromDB.forEach(System.out::println);
+        bobFromDb.setLicenseNumber("98745");
+        Driver updatedBobFromDB = driverService.update(bobFromDb);
+        System.out.println("Updated driver from DB: " + updatedBobFromDB);
+        System.out.println("Deleted driver by ID from DB: "
+                + driverService.delete(updatedBobFromDB.getId()));
     }
 }
