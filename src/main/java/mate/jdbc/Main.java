2245daf9df257aced23b1259c5e8f7c3664c514a
Roman <dudanecr@gmail.com>
2022-03-21 15:44:36 +0200

    dao_vs_service

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,27 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        DriverService driverService = (DriverService)
+                injector.getInstance(DriverService.class);
+        Driver driver = new Driver();
+        driver.setName("Jon");
+        driver.setLicenseNumber("123");
+        driverService.create(driver);
+        driverService.update(new Driver(2L, "Kevin", "234"));
+        driverService.delete(2L);
+        driverService.getAll().forEach(System.out::println);
+        System.out.println(driverService.get(3L));
+        ManufacturerService manufacturerService = (ManufacturerService)
+                injector.getInstance(ManufacturerService.class);
+        manufacturerService.getAll().forEach(System.out::println);
+
     }
 }
