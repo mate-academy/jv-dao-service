e619c4ad4c68b9dcb29e06a97c5b09e66c4eb402
AprioriKS <sounddummies@gmail.com>
2022-03-21 18:53:54 +0200

    DriverDao implemented

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -2,6 +2,12 @@ package mate.jdbc;
 
 public class Main {
     public static void main(String[] args) {
-        // test your code here
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        Driver driver = driverService.create(new Driver("Kostya", "12345"));
+        driverService.get(driver.getId());
+        driverService.getAll();
+        driver.setName("Alice");
+        driverService.update(driver);
+        driverService.delete(driver.getId());
     }
 }
