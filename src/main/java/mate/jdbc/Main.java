c7513549e7dc1ade17f4d2937a80c28a61fc6b98
DenTima <tymoshchuk.den@gmail.com>
2022-03-22 09:39:23 +0200

    Fixed hardcode identifiers

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -12,30 +12,27 @@ public class Main {
     public static void main(String[] args) {
         DriverService driverService =
                 (DriverService) injector.getInstance(DriverService.class);
-        Driver driver1 = new Driver("driver1", "12345");
-        Driver driver2 = new Driver("driver2", "54321");
-        Driver driver3 = new Driver("driver3", "56789");
-        driverService.create(driver1);
-        driverService.create(driver2);
-        driverService.create(driver3);
-        driver1 = driverService.get(1L);
+        Driver driver1 = driverService.create(new Driver("driver1", "12345"));
+        Driver driver2 = driverService.create(new Driver("driver2", "54321"));
+        Driver driver3 = driverService.create(new Driver("driver3", "56789"));
+        driver1 = driverService.get(driver1.getId());
         driver1.setLicenseNumber("98765");
         driverService.update(driver1);
-        driverService.delete(1L);
+        System.out.println("Deleted: = " + driverService.delete(driver1.getId()));
         driverService.getAll().forEach(System.out::println);
 
         ManufacturerService manufacturerService =
                 (ManufacturerService) injector.getInstance(ManufacturerService.class);
-        Manufacturer manufacturer1 = new Manufacturer("manufacturer1", "country1");
-        Manufacturer manufacturer2 = new Manufacturer("manufacturer2", "country2");
-        Manufacturer manufacturer3 = new Manufacturer("manufacturer3", "country3");
-        manufacturerService.create(manufacturer1);
-        manufacturerService.create(manufacturer2);
-        manufacturerService.create(manufacturer3);
-        manufacturer1 = manufacturerService.get(1L);
+        Manufacturer manufacturer1 = manufacturerService.create(
+                new Manufacturer("manufacturer1", "country1"));
+        Manufacturer manufacturer2 = manufacturerService.create(
+                new Manufacturer("manufacturer2", "country2"));
+        Manufacturer manufacturer3 = manufacturerService.create(
+                new Manufacturer("manufacturer3", "country3"));
+        manufacturer1 = manufacturerService.get(manufacturer1.getId());
         manufacturer1.setCountry("country99");
         manufacturerService.update(manufacturer1);
-        manufacturerService.delete(1L);
+        System.out.println("Deleted: = " + manufacturerService.delete(manufacturer1.getId()));
         manufacturerService.getAll().forEach(System.out::println);
     }
 }
