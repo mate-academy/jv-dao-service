82e96d5a42d2ddc0a12ca1d9c3034fa4e704ecc5
Vitalii Bozadzhy <VitaliiBozadzhy@icloud.com>
2022-04-22 10:11:23 +0200

    - corrected code

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `drivers` (
-                           `id` bigint NOT NULL AUTO_INCREMENT,
-                           `name` varchar(255) NOT NULL,
-                           `licenseNumber` varchar(255) NOT NULL,
-                           `is_deleted` tinyint NOT NULL DEFAULT 0,
+                           `id` BIGINT NOT NULL AUTO_INCREMENT,
+                           `name` VARCHAR(255) NOT NULL,
+                           `licenseNumber` VARCHAR(255) NOT NULL,
+                           `is_deleted` TINYINT NOT NULL DEFAULT 0,
                            PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
