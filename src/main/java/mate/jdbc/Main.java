2b22185923478835a42fc2519e2cf2d8cce8f065
Vitalii Moisieiev <vitalij.mojsejiv@gmail.com>
2022-05-03 21:44:34 +0300

    Refactored application testing methods calls in Main method

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -11,27 +11,29 @@ public class Main {
         Injector injector = Injector.getInstance("mate.jdbc");
         DriverService driverService =
                 (DriverService) injector.getInstance(DriverService.class);
-        Driver driver1 = new Driver("Bob", "123456");
-        Driver driver2 = new Driver("Alice", "654321");
-        Driver driver3 = new Driver("John", "123654");
-        driverService.create(driver2);
-        driverService.create(driver3);
-        driverService.create(driver1);
-        driverService.update(new Driver(1L, "Patricia", "918273"));
-        driverService.delete(2L);
-        System.out.println(driverService.get(1L));
+        Driver driverBob = driverService.create(
+                new Driver("Bob", "123456"));
+        Driver driverAlice = driverService.create(
+                new Driver("Alice", "654321"));
+        Driver driverJohn = driverService.create(
+                new Driver("John", "123654"));
+        driverService.update(
+                new Driver(driverBob.getId(), "Patricia", "918273"));
+        driverService.delete(driverAlice.getId());
+        System.out.println(driverService.get(driverBob.getId()));
         driverService.getAll().forEach(System.out::println);
         ManufacturerService manufacturerService =
                 (ManufacturerService) injector.getInstance(ManufacturerService.class);
-        Manufacturer manufacturer1 = new Manufacturer("Audi", "Germany");
-        Manufacturer manufacturer2 = new Manufacturer("Daewoo", "Ukraine");
-        Manufacturer manufacturer3 = new Manufacturer("KIA", "South Korea");
-        manufacturerService.create(manufacturer1);
-        manufacturerService.create(manufacturer2);
-        manufacturerService.create(manufacturer3);
-        manufacturerService.update(new Manufacturer(1L,"Rolls Royce", "UK"));
-        manufacturerService.delete(2L);
-        System.out.println(manufacturerService.get(1L));
+        Manufacturer manufacturerAudi = manufacturerService.create(
+                new Manufacturer("Audi", "Germany"));
+        Manufacturer manufacturerDaewoo = manufacturerService.create(
+                new Manufacturer("Daewoo", "Ukraine"));
+        Manufacturer manufacturerKia = manufacturerService.create(
+                new Manufacturer("KIA", "South Korea"));
+        manufacturerService.update(
+                new Manufacturer(manufacturerAudi.getId(), "Rolls Royce", "UK"));
+        manufacturerService.delete(manufacturerDaewoo.getId());
+        System.out.println(manufacturerService.get(manufacturerAudi.getId()));
         manufacturerService.getAll().forEach(System.out::println);
     }
 }
