0bbd3cb8a36c7d189e1f40c67d7503fb270d0238
Alina <vasylyshyn.alina@gmail.com>
2022-05-04 11:54:22 +0300

    added correct init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,15 +1,16 @@
-CREATE DATABASE 'transport_db';
-CREATE TABLE 'drivers' (
-    'id' BIGINT NOT NULL AUTO_INCREMENT,
-    'name' VARCHAR(255) NULL,
-    'licenseNumber' VARCHAR(255) NULL ,
-    'is_deleted' TINYINT NOT NULL  DEFAULT  0,
-    PRIMARY KEY ('id')
+CREATE DATABASE `transport_db`;
+CREATE TABLE `drivers` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NULL,
+    `licenseNumber` VARCHAR(255) NULL ,
+    `is_deleted` TINYINT NOT NULL DEFAULT 0,
+    PRIMARY KEY (`id`)
 );
 
-CREATE TABLE 'manufacturers' (
-    'id' INT NOT NULL AUTO_INCREMENT,
-    'name' VARCHAR(255) NULL,
-    'country' VARCHAR(255) NULL,
-    'is_deleted' TINYINT NOT NULL  DEFAULT  0
+CREATE TABLE `manufacturers` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NULL,
+    `country` VARCHAR(255) NULL,
+    `is_deleted` TINYINT NOT NULL DEFAULT 0,
+    PRIMARY KEY (`id`)
 );
