b847b0486e170c381c1fe00f79ab5ce0041f22c8
GerasimovTA-92 <olexandr.gerasimov.02@gmail.com>
2022-05-11 14:24:39 +0300

    fixed init.db

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,5 +1,5 @@
-CREATE SCHEMA IF NOT EXISTS `taxi_service_db` DEFAULT CHARACTER SET utf8;
-USE `taxi_service_db`;
+CREATE DATABASE `taxi_service_db` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
 
 CREATE TABLE `manufacturers` (
                                         `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
