3474abecf4da66482792aa4159ae203f47da7959
VladyslavLn <kapuuustin@gmail.com>
2022-05-11 13:24:13 +0300

    Added solution :)

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,9 +1,20 @@
-CREATE SCHEMA IF NOT EXISTS `taxi_service` DEFAULT CHARACTER SET utf8;
-USE `taxi_service`;
+CREATE SCHEMA IF NOT EXISTS `taxi_db` DEFAULT CHARACTER SET utf8;
+USE `taxi_db`;
 
 CREATE TABLE `manufacturers` (
-                                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                        `name` VARCHAR(225) NOT NULL,
-                                        `country` VARCHAR(225) NOT NULL,
-                                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                        PRIMARY KEY (`id`));
+                                 `id` bigint NOT NULL AUTO_INCREMENT,
+                                 `name` varchar(225) NOT NULL,
+                                 `country` varchar(225) NOT NULL,
+                                 `is_deleted` tinyint NOT NULL DEFAULT '0',
+                                 PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `driver` (
+                          `id` bigint NOT NULL AUTO_INCREMENT,
+                          `name` varchar(225) NOT NULL,
+                          `licenseNumber` varchar(225) NOT NULL,
+                          `is_deleted` tinyint NOT NULL DEFAULT '0',
+                          PRIMARY KEY (`id`),
+                          UNIQUE KEY `driver_id_uindex` (`id`),
+                          UNIQUE KEY `driver_licenseNumber_uindex` (`licenseNumber`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
