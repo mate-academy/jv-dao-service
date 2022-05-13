fbb9b4199947438bca8c477ba29722f0949187ab
GrabenkoVitaliy <grabenkovitaliy7@gmail.com>
2022-05-13 11:05:22 +0300

    hw has been done

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,23 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.services.DriverService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
         // test your code here
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        Driver sashko = new Driver(1L, "Alex", "12345");
+        Driver viktor = new Driver(2L, "Viktor", "21345");
+        driverService.create(sashko);
+        driverService.create(viktor);
+        System.out.println(driverService.get(viktor.getId()));
+        sashko.setLicenseNumber("9999");
+        driverService.update(sashko);
+        driverService.delete(viktor.getId());
+        System.out.println(driverService.getAll());
     }
 }
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -7,3 +7,12 @@ CREATE TABLE `manufacturers` (
                                         `country` VARCHAR(225) NOT NULL,
                                         `is_deleted` TINYINT NOT NULL DEFAULT 0,
                                         PRIMARY KEY (`id`));
+
+CREATE TABLE `taxiservice_db`.`drivers` (
+                                            `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                                            `name` VARCHAR(255) NOT NULL,
+                                            `licenseNumber` VARCHAR(255) NOT NULL,
+                                            `is_deleted` TINYINT NOT NULL DEFAULT 0,
+                                            PRIMARY KEY (`id`))
+    ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
