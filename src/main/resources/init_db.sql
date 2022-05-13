5bde3d9990e353b9124b24be3f3b55972184b50b
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2022-05-13 11:07:52 +0300

    Create `Driver` model & implement its `DAO`

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -7,3 +7,13 @@ CREATE TABLE `manufacturers` (
                                         `country` VARCHAR(225) NOT NULL,
                                         `is_deleted` TINYINT NOT NULL DEFAULT 0,
                                         PRIMARY KEY (`id`));
+
+CREATE TABLE `drivers` (
+                                        `id` bigint NOT NULL AUTO_INCREMENT,
+                                        `name` varchar(225) NOT NULL,
+                                        `license_number` varchar(12) NOT NULL,
+                                        `is_deleted` tinyint NOT NULL DEFAULT '0',
+                                        PRIMARY KEY (`id`),
+                                        UNIQUE KEY `license_number_UNIQUE` (`license_number`)
+                                        ) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb3;
+
