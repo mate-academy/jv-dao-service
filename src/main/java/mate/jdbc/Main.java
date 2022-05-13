36289542e62ff0f07d341ed28ddb12b3fd4c3837
Vladyslav Fomenko <with.god.victorius@gmail.com>
2022-05-13 16:02:13 +0300

    implemented drivers dao service

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,25 @@
 package mate.jdbc;
 
+import mate.jdbc.dao.DriverDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        DriverDao driverDao =
+                (DriverDao) injector.getInstance(DriverDao.class);
+        Driver petro = new Driver();
+        petro.setName("Petro");
+        petro.setLicenseNumber("123654");
+        driverDao.create(petro);
+
+        Driver sasha = new Driver();
+        sasha.setName("Sasha");
+        sasha.setLicenseNumber("123604");
+        driverDao.create(sasha);
+
+        driverDao.getAll().forEach(System.out::println);
     }
 }
