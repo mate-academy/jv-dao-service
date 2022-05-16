6118577cdbc1f9a1b483ff0a239ceee382adabd3
Denys.Sh <7860denis@gmail.com>
2022-05-16 16:41:23 +0300

    my solution is done

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,26 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
     public static void main(String[] args) {
-        // test your code here
+        Injector injector = Injector.getInstance("mate.jdbc");
+        ManufacturerService manufacturerService
+                = (ManufacturerService) injector.getInstance(ManufacturerService.class);
+        DriverService driverService
+                = (DriverService) injector.getInstance(DriverService.class);
+
+        Manufacturer manufacturer = new Manufacturer(1L, "BMW", "Germany");
+        Driver driver = new Driver(1L,"Denys","EC-456789");
+
+        manufacturerService.create(manufacturer);
+        System.out.println("service.getAll() = " + manufacturerService.getAll());
+
+        driverService.create(driver);
+        System.out.println("driverService.getAll() = " + driverService.getAll());
     }
 }
