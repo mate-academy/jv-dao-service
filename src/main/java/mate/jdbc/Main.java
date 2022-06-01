7bef437023289964e5e8cbd494a98be11fd65ac8
Oleksandr Nesteruk <54321777tv@gmail.com>
2022-06-01 14:52:37 +0300

    Created dao and service for Taxi_Service

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,26 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerService manufacturerService
+                = (ManufacturerService) injector.getInstance(ManufacturerService.class);
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturer.setName("Reno");
+        manufacturer.setCountry("France");
+        System.out.println(manufacturerService.create(manufacturer));
+        DriverService driverService
+                = (DriverService) injector.getInstance(DriverService.class);
+        Driver driver = new Driver();
+        driver.setName("Bob");
+        driver.setLicenseNumber("1235");
+        System.out.println(driverService.create(driver));
     }
 }
