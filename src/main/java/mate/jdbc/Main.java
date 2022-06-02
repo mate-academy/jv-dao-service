6e0edb2a434ae05ff6d004ac35d29b9027363aad
Oleksandr Nesteruk <54321777tv@gmail.com>
2022-06-02 10:36:13 +0300

    Added tests for created methods to Main class

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -12,15 +12,32 @@ public class Main {
     public static void main(String[] args) {
         ManufacturerService manufacturerService
                 = (ManufacturerService) injector.getInstance(ManufacturerService.class);
-        Manufacturer manufacturer = new Manufacturer();
-        manufacturer.setName("Reno");
-        manufacturer.setCountry("France");
-        System.out.println(manufacturerService.create(manufacturer));
+        Manufacturer renoManufacturer = new Manufacturer();
+        renoManufacturer.setName("Reno");
+        renoManufacturer.setCountry("France");
+        System.out.println(manufacturerService.create(renoManufacturer));
+        System.out.println(manufacturerService.get(renoManufacturer.getId()));
+        Manufacturer kiaManufacturer = new Manufacturer();
+        kiaManufacturer.setId(renoManufacturer.getId());
+        kiaManufacturer.setName("Kia");
+        kiaManufacturer.setCountry("South Korea");
+        System.out.println(manufacturerService.update(kiaManufacturer));
+        System.out.println(manufacturerService.delete(renoManufacturer.getId()));
+        manufacturerService.getAll().forEach(System.out::println);
+
         DriverService driverService
                 = (DriverService) injector.getInstance(DriverService.class);
-        Driver driver = new Driver();
-        driver.setName("Bob");
-        driver.setLicenseNumber("1235");
-        System.out.println(driverService.create(driver));
+        Driver driverBob = new Driver();
+        driverBob.setName("Bob");
+        driverBob.setLicenseNumber("1235");
+        System.out.println(driverService.create(driverBob));
+        System.out.println(driverService.get(driverBob.getId()));
+        Driver driverAlex = new Driver();
+        driverAlex.setId(driverBob.getId());
+        driverAlex.setName("Alex");
+        driverAlex.setLicenseNumber("1236");
+        System.out.println(driverService.update(driverAlex));
+        System.out.println(driverService.delete(driverBob.getId()));
+        driverService.getAll().forEach(System.out::println);
     }
 }
