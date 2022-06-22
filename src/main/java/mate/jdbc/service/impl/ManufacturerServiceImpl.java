71688f4ec48e8701efe82660d3a2ac570b4bf25e
Andrey Chumak <chumakandrey7@gmail.com>
2022-06-22 18:29:40 +0300

    Created Driver class Created DriverService Created ManufacturerService and its implementation

---
+++ src/main/java/mate/jdbc/service/impl/ManufacturerServiceImpl.java
@@ -0,0 +1,46 @@
+package mate.jdbc.service.impl;
+
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.exception.DataProcessingException;
+import mate.jdbc.lib.Inject;
+import mate.jdbc.lib.Service;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.ManufacturerService;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+public class ManufacturerServiceImpl implements ManufacturerService {
+    @Inject
+    ManufacturerDao manufacturerDao;
+
+    @Override
+    public Manufacturer create(Manufacturer manufacturer) {
+        return manufacturerDao.create(manufacturer);
+    }
+
+    @Override
+    public Manufacturer get(Long id) {
+        Optional<Manufacturer> optionalManufacturer = manufacturerDao.get(id);
+        return optionalManufacturer.orElseThrow(() ->
+                new DataProcessingException("No such manufacturer with ID " + id,
+                        new NoSuchElementException()));
+    }
+
+    @Override
+    public List<Manufacturer> getAll() {
+        return manufacturerDao.getAll();
+    }
+
+    @Override
+    public Manufacturer update(Manufacturer manufacturer) {
+        return manufacturerDao.update(manufacturer);
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return manufacturerDao.delete(id);
+    }
+}
