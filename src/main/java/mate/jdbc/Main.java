09600662462dc048698f732cc1efaf87f7118078
Konstantin Miroshnichenko <bigfishnotbad@gmail.com>
2022-06-26 10:33:37 +0300

    methods of DriverDao interface were implemented

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,24 @@
 package mate.jdbc;
 
+import mate.jdbc.dao.DriverDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc.dao");
+
     public static void main(String[] args) {
-        // test your code here
+        DriverDao driverDao = (DriverDao) injector.getInstance(DriverDao.class);
+        Driver max = new Driver("mad_max", "no_license_never_was");
+        Driver alonso = new Driver("fernando", "McLaren#666");
+        max = driverDao.create(max);
+        alonso = driverDao.create(alonso);
+        driverDao.getAll().forEach(System.out::println);
+        System.lineSeparator();
+        alonso.setLicenseNumber("renault#777");
+        driverDao.update(alonso);
+        System.out.println(driverDao.get(alonso.getId()));
+        driverDao.delete(max.getId());
+        driverDao.getAll().forEach(System.out::println);
     }
 }
