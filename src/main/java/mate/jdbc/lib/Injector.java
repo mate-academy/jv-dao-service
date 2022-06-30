694d4f91526af35a47798bc928ef21ab23139531
Oleksii Mospan <alexey.mospan@gmail.com>
2022-06-30 15:18:00 +0300

    New Injector from lesson

--- src/main/java/mate/jdbc/lib/Injector.java
+++ src/main/java/mate/jdbc/lib/Injector.java
@@ -110,7 +110,6 @@ public class Injector {
             throw new RuntimeException("Can't set value to field ", e);
         }
     }
-
     /**
      * Scans all classes accessible from the context class loader which
      * belong to the given package and subpackages.
@@ -139,7 +138,6 @@ public class Injector {
         }
         return classes;
     }
-
     /**
      * Recursive method used to find all classes in a given directory and subdirs.
      *
