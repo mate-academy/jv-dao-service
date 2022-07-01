3018eac361229769fa70af27638a5e38fae21b6c
Oleksii Mospan <alexey.mospan@gmail.com>
2022-07-01 11:30:21 +0300

    Refactored code

--- src/main/java/mate/jdbc/dao/DriverDao.java
+++ src/main/java/mate/jdbc/dao/DriverDao.java
@@ -2,11 +2,9 @@ package mate.jdbc.dao;
 
 import java.util.List;
 import java.util.Optional;
-
 import mate.jdbc.model.Driver;
 
 public interface DriverDao {
-
     Driver create(Driver driver);
 
     Optional<Driver> get(Long id);
