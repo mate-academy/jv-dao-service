c6f37cb4806c9a9cfd6767df8b0a74a55d0a81a4
Tymur Tsvietkov <tymur.tsvietkov@gmail.com>
2022-07-04 19:49:13 +0300

    hw-dao-service

---
+++ src/main/java/mate/jdbc/service/impl/DriverServiceImpl.java
@@ -0,0 +1,44 @@
+package mate.jdbc.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+import mate.jdbc.dao.DriverDao;
+import mate.jdbc.lib.Inject;
+import mate.jdbc.lib.Service;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriverService;
+
+@Service
+public class DriverServiceImpl implements DriverService {
+    @Inject
+    private DriverDao driverDao;
+
+    @Override
+    public Driver create(Driver driver) {
+        return driverDao.create(driver);
+    }
+
+    @Override
+    public Driver get(Long id) {
+        Optional<Driver> driver = driverDao.get(id);
+        if (driver.isEmpty()) {
+            throw new RuntimeException("Can`t find driver with id: " + id);
+        }
+        return driver.get();
+    }
+
+    @Override
+    public List<Driver> getAll() {
+        return driverDao.getAll();
+    }
+
+    @Override
+    public Driver update(Driver driver) {
+        return driverDao.update(driver);
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return driverDao.delete(id);
+    }
+}
