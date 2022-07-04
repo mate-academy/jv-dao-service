4e3c0fcff52f5861d5bdb659b3d01f219f84e967
Mykola Yakymiv <mykola8426@gmail.com>
2022-07-04 17:46:59 +0300

    services and driverDao were implemented

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,22 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriverService;
+
 public class Main {
     public static void main(String[] args) {
-        // test your code here
+        Injector injector = Injector.getInstance("mate.jdbc");
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        Driver driver = new Driver(null, "Karl", "1421421");
+        System.out.println(driverService.create(driver));
+        driverService.getAll()
+                .stream()
+                .forEach(System.out::println);
+        System.out.println(driverService.get(2L));
+        driverService.delete(5L);
+        driverService.create(new Driver(null, "Melisa", "21121"));
+        driverService.update(new Driver(6L, "Mel", "0000"));
+
     }
 }
