f316173dbdc1b0dc88185a7fc1deb3fbdd01c12c
Yaroslav Lastivka <yaroslav0108@gmail.com>
2022-07-04 17:03:21 +0300

    Service layer was created, Driver model was created

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,40 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        Manufacturer ferrari = new Manufacturer(null,"Ferrari", "Italy");
+        Manufacturer ford = new Manufacturer(null,"Ford", "USA");
+        Manufacturer toyota = new Manufacturer(null, "Toyota", "Germany");
+        ManufacturerService manufacturerService =
+                (ManufacturerService) injector.getInstance(ManufacturerService.class);
+        manufacturerService.create(ferrari);
+        manufacturerService.create(toyota);
+        manufacturerService.create(ford);
+        System.out.println(manufacturerService.get(ferrari.getId()));
+        toyota.setCountry("Japan");
+        manufacturerService.update(toyota);
+        System.out.println(manufacturerService.delete(ferrari.getId()));
+        manufacturerService.getAll().forEach(System.out::println);
+        System.out.println("------------------------------------------------------");
+        Driver bob = new Driver(null, "Bob", "B13654424");
+        Driver alice = new Driver(null, "Alice", "A0002144");
+        Driver ted = new Driver(null, "Ted", "G544061739250");
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        driverService.create(bob);
+        driverService.create(alice);
+        driverService.create(ted);
+        System.out.println(driverService.get(bob.getId()));
+        ted.setLicenseNumber("A4552135");
+        driverService.update(ted);
+        System.out.println(driverService.delete(alice.getId()));
+        driverService.getAll().forEach(System.out::println);
     }
 }
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -2,8 +2,17 @@ CREATE SCHEMA IF NOT EXISTS `taxi_service` DEFAULT CHARACTER SET utf8;
 USE `taxi_service`;
 
 CREATE TABLE `manufacturers` (
-                                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                                        `id` BIGINT NOT NULL AUTO_INCREMENT,
                                         `name` VARCHAR(225) NOT NULL,
                                         `country` VARCHAR(225) NOT NULL,
                                         `is_deleted` TINYINT NOT NULL DEFAULT 0,
                                         PRIMARY KEY (`id`));
+
+CREATE TABLE `taxi_service`.`drivers` (
+                                          `id` BIGINT NOT NULL AUTO_INCREMENT,
+                                          `name` VARCHAR(255) NOT NULL,
+                                          `license_number` VARCHAR(225) NOT NULL,
+                                          `is_deleted` TINYINT NOT NULL DEFAULT 0,
+                                          PRIMARY KEY (`id`))
+    ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
