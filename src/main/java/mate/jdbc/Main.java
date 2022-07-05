17ead89e7cf07ae146359be480c902dbd6a83961
Dima Podakin <calombo.podakin@ukr.net>
2022-07-05 06:23:46 +0300

    created ManufacturerSrvice, DriverService and DriverDao

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,53 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerService manufacturerService = (ManufacturerService) injector
+                .getInstance(ManufacturerService.class);
+
+        Manufacturer bmw = new Manufacturer(null, "BMW", "Germany");
+        Manufacturer mustang = new Manufacturer(null, "Mustang", "USA");
+        Manufacturer audi = new Manufacturer(null, "Audi", "Germany");
+
+        manufacturerService.create(bmw);
+        manufacturerService.create(mustang);
+        manufacturerService.create(audi);
+        manufacturerService.getAll().forEach(System.out::println);
+
+        mustang.setCountry("Great Britain");
+
+        manufacturerService.update(mustang);
+        System.out.println(manufacturerService.get(mustang.getId()));
+
+        manufacturerService.delete(mustang.getId());
+        manufacturerService.getAll().forEach(System.out::println);
+
+        DriverService driverService = (DriverService) injector
+                .getInstance(DriverService.class);
+
+        Driver dima = new Driver(null, "Dima", "12345");
+        Driver bob = new Driver(null, "Bob", "67890");
+        Driver alice = new Driver(null, "Alice", "15973");
+
+        driverService.create(dima);
+        driverService.create(bob);
+        driverService.create(alice);
+        driverService.getAll().forEach(System.out::println);
+
+        dima.setLicenseNumber("00000");
+
+        driverService.update(dima);
+        System.out.println(driverService.get(dima.getId()));
+
+        driverService.delete(bob.getId());
+        driverService.getAll().forEach(System.out::println);
     }
 }
--- src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
+++ src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
@@ -18,7 +18,7 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
     @Override
     public Manufacturer create(Manufacturer manufacturer) {
         String query = "INSERT INTO manufacturers (name, country) "
-                + "VALUES (?, ?)";
+                + "VALUES (?, ?);";
         try (Connection connection = ConnectionUtil.getConnection();
                 PreparedStatement statement
                         = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
@@ -38,7 +38,7 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
     @Override
     public Optional<Manufacturer> get(Long id) {
         String query = "SELECT * FROM manufacturers"
-                + " WHERE id = ? AND is_deleted = FALSE";
+                + " WHERE id = ? AND is_deleted = FALSE;";
         try (Connection connection = ConnectionUtil.getConnection();
                 PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setLong(1, id);
@@ -55,7 +55,7 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
 
     @Override
     public List<Manufacturer> getAll() {
-        String query = "SELECT * FROM manufacturers WHERE is_deleted = FALSE";
+        String query = "SELECT * FROM manufacturers WHERE is_deleted = FALSE;";
         try (Connection connection = ConnectionUtil.getConnection();
                 PreparedStatement statement
                         = connection.prepareStatement(query)) {
