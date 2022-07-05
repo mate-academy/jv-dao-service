7bad3e8700ad2d0dd8d2e3ad4ce4fbe18c4cdae2
olyaklipa <olyaklipa@gmail.com>
2022-07-05 16:36:28 +0300

    DriverDao, DriverService and ManufacturerService were implemented

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,5 +1,5 @@
-CREATE SCHEMA IF NOT EXISTS `taxi_service` DEFAULT CHARACTER SET utf8;
-USE `taxi_service`;
+CREATE SCHEMA IF NOT EXISTS `taxi_db` DEFAULT CHARACTER SET utf8;
+USE `taxi_db`;
 
 CREATE TABLE `manufacturers` (
                                         `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
@@ -7,3 +7,11 @@ CREATE TABLE `manufacturers` (
                                         `country` VARCHAR(225) NOT NULL,
                                         `is_deleted` TINYINT NOT NULL DEFAULT 0,
                                         PRIMARY KEY (`id`));
+
+
+CREATE TABLE `drivers` (
+                                          `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                                          `name` VARCHAR(255) NOT NULL,
+                                          `licenseNumber` VARCHAR(255) NOT NULL,
+                                          `is_deleted` TINYINT NOT NULL DEFAULT 0,
+                                          PRIMARY KEY (`id`));
