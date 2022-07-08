7a6f85f8f1d5fa9e2b63e973249f91f826a478f6
drapail <mv.dibrov@gmail.com>
2022-07-08 09:37:41 +0300

    mentors notes implemented

--- src/main/java/mate/jdbc/service/impl/ManufacturerServiceImpl.java
+++ src/main/java/mate/jdbc/service/impl/ManufacturerServiceImpl.java
@@ -3,7 +3,6 @@ package mate.jdbc.service.impl;
 import java.util.List;
 import mate.jdbc.dao.ManufacturerDao;
 import mate.jdbc.lib.Inject;
-import mate.jdbc.lib.Injector;
 import mate.jdbc.lib.Service;
 import mate.jdbc.model.Manufacturer;
 import mate.jdbc.service.ManufacturerService;
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -4,7 +4,7 @@ create table drivers
 (
     id            bigint auto_increment primary key,
     name          varchar(255)         null,
-    licenseNumber varchar(255)         null,
+    license_number varchar(255)         null,
     is_deleted    tinyint(1) default 0 not null
 )
     charset = utf8mb4;
