64b751ec2b8f0b5f49563b3655d5c6a3142345a3
Maksym Lutovinov <90464472+maxlutovinov@users.noreply.github.com>
2022-07-08 15:28:35 +0300

    Change DB column name "licenseNumber" to "license_number"

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -11,7 +11,7 @@ CREATE TABLE `manufacturers` (
 CREATE TABLE `drivers` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
-  `licenseNumber` varchar(45) NOT NULL,
+  `license_number` varchar(45) NOT NULL,
   `is_deleted` tinyint NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
