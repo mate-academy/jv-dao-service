bff06fe8d5196ab473b1944ceeb0ac611bd63229
Serhii Davydov <serhii.davydov@gmail.com>
2022-08-10 17:38:42 +0300

    Add DriverService realisation

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,38 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriverService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        System.out.println("Create five drivers.");
+        Driver john = new Driver("John", "AAA-00001");
+        Driver kate = new Driver("Kate", "AAA-00002");
+        Driver pamela = new Driver("Pamela", "AAA-00003");
+        Driver steve = new Driver("Steve", "AAA-00004");
+        Driver adam = new Driver("Adam", "AAA-00005");
+        driverService.create(john);
+        driverService.create(kate);
+        driverService.create(pamela);
+        driverService.create(steve);
+        driverService.create(adam);
+        driverService.getAll().forEach(System.out::println);
+
+        System.out.println("Get driver with id: 1.");
+        System.out.println(driverService.get(1L));
+
+        System.out.println("Delete driver with id: 3");
+        System.out.println(driverService.delete(3L));
+        driverService.getAll().forEach(System.out::println);
+
+        System.out.println("Update driver with name: Steve - change license number.");
+        steve.setLicenseNumber("AAA-00006");
+        driverService.update(steve);
+        driverService.getAll().forEach(System.out::println);
+
     }
 }
