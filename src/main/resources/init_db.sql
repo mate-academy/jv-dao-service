ee338b756bd150d8b8f9823253931bbf7808cdc1
Benatti <ssunica0@gmail.com>
2022-08-18 16:05:54 +0300

    modify init_db file

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -9,9 +9,9 @@ CREATE TABLE `manufacturers` (
                                         PRIMARY KEY (`id`));
 
 CREATE TABLE `drivers` (
-                           `id` int NOT NULL AUTO_INCREMENT,
-                           `name` varchar(45) DEFAULT NULL,
-                           `license_number` varchar(45) DEFAULT NULL,
-                           `is_deleted` tinyint DEFAULT '0',
+                           `id` BIGINT NOT NULL AUTO_INCREMENT,
+                           `name` VARCHAR(225) NOT NULL DEFAULT NULL,
+                           `license_number` VARCHAR(225) NOT NULL DEFAULT NULL,
+                           `is_deleted` TINYINT NOT NULL DEFAULT '0',
                            PRIMARY KEY (`id`)
 
