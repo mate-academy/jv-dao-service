513e8e2497ff30de42c7f8b02b200d4df52cb863
Olesya Khalak <khalak.olesya.official@gmail.com>
2022-08-25 11:38:26 +0300

    the task is completed

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,47 @@
 package mate.jdbc;
 
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerDao manufacturerDao =
+                (ManufacturerDao) injector.getInstance(ManufacturerDao.class);
+        Manufacturer manufacturerAudi = new Manufacturer(null, "Nissan", "Japan");
+        Manufacturer manufacturerBmw = new Manufacturer(null, "Porsche", "Germany");
+        manufacturerAudi = manufacturerDao.create(manufacturerAudi);
+        manufacturerBmw = manufacturerDao.create(manufacturerBmw);
+        System.out.println("Added two manufacturers");
+        manufacturerDao.getAll().forEach(System.out::println);
+        manufacturerAudi.setCountry("Ukraine");
+        manufacturerDao.update(manufacturerAudi);
+        System.out.println("Updated audi manufacturer");
+        manufacturerDao.getAll().forEach(System.out::println);
+        manufacturerDao.delete(manufacturerBmw.getId());
+        System.out.println("Deleted BMW manufacturer");
+        manufacturerDao.getAll().forEach(System.out::println);
+        System.out.println("Got audi manufacturer");
+        System.out.println(manufacturerDao.get(manufacturerAudi.getId()));
+
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        Driver bobDriver = new Driver("Bob","1234567");
+        Driver alisaDriver = new Driver("Alisa", "7654321");
+        driverService.create(bobDriver);
+        driverService.create(alisaDriver);
+        driverService.getAll().forEach(System.out::println);
+        System.out.println(driverService.get(bobDriver.getId()));
+        Driver mikeDriver = new Driver();
+        mikeDriver.setId(alisaDriver.getId());
+        mikeDriver.setName("Mike");
+        mikeDriver.setLicenseNumber("34567");
+        driverService.update(mikeDriver);
+        driverService.delete(bobDriver.getId());
+        driverService.getAll().forEach(System.out::println);
     }
 }
+
--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -6,23 +6,23 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL = "YOUR DATABASE URL";
-    private static final String USERNAME = "YOUR USERNAME";
-    private static final String PASSWORD = "YOUR PASSWORD";
-    private static final String JDBC_DRIVER = "YOUR DRIVER";
-
     static {
         try {
-            Class.forName(JDBC_DRIVER);
+            Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
-            throw new RuntimeException("Can't find SQL Driver", e);
+            throw new RuntimeException("Can't load JDBC driver for MySQL", e);
         }
     }
 
-    public static Connection getConnection() throws SQLException {
-        Properties dbProperties = new Properties();
-        dbProperties.setProperty("user", USERNAME);
-        dbProperties.setProperty("password", PASSWORD);
-        return DriverManager.getConnection(URL, dbProperties);
+    public static Connection getConnection() {
+        try {
+            Properties dbProperties = new Properties();
+            dbProperties.put("user", "root");
+            dbProperties.put("password", "123456789");
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/taxi_service",
+                    dbProperties);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't create connection to DB", e);
+        }
     }
 }
