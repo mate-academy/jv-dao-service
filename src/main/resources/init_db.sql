2afb3f7999183f21d8faef4496362e0e88d8ecf8
AndriiP <andrey.pslk@gmail.com>
2022-08-26 20:37:04 +0300

    DriverDao, ManufacturerService, DriverService created and implemented. Appropriate annotations placed. Code tested in Main class.

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -7,3 +7,11 @@ CREATE TABLE `manufacturers` (
                                         `country` VARCHAR(225) NOT NULL,
                                         `is_deleted` TINYINT NOT NULL DEFAULT 0,
                                         PRIMARY KEY (`id`));
+
+CREATE TABLE `manufacturer` (
+                                `id` bigint NOT NULL AUTO_INCREMENT,
+                                `name` varchar(255) DEFAULT NULL,
+                                `country` varchar(255) DEFAULT NULL,
+                                `is_deleted` tinyint(1) DEFAULT '0',
+                                PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
