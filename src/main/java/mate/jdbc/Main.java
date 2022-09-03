dfa3bfa09aa3ae436c900151c99809beed6e219e
amarkelovUA <java.developer.aleksandr@gmail.com>
2022-09-03 13:13:23 +0300

    The constant with package name is created

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -6,8 +6,10 @@ import mate.jdbc.service.DriverService;
 import mate.jdbc.service.ManufacturerService;
 
 public class Main {
+    private static final String PACKAGE_NAME = "mate.jdbc";
+
     public static void main(String[] args) {
-        Injector injector = Injector.getInstance("mate.jdbc");
+        Injector injector = Injector.getInstance(PACKAGE_NAME);
         ManufacturerService manufacturerService =
                 (ManufacturerService) injector.getInstance(ManufacturerService.class);
         DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
