84da417682808a427408596fd63cdb2defb5eb19
Nazar <litvik.12@gmail.com>
2022-09-06 11:20:45 +0300

    deleted connection from main

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,18 +1,15 @@
 package mate.jdbc;
 
-import java.sql.SQLException;
 import mate.jdbc.lib.Injector;
 import mate.jdbc.model.Driver;
 import mate.jdbc.model.Manufacturer;
 import mate.jdbc.service.DriverService;
 import mate.jdbc.service.ManufacturerService;
-import mate.jdbc.util.ConnectionUtil;
 
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.jdbc");
 
-    public static void main(String[] args) throws SQLException {
-        ConnectionUtil.getConnection();
+    public static void main(String[] args) {
         ManufacturerService manufacturerService = (ManufacturerService) injector
                 .getInstance(ManufacturerService.class);
         Manufacturer nissan = new Manufacturer(null,"Nissan", "Japan");
