49b2e4f6e7a1a8707d9f0ba10914b9b671f8ea02
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-09-08 18:37:23 +0300

    Main test

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,32 @@
 package mate.jdbc;
 
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerService manufacturerService = (ManufacturerService)
+                injector.getInstance(ManufacturerService.class);
+        /////create/////
+        Manufacturer toyota = manufacturerService
+                .create(new Manufacturer(null, "Toyota", "Japa"));
+        System.out.println("savedManufacturer = " + toyota);
+        /////get/////
+        Manufacturer manufacturerToyota = manufacturerService.get(toyota.getId());
+        System.out.println("manufacturerToyota = " + manufacturerToyota);
+        /////update/////
+        Manufacturer updateToyota = new Manufacturer(toyota.getId(), "Toyota", "Japan");
+        Manufacturer update = manufacturerService.update(updateToyota);
+        System.out.println("update = " + update);
+        /////delete/////
+        boolean isDeletedToyota = manufacturerService.delete(toyota.getId());
+        System.out.println("deleteToyota = " + isDeletedToyota);
+        /////getAll/////
+        List<Manufacturer> manufacturers = manufacturerService.getAll();
+        manufacturers.forEach(System.out::println);
     }
 }
