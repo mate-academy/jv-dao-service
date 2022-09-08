64a97063dbddca2241e2d9777aae85ee15e8869c
Volodymyr Kachan <vladimirkachan1996@gmail.com>
2022-09-08 18:21:43 +0300

    my hw after student review

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -10,8 +10,8 @@ CREATE TABLE `manufacturers` (
 
 CREATE TABLE `drivers` (
                                  `id` BIGINT NOT NULL AUTO_INCREMENT,
-                                 `name` varchar(225) NOT NULL,
-                                 `country` varchar(225) NOT NULL,
+                                 `name` VARCHAR(225) NOT NULL,
+                                 `country` VARCHAR(225) NOT NULL,
                                  `is_deleted` TINYINT NOT NULL DEFAULT 0,
                                  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
