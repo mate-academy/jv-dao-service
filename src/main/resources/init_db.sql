679446d7176513859202be75a4b6c55afdd5f1f9
Yaroslav Shkvarla <slav.shkvarlo@gmail.com>
2022-09-08 20:42:28 +0300

    fix after student's review

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -9,9 +9,9 @@ CREATE TABLE `manufacturers` (
                                         PRIMARY KEY (`id`));
 
 CREATE TABLE `drivers` (
-                           `id` bigint NOT NULL AUTO_INCREMENT,
-                           `name` varchar(255) NOT NULL,
-                           `licenseNumber` varchar(255) NOT NULL,
-                           `is_deleted` tinyint NOT NULL DEFAULT '0',
+                           `id` BIGINT NOT NULL AUTO_INCREMENT,
+                           `name` VARCHAR(255) NOT NULL,
+                           `licenseNumber` VARCHAR(255) NOT NULL,
+                           `is_deleted` TUNYINT NOT NULL DEFAULT '0',
                            PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
