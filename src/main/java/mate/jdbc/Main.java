a5011b960a1bc582aa6861ba5c52d5212a610ecc
Andrii Nesterenko <nesterenkoandrii23@gmail.com>
2022-09-08 23:09:00 +0300

    first commit

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,35 @@
 package mate.jdbc;
 
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        Driver vitalii = new Driver(null,"Vitalii", "111511");
+        Driver igor = new Driver(null,"Igor", "201115");
+        driverService.create(vitalii);
+        driverService.create(igor);
+        driverService.getAll().forEach(System.out::println);
+
+        igor.setLicenseNumber("100150");
+        driverService.update(igor);
+        driverService.delete(igor.getId());
+
+        ManufacturerService manufacturerService = (ManufacturerService) injector
+                .getInstance(ManufacturerService.class);
+        Manufacturer alfaRomeo = new Manufacturer(null,"AlfaRomeo", "Italy");
+        Manufacturer citroen = new Manufacturer(null,"Citroen", "France");
+        manufacturerService.create(alfaRomeo);
+        manufacturerService.create(citroen);
+
+        manufacturerService.delete(citroen.getId());
+        manufacturerService.getAll().forEach(System.out::println);
     }
 }
