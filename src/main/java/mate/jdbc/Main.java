66a3862dce5eda21e794fe5ab82cd9e828ec3681
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-09-08 21:26:34 +0200

    implement injector, implement methods in ManufacturerServiceImpl

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,30 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerService manufecturerService = (ManufacturerService)
+                injector.getInstance(ManufacturerService.class);
+
+        System.out.println();
+        System.out.println("ALL DATA BASE:");
+        System.out.println();
+        manufecturerService.getAll().forEach(System.out::println);
+        System.out.println();
+        System.out.println("-------------------------------------------");
+        System.out.println();
+
+        Manufacturer mercedesBenz = new Manufacturer();
+        mercedesBenz.setName("Mercedes-Benz");
+        mercedesBenz.setCountry("Germany");
+        Manufacturer mercedesBenzToDataBase = manufecturerService.create(mercedesBenz);
+        System.out.println(mercedesBenz + " was add to DB");
+        System.out.println();
+        System.out.println("-------------------------------------------");
     }
 }
