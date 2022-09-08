ce2d78bb6835bae1f4e70220323387ef826be56a
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-09-08 19:18:51 +0200

    add ManufacturerService and its implementation

---
+++ src/main/java/mate/jdbc/service/ManufacturerService.java
@@ -0,0 +1,18 @@
+package mate.jdbc.service;
+
+import mate.jdbc.model.Manufacturer;
+
+import java.util.List;
+
+public interface ManufacturerService {
+
+    Manufacturer create(Manufacturer manufacturer);
+
+    Manufacturer get(Long id);
+
+    List<Manufacturer> getAll();
+
+    Manufacturer update(Manufacturer manufacturer);
+
+    boolean delete(Long id);
+}
