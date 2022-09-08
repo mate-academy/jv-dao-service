5d095c50c511afe581ff708647b8b28f91d0ed62
vadimkik2 <vadimkik@ukr.net>
2022-09-09 00:19:08 +0300

    Added new solution.

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -9,11 +9,14 @@ public class Main {
 
     public static void main(String[] args) {
         DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
-
-        Driver bob = new Driver("Bob","1234");
+        Driver bob = new Driver();
+        bob.setName("Bob");
+        bob.setLicenseNumber("1234");
         driverService.create(bob);
 
-        Driver john = new Driver("John","2345");
+        Driver john = new Driver();
+        john.setName("John");
+        john.setLicenseNumber("2345");
         driverService.create(john);
 
         Driver jack = new Driver("Jack","4567");
@@ -21,7 +24,6 @@ public class Main {
 
         driverService.delete(bob.getId());
         System.out.println(driverService.get(john.getId()));
-
         john.setLicenseNumber("3456");
         driverService.update(john);
         System.out.println(driverService.get(jack.getId()));
