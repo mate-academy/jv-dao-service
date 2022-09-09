96a31d7cdb151b7e59d0324c7579b833e3758308
mr-Claster <67782747+mr-Claster@users.noreply.github.com>
2022-09-09 15:44:34 +0300

    done my homework

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,50 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        System.out.println("Testing ManufacturerService: ");
+        ManufacturerService manufacturerService =
+                (ManufacturerService) injector.getInstance(ManufacturerService.class);
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturer.setCountry("Germany");
+        manufacturer.setName("Mazda");
+        manufacturer = manufacturerService.create(manufacturer);
+
+        System.out.println(manufacturerService.get(manufacturer.getId()));
+
+        manufacturer.setCountry("France");
+        manufacturer.setName("Reno");
+        System.out.println(manufacturerService.update(manufacturer));
+
+        manufacturerService.getAll().forEach(System.out::println);
+        manufacturerService.delete(manufacturer.getId());
+        manufacturerService.getAll().forEach(System.out::println);
+
+        System.out.println("\nTesting DriverService: ");
+        DriverService driverService =
+                (DriverService) injector.getInstance(DriverService.class);
+        Driver driver = new Driver();
+
+        driver.setLicenseNumber("234");
+        driver.setName("Bob");
+        driver = driverService.create(driver);
+
+        System.out.println(driverService.get(driver.getId()));
+
+        driver.setLicenseNumber("312");
+        driver.setName("Bill");
+        System.out.println(driverService.update(driver));
+
+        driverService.getAll().forEach(System.out::println);
+        manufacturerService.delete(driver.getId());
+        manufacturerService.getAll().forEach(System.out::println);
     }
 }
