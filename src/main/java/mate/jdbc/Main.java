b1dede4a339ed5cf08ec61e98840f2b61260659e
Roman Voitalik <voitalik1@gmail.com>
2022-09-09 13:09:56 +0300

    create class Driver implement CRUD methods in DriverDaoImpl update init_db.sql test DriverDao in Main class

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,21 @@
 package mate.jdbc;
 
+import mate.jdbc.dao.DriverDao;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+
 public class Main {
     public static void main(String[] args) {
-        // test your code here
+        Injector injector = Injector.getInstance("mate.jdbc");
+        DriverDao driverDao = (DriverDao) injector.getInstance(DriverDao.class);
+
+        Driver driver = new Driver(0L, "Driver 1", "111111111000");
+        driverDao.create(driver);
+        driverDao.getAll().forEach(System.out::println);
+        driver = new Driver(5L, "Driver 5", "111111111000");
+        driverDao.update(driver);
+        System.out.println(driverDao.delete(5L));
+        System.out.println(driverDao.get(6L));
+        System.out.println(driverDao.get(5L));
     }
 }
