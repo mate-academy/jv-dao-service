d1a90fc720660c42127c0178d7b1eba1da3b5374
Maksym Korkosh <makskorkosh@gmail.com>
2022-09-09 15:30:36 +0300

    fixed mistakes

--- src/main/java/mate/jdbc/model/Driver.java
+++ src/main/java/mate/jdbc/model/Driver.java
@@ -7,14 +7,18 @@ public class Driver {
     private String name;
     private String licenseNumber;
 
+    public Driver() {
+    }
+
     public Driver(String name, String licenseNumber) {
         this.name = name;
         this.licenseNumber = licenseNumber;
     }
 
     public Driver(Long id, String name, String licenseNumber) {
-        this(name, licenseNumber);
         this.id = id;
+        this.name = name;
+        this.licenseNumber = licenseNumber;
     }
 
     public Long getId() {
