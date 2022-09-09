9d97c6fab150306ce07673aa7de3c6b721e1510e
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-09-09 13:34:44 +0300

    refactor init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,5 +1,5 @@
-CREATE SCHEMA IF NOT EXISTS `taxi_service` DEFAULT CHARACTER SET utf8;
-USE `taxi_service`;
+CREATE SCHEMA IF NOT EXISTS `taxi_service_db` DEFAULT CHARACTER SET utf8;
+USE `taxi_service_db`;
 
 CREATE TABLE `manufacturers` (
 `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
@@ -9,8 +9,8 @@ CREATE TABLE `manufacturers` (
  PRIMARY KEY (`id`));
 
 CREATE TABLE `drivers` (
-`id` bigint NOT NULL AUTO_INCREMENT,
-`name` varchar(100) NOT NULL,
-`license_number` varchar(50) NOT NULL,
-`is_deleted` tinyint NOT NULL DEFAULT '0',
+`id` BIGINT NOT NULL AUTO_INCREMENT,
+`name` VARCHAR(100) NOT NULL,
+`license_number` VARCHAR(50) NOT NULL,
+`is_deleted` TINYINT NOT NULL DEFAULT '0',
 PRIMARY KEY (`id`));
