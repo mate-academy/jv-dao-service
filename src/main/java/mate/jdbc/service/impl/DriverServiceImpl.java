1946643da27534bf811396fe224bb1531659f662
Thousandfold <you@example.com>
2022-09-09 08:31:13 +0300

    create_model_dao_driver_empty_servises

---
+++ src/main/java/mate/jdbc/service/impl/DriverServiceImpl.java
@@ -0,0 +1,33 @@
+package mate.jdbc.service.impl;
+
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriverService;
+
+import java.util.List;
+
+public class DriverServiceImpl implements DriverService {
+    @Override
+    public Driver create(Driver driver) {
+        return null;
+    }
+
+    @Override
+    public Driver get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Driver> getAll() {
+        return null;
+    }
+
+    @Override
+    public Driver update(Driver driver) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+}
