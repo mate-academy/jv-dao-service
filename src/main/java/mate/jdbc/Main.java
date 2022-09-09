f0c00a1b87de400016dc794963f9268bec240671
Roman Voitalik <voitalik1@gmail.com>
2022-09-09 20:56:49 +0300

    create services and their implementations test service work in Main

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,21 +1,46 @@
 package mate.jdbc;
 
-import mate.jdbc.dao.DriverDao;
 import mate.jdbc.lib.Injector;
 import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
 
 public class Main {
-    public static void main(String[] args) {
-        Injector injector = Injector.getInstance("mate.jdbc");
-        DriverDao driverDao = (DriverDao) injector.getInstance(DriverDao.class);
+    private static Injector injector = Injector.getInstance("mate.jdbc");
 
-        Driver driver = new Driver(0L, "Driver 1", "111111111000");
-        driverDao.create(driver);
-        driverDao.getAll().forEach(System.out::println);
-        driver = new Driver(5L, "Driver 5", "111111111000");
-        driverDao.update(driver);
-        System.out.println(driverDao.delete(5L));
-        System.out.println(driverDao.get(6L));
-        System.out.println(driverDao.get(5L));
+    public static void main(String[] args) {
+        final DriverService driverService =
+                (DriverService) injector.getInstance(DriverService.class);
+        final ManufacturerService manufacturerService =
+                (ManufacturerService) injector.getInstance(ManufacturerService.class);
+
+        System.out.println("**************** TEST DRIVER SERVICE *******************");
+        Driver driver = new Driver(0L, "driverServiceDriver", "007");
+        System.out.println("**************** CREATE DRIVER *******************");
+        driverService.create(driver);
+        System.out.println("**************** GET ALL DRIVERS *******************");
+        driverService.getAll().forEach(System.out::println);
+        System.out.println("**************** GET ONE DRIVER *******************");
+        System.out.println(driverService.get(20L));
+        System.out.println("**************** UPDATE DRIVER *******************");
+        driver = driverService.update(new Driver(7L, "Updated driver", "NEW LICENSE"));
+        System.out.println(driver);
+        System.out.println("**************** DELETE DRIVER *******************");
+        System.out.println(driverService.delete(15L));
+        System.out.println("**************** TEST MANUFACTURER SERVICE *******************");
+        Manufacturer manufacturer = new Manufacturer(0L, "manufacturerServiceManufacturer", "007");
+        System.out.println("**************** CREATE MANUFACTURER *******************");
+        manufacturerService.create(manufacturer);
+        System.out.println("**************** GET ALL MANUFACTURERS *******************");
+        manufacturerService.getAll().forEach(System.out::println);
+        System.out.println("**************** GET ONE MANUFACTURER *******************");
+        System.out.println(manufacturerService.get(20L));
+        System.out.println("**************** UPDATE MANUFACTURER *******************");
+        manufacturer = manufacturerService.update(
+                new Manufacturer(7L, "Updated manufacturer", "NEW COUNTRY"));
+        System.out.println(manufacturer);
+        System.out.println("**************** DELETE MANUFACTURER *******************");
+        System.out.println(manufacturerService.delete(15L));
     }
 }
