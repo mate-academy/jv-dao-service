7a4b4735eb1ea85bc88d77d105fe5db6e36b198c
Veers <raadch3011@gmail.com>
2022-09-09 14:03:31 +0300

    complete jv-dao-service-task

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,9 +1,16 @@
 CREATE SCHEMA IF NOT EXISTS `taxi_service` DEFAULT CHARACTER SET utf8;
 USE `taxi_service`;
 
-CREATE TABLE `manufacturers` (
-                                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                        `name` VARCHAR(225) NOT NULL,
-                                        `country` VARCHAR(225) NOT NULL,
-                                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                        PRIMARY KEY (`id`));
+CREATE TABLE `manufacturer_db`.`drivers` (
+                                             `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                                             `name` VARCHAR(255) NULL,
+                                             `license_number` VARCHAR(255) NULL,
+                                             `is_deleted` TINYINT(1) NOT NULL DEFAULT 0,
+                                             PRIMARY KEY (`id`));
+
+CREATE TABLE `manufacturer_db`.`manufacturers` (
+                                                   `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                                                   `name` VARCHAR(255) NULL,
+                                                   `country` VARCHAR(255) NULL,
+                                                   `is_deleted` TINYINT(1) NOT NULL DEFAULT 0,
+                                                   PRIMARY KEY (`id`));
