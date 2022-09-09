99830738c5f2ab543ba77fa606390ca2c18e00f1
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-09-09 13:58:25 +0300

    cosmetic

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -3,11 +3,11 @@ USE `taxi_service`;
 
 DROP TABLE IF EXISTS `manufacturers`;
 CREATE TABLE `manufacturers` (
-                                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                        `name` VARCHAR(225) NOT NULL,
-                                        `country` VARCHAR(225) NOT NULL,
-                                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                        PRIMARY KEY (`id`));
+								`id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+								`name` VARCHAR(225) NOT NULL,
+								`country` VARCHAR(225) NOT NULL,
+								`is_deleted` TINYINT NOT NULL DEFAULT 0,
+								PRIMARY KEY (`id`));
 
 INSERT INTO `manufacturers` (`id`, `name`, `country`) VALUES
 (1,'Acura','Japan'),
@@ -24,11 +24,11 @@ INSERT INTO `manufacturers` (`id`, `name`, `country`) VALUES
 
 DROP TABLE IF EXISTS `drivers`;
 CREATE TABLE `drivers` (
-                                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                        `name` VARCHAR(225) NOT NULL,
-                                        `license_number` VARCHAR(225) NOT NULL,
-                                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                        PRIMARY KEY (`id`));
+						`id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+						`name` VARCHAR(225) NOT NULL,
+						`license_number` VARCHAR(225) NOT NULL,
+						`is_deleted` TINYINT NOT NULL DEFAULT 0,
+						PRIMARY KEY (`id`));
 
 INSERT INTO `drivers` (`id`, `name`, `license_number`) VALUES
 (1,'Ihor','123456789'),
