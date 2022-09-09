3e4e821a6a9e57aa75ff5fdba10e74f3bdd41bbd
Danylo Frolov <frolovdanilik@gmail.com>
2022-09-09 15:44:50 +0300

    solution

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -7,3 +7,11 @@ CREATE TABLE `manufacturers` (
                                         `country` VARCHAR(225) NOT NULL,
                                         `is_deleted` TINYINT NOT NULL DEFAULT 0,
                                         PRIMARY KEY (`id`));
+CREATE TABLE `drivers` (
+                           `id` bigint NOT NULL AUTO_INCREMENT,
+                           `name` varchar(45) DEFAULT NULL,
+                           `license_number` varchar(45) DEFAULT NULL,
+                           `is_deleted` TINYINT NOT NULL DEFAULT 0,
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
