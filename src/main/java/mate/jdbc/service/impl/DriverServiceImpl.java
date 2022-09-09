06160260a02a958c9908d7a9fb1430e25b6f8244
fliarkovskyi <fliarkovskyi@gmail.com>
2022-09-09 09:45:36 +0300

    fix checkstyle method get()

--- src/main/java/mate/jdbc/service/impl/DriverServiceImpl.java
+++ src/main/java/mate/jdbc/service/impl/DriverServiceImpl.java
@@ -19,7 +19,8 @@ public class DriverServiceImpl implements DriverService {
 
     @Override
     public Driver get(Long id) {
-        return driverDao.get(id).orElseThrow(() -> new RuntimeException("Can't get driver id " + id));
+        return driverDao.get(id).orElseThrow(() ->
+                new RuntimeException("Can't get driver id " + id));
     }
 
     @Override
--- src/main/java/mate/jdbc/service/impl/ManufacturerServiceImpl.java
+++ src/main/java/mate/jdbc/service/impl/ManufacturerServiceImpl.java
@@ -19,7 +19,8 @@ public class ManufacturerServiceImpl implements ManufacturerService {
 
     @Override
     public Manufacturer get(Long id) {
-        return manufacturerDao.get(id).orElseThrow(() -> new RuntimeException("Can't get driver id " + id));
+        return manufacturerDao.get(id).orElseThrow(() ->
+                new RuntimeException("Can't get driver id " + id));
     }
 
     @Override
