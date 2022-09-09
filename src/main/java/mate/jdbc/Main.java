3f49557ac257472bb8e5bcd423bd202c75a4e8d9
KosatskiyKirill <kirillkosatskiy@gmail.com>
2022-09-09 16:22:17 +0300

    home work dao service

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,65 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        final ManufacturerService manufacturerService = (ManufacturerService) injector
+                .getInstance(ManufacturerService.class);
+
+        Manufacturer astonMartin = new Manufacturer();
+
+        astonMartin.setName("Aston Martin");
+        astonMartin.setCountry("United Kingdom");
+
+        System.out.println("Create manufacturer");
+        System.out.println(manufacturerService.create(astonMartin));
+
+        System.out.println("Get manufacturer by id");
+        Manufacturer getManufacturer = manufacturerService.get(astonMartin.getId());
+        System.out.println(getManufacturer);
+
+        System.out.println("Get all manufacturer");
+        manufacturerService.getAll().forEach(System.out::println);
+
+        System.out.println("Update manufacturer");
+        astonMartin.setCountry("England");
+        System.out.println(manufacturerService.update(astonMartin));
+
+        System.out.println("Delete driver");
+        boolean deleteManufacturer = manufacturerService.delete(astonMartin.getId());
+        System.out.println(deleteManufacturer);
+
+        Driver valera = new Driver();
+
+        valera.setName("Valera");
+        valera.setLicenseNumber("007");
+
+        final DriverService driverService = (DriverService) injector
+                .getInstance(DriverService.class);
+
+        System.out.println("Create driver");
+        System.out.println(driverService.create(valera));
+
+        System.out.println("Get driver by id");
+        Driver getDriver = driverService.get(valera.getId());
+        System.out.println(getDriver);
+
+        System.out.println("Get all drivers");
+        driverService.getAll().forEach(System.out::println);
+
+        System.out.println("Update driver");
+        valera.setLicenseNumber("001");
+        System.out.println(driverService.update(valera));
+
+        System.out.println("Delete driver");
+        boolean deleteDriver = driverService.delete(valera.getId());
+        System.out.println(deleteDriver);
     }
 }
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -2,8 +2,16 @@ CREATE SCHEMA IF NOT EXISTS `taxi_service` DEFAULT CHARACTER SET utf8;
 USE `taxi_service`;
 
 CREATE TABLE `manufacturers` (
-                                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                        `name` VARCHAR(225) NOT NULL,
-                                        `country` VARCHAR(225) NOT NULL,
-                                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                        PRIMARY KEY (`id`));
+    `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(225) NOT NULL,
+    `country` VARCHAR(225) NOT NULL,
+    `is_deleted` TINYINT NOT NULL DEFAULT 0,
+    PRIMARY KEY (`id`));
+
+CREATE TABLE `taxi_service`.`drivers` (
+    `id` INT NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `licenseNumber` VARCHAR(255) NOT NULL,
+    `is_deleted` TINYINT NOT NULL DEFAULT 0,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
