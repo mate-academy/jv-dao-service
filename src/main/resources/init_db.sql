ad44b77af4e1cc2e593c7c5a481efe6ed13c28e0
Yevhen Kupriiuk <kupriuk@gmail.com>
2022-09-09 17:26:40 +0300

    mistakes are fixed 2

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -8,9 +8,9 @@ CREATE TABLE `manufacturers` (
                                         `is_deleted` TINYINT NOT NULL DEFAULT 0,
                                         PRIMARY KEY (`id`));
 CREATE TABLE `drivers` (
-                           `id` bigint NOT NULL AUTO_INCREMENT,
+                           `id` BIGINT NOT NULL AUTO_INCREMENT,
                            `name` varchar(255) NOT NULL,
                            `licenseNumber` varchar(255) NOT NULL,
-                           `is_deleted` tinyint NOT NULL DEFAULT '0',
+                           `is_deleted` TINYINT NOT NULL DEFAULT '0',
                            PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
