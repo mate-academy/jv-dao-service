b240c2eece1d1a50ae1804e8b37d8e1459de7550
Maksym Korkosh <makskorkosh@gmail.com>
2022-09-09 12:18:39 +0300

    added solution

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,37 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerService manufacturerService = (ManufacturerService) injector
+                .getInstance(ManufacturerService.class);
+        manufacturerService.getAll().forEach(System.out::println);
+        Manufacturer manufacturer = new Manufacturer("Nissan", "Japan");
+        manufacturerService.create(manufacturer);
+        manufacturer = new Manufacturer("Jeep", "Usa");
+        manufacturer = manufacturerService.create(manufacturer);
+        System.out.println(manufacturerService.get(manufacturer.getId()).toString());
+        manufacturer.setCountry(manufacturer.getCountry().toUpperCase());
+        System.out.println(manufacturerService.update(manufacturer).toString());
+        manufacturerService.delete(manufacturer.getId());
+
+        DriverService driverService =
+                (DriverService) injector.getInstance(DriverService.class);
+        driverService.getAll().forEach(System.out::println);
+        Driver driver = new Driver("Maks", "UA1111");
+        driverService.create(driver);
+        driver = new Driver("Masha", "UA2222");
+        driver = driverService.create(driver);
+        System.out.println(driverService.get(driver.getId()).toString());
+        driver.setName(driver.getName().toUpperCase());
+        System.out.println(driverService.update(driver).toString());
+        driverService.delete(driver.getId());
     }
 }
