e46dd91cf7c81fe3ee0366b3ff520838bd08b9ba
KosatskiyKirill <kirillkosatskiy@gmail.com>
2022-09-09 16:30:05 +0300

    fix mistakes

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -11,7 +11,7 @@ CREATE TABLE `manufacturers` (
 CREATE TABLE `taxi_service`.`drivers` (
     `id` INT NOT NULL,
     `name` VARCHAR(255) NOT NULL,
-    `licenseNumber` VARCHAR(255) NOT NULL,
+    `license_Number` VARCHAR(255) NOT NULL,
     `is_deleted` TINYINT NOT NULL DEFAULT 0,
     PRIMARY KEY (`id`),
     UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
