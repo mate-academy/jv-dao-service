ca848646ca1a6d5d2566d8ae42912ee61c9abfd5
azarovartur <99978155+azarovartur@users.noreply.github.com>
2022-09-11 22:38:31 +0300

    Update src/main/java/mate/jdbc/dao/impl/ManufacturerDaoImpl.java
    Co-authored-by: Anastasia Len <81781479+aaananas@users.noreply.github.com>

--- src/main/java/mate/jdbc/dao/impl/ManufacturerDaoImpl.java
+++ src/main/java/mate/jdbc/dao/impl/ManufacturerDaoImpl.java
@@ -107,10 +107,5 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
         Long id = resultSet.getObject("id", Long.class);
         String name = resultSet.getString("name");
         String country = resultSet.getString("country");
-        Manufacturer manufacturer = new Manufacturer();
-        manufacturer.setId(id);
-        manufacturer.setName(name);
-        manufacturer.setCountry(country);
-        return manufacturer;
     }
 }
