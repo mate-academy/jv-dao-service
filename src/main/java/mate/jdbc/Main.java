615931d9434c9754c3d40dddd6e5354d6431ba9d
Serhii Buria <sergeiburya@gmail.com>
2022-09-12 21:49:43 +0300

    objects of the class DriverService are created, the work of methods is checked

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,9 @@
 package mate.jdbc;
 
 import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
 import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
 import mate.jdbc.service.ManufacturerService;
 
 public class Main {
@@ -9,25 +11,34 @@ public class Main {
     public static void main(String[] args) {
         ManufacturerService manufacturerService =
                 (ManufacturerService) injector.getInstance(ManufacturerService.class);
-//        Manufacturer vw = new Manufacturer();
-//        vw.setName("VW");
-//        vw.setCountry("Germany");
-//
-//        Manufacturer honda = new Manufacturer();
-//        honda.setCountry("Japan");
-//        honda.setName("Honda");
-//
-//        manufacturerService.create(vw);
-//        manufacturerService.create(honda);
-//        System.out.println(manufacturerService.getAll());
-//        Manufacturer ford = new Manufacturer();
-//        ford.setName("Ford");
-//        ford.setCountry("America");
-//        manufacturerService.create(ford);
-//        ford.setCountry("USA");
-//        manufacturerService.update(ford);
-//        manufacturerService.get(ford.getId());
-//        manufacturerService.delete(ford.getId());
+        Manufacturer vw = new Manufacturer();
+        vw.setName("VW");
+        vw.setCountry("Germany");
 
+        Manufacturer honda = new Manufacturer();
+        honda.setCountry("Japan");
+        honda.setName("Honda");
+
+        manufacturerService.create(vw);
+        manufacturerService.create(honda);
+        System.out.println(manufacturerService.getAll());
+        Manufacturer ford = new Manufacturer();
+        ford.setName("Ford");
+        ford.setCountry("America");
+        manufacturerService.create(ford);
+        ford.setCountry("USA");
+        manufacturerService.update(ford);
+        manufacturerService.get(ford.getId());
+        manufacturerService.delete(ford.getId());
+
+        DriverService driverService =
+                (DriverService) injector.getInstance(DriverService.class);
+        System.out.println(driverService.getAll());
+        Driver ayrtonSenna  = new Driver("Ayrton Senna", "789");
+        driverService.create(ayrtonSenna);
+        driverService.get(ayrtonSenna.getId());
+        ayrtonSenna.setLicenseNumber("777");
+        driverService.update(ayrtonSenna);
+        driverService.delete(ayrtonSenna.getId());
     }
 }
