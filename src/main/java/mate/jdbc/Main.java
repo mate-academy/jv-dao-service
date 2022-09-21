a1654d3eac01a345b7002f66119977f870d01119
Anastasiia Chumak <nastya.ch@mate.academy>
2022-09-21 12:28:00 +0300

    adding service layer impl

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,19 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriverService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        Driver driver1 = new Driver(null, "John", "J182");
+        Driver driver2 = new Driver(null, "Bob", "b123");
+        DriverService manager = (DriverService) injector.getInstance(DriverService.class);
+        manager.create(driver1);
+        manager.create(driver2);
+        System.out.println("List of all drivers:");
+        manager.getAll().forEach(System.out::println);
     }
 }
