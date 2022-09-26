1dce8965664a25fb72cbc21865a1f03ba024d726
Alina Nikiforova <0500302717a@gmail.com>
2022-09-26 13:45:47 +0200

    added driver

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,27 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerService manufacturerService =
+                (ManufacturerService) injector.getInstance(ManufacturerService.class);
+        System.out.println(manufacturerService.delete(1L));
+        System.out.println(manufacturerService.create(new Manufacturer("Audi", "Germany")));
+        manufacturerService.getAll().forEach(System.out::println);
+
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        System.out.println(driverService.create(new Driver("Bob", "GT45896")));
+        System.out.println(driverService.create(new Driver("Alisa Yevtushenko", "GT89476")));
+        System.out.println(driverService.create(new Driver("Valerii Rudenko", "GT32459")));
+        System.out.println(driverService.update(new Driver(1L, "Bob Kobzar", "GT45896")));
+        System.out.println(driverService.delete(4L));
+        manufacturerService.getAll().forEach(System.out::println);
     }
 }
