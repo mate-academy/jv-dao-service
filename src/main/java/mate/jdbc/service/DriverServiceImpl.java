ba31074326b9d63bf65a41f3a20617863e69df74
Evhenii Vaskiv <evgenijvaskiv@gmail.com>
2022-10-13 02:45:13 +0300

    Exception check added to get method from service class

--- src/main/java/mate/jdbc/service/DriverServiceImpl.java
+++ src/main/java/mate/jdbc/service/DriverServiceImpl.java
@@ -1,6 +1,7 @@
 package mate.jdbc.service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import mate.jdbc.dao.DriverDao;
 import mate.jdbc.lib.Inject;
 import mate.jdbc.lib.Service;
@@ -18,7 +19,11 @@ public class DriverServiceImpl implements DriverService {
 
     @Override
     public Driver get(Long id) {
-        return driverDao.get(id).get();
+        try {
+            return driverDao.get(id).get();
+        } catch (NoSuchElementException e) {
+            throw new RuntimeException("There is no Driver by id: " + id, e);
+        }
     }
 
     @Override
