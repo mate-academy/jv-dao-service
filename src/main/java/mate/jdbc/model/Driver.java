8e2887c15b4d7e3dec291ae7bb311af2870415af
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-10-21 19:22:56 +0300

    Add Driver model

---
+++ src/main/java/mate/jdbc/model/Driver.java
@@ -0,0 +1,61 @@
+package mate.jdbc.model;
+
+import java.util.Objects;
+
+public class Driver {
+    private Long id;
+    private String name;
+    private String licenseNumber;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(final String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Driver driver = (Driver) o;
+        if (!Objects.equals(id, driver.id)) return false;
+        if (!Objects.equals(name, driver.name)) return false;
+        return Objects.equals(licenseNumber, driver.licenseNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (licenseNumber != null ? licenseNumber.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Driver{");
+        sb.append("id=").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", licenseNumber='").append(licenseNumber).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
