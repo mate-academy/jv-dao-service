d311d015c0ff3876c32c8f68ae2e14c6d48919b0
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-10-21 20:12:55 +0300

    Implement Driver Service

--- src/main/java/mate/jdbc/service/DriverServiceImpl.java
+++ src/main/java/mate/jdbc/service/DriverServiceImpl.java
@@ -1,31 +1,34 @@
 package mate.jdbc.service;
 
+import mate.jdbc.dao.DriverDao;
 import mate.jdbc.model.Driver;
 import java.util.List;
 
 public class DriverServiceImpl implements DriverService {
+    DriverDao driverDao;
+
     @Override
     public Driver create(final Driver driver) {
-        return null;
+        return driverDao.create(driver);
     }
 
     @Override
     public Driver get(final Long id) {
-        return null;
+        return driverDao.get(id).get();
     }
 
     @Override
     public List<Driver> getAll() {
-        return null;
+        return driverDao.getAll();
     }
 
     @Override
     public Driver update(final Driver driver) {
-        return null;
+        return driverDao.update(driver);
     }
 
     @Override
     public boolean delete(final Long id) {
-        return false;
+        return driverDao.delete(id);
     }
 }
