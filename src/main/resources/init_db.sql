9b17a52dbc90dac8b52add2710b33f114646e10b
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-10-21 20:19:57 +0300

    Update init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -7,3 +7,13 @@ CREATE TABLE `manufacturers` (
                                         `country` VARCHAR(225) NOT NULL,
                                         `is_deleted` TINYINT NOT NULL DEFAULT 0,
                                         PRIMARY KEY (`id`));
+
+CREATE TABLE `drivers` (
+                                          `id` bigint NOT NULL AUTO_INCREMENT,
+                                          `name` varchar(45) DEFAULT NULL,
+                                          `license_number` bigint DEFAULT NULL,
+                                          `is_deleted` tinyint NOT NULL DEFAULT '0',
+                                          PRIMARY KEY (`id`)
+                                        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
