c60ef5f99437aca25013aff34566dd4b4ad06a3a
shmax <mshveda28@gmail.com>
2022-11-01 17:32:17 +0200

    Create Driver entity class

---
+++ src/main/java/mate/jdbc/model/Driver.java
@@ -0,0 +1,77 @@
+package mate.jdbc.model;
+
+import java.util.Objects;
+
+public class Driver {
+    private Long id;
+    private String name;
+    private String licenseNumber;
+
+    public Driver(Long id, String name, String licenseNumber) {
+        this.id = id;
+        this.name = name;
+        this.licenseNumber = licenseNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Driver setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Driver setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public Driver setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Driver driver = (Driver) o;
+        if (!id.equals(driver.id)) {
+            return false;
+        }
+        if (!Objects.equals(name, driver.name)) {
+            return false;
+        }
+        return Objects.equals(licenseNumber, driver.licenseNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (licenseNumber != null ? licenseNumber.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Driver{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + ", licenseNumber='" + licenseNumber + '\''
+                + '}';
+    }
+}
