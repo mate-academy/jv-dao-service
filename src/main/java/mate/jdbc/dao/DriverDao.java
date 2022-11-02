0fd13d2febd2730afeaaaf00ff718c16194359a7
shmax <mshveda28@gmail.com>
2022-11-02 16:42:13 +0200

    Add generic services to reduce code duplications

--- src/main/java/mate/jdbc/dao/DriverDao.java
+++ src/main/java/mate/jdbc/dao/DriverDao.java
@@ -1,17 +1,5 @@
 package mate.jdbc.dao;
 
-import java.util.List;
-import java.util.Optional;
 import mate.jdbc.model.Driver;
 
-public interface DriverDao {
-    Driver create(Driver manufacturer);
-
-    Optional<Driver> get(Long id);
-
-    List<Driver> getAll();
-
-    Driver update(Driver manufacturer);
-
-    boolean delete(Long id);
-}
+public interface DriverDao extends GenericDao<Driver> { }
