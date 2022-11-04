33987f4833b1920fb392d612cc299808cd794800
Yura Hryniv <superyura444@gmail.com>
2022-11-04 13:06:39 +0200

    Added more tests to the main

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -6,6 +6,8 @@ import mate.jdbc.model.Manufacturer;
 import mate.jdbc.service.DriverService;
 import mate.jdbc.service.ManufacturerService;
 
+import java.util.List;
+
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.jdbc");
 
@@ -13,7 +15,27 @@ public class Main {
         DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
         ManufacturerService manufacturerService =
                 (ManufacturerService) injector.getInstance(ManufacturerService.class);
+/*
+        //Driver test
         driverService.create(new Driver(11L, "Afanasij", "2150120"));
-        manufacturerService.create(new Manufacturer(15L, "Lada", "Russia"));
+        driverService.get(3L);
+        driverService.delete(5L);
+        List<Driver> all = driverService.getAll();
+        for (Driver driver: all) {
+            System.out.println(driver);
+        }
+        driverService.update(new Driver(1L, "Anton", "0235987"));
+
+
+ */
+        //Manufacturer test
+        manufacturerService.create(new Manufacturer("Tesla", "Germany"));
+        manufacturerService.get(1L);
+        manufacturerService.delete(5L);
+        for (Manufacturer manufacturer : manufacturerService.getAll()) {
+            System.out.println(manufacturer);
+        }
+        manufacturerService.update(new Manufacturer(1L, "Opel", "Germany"));
+
     }
 }
