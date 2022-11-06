e187f1a8486a60719b6813c3bfc73eefb3745017
Anton Pavliuk <anton.pavliuk20@gmail.com>
2022-11-06 15:03:53 +0200

    Correct SQL table name

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -8,7 +8,7 @@ CREATE TABLE `manufacturers` (
                                         `is_deleted` TINYINT NOT NULL DEFAULT 0,
                                         PRIMARY KEY (`id`));
 
-CREATE TABLE `manufacturers` (
+CREATE TABLE `drivers` (
                                         `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
                                         `name` VARCHAR(225) NOT NULL,
                                         `license_number` VARCHAR(225) NOT NULL,
