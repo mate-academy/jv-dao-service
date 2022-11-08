bd76781d07e8889337e981b6f8092274d3435067
galenkoigor <galenkoigor@gmail.com>
2022-11-08 17:32:42 +0100

    Made essential structure

---
+++ src/main/java/mate/jdbc/dao/DriverDaoImpl.java
@@ -0,0 +1,34 @@
+package mate.jdbc.dao;
+
+import mate.jdbc.lib.Dao;
+import mate.jdbc.model.Driver;
+
+import java.util.List;
+
+@Dao
+public class DriverDaoImpl implements DriverDao {
+    @Override
+    public Driver create(Driver driver) {
+        return null;
+    }
+
+    @Override
+    public Driver get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Driver> getAll() {
+        return null;
+    }
+
+    @Override
+    public Driver update(Driver driver) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+}
