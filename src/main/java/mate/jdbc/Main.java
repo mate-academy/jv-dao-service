2137b5ec06a019e348c1b994ae831572e1f0927d
galenkoigor <galenkoigor@gmail.com>
2022-11-08 19:10:20 +0100

    Developed one more part of taxi service assignment

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,20 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriverService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        Driver driver = new Driver(null, "Dominic", "MA-753159");
+        driverService.create(driver);
+        System.out.println(driverService.get(1L));
+        driver.setLicenseNumber("MA-987654321");
+        driverService.update(driver);
+        driverService.getAll().forEach(System.out::println);
+        driverService.delete(1L);
     }
 }
