4f17ddb112a420e6742135e9d0056246dab1d4ff
Andrii Matveev <matveev.tesla@gmail.com>
2022-11-09 14:34:34 +0200

    homework has been done

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,20 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriverService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        Driver driverBob = new Driver(null, "Bob", "1234");
+        Driver driverALice = new Driver(1L, "ALice", "5678");
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        //driverService.create(driverBob);
+        driverService.get(1L);
+        driverService.update(driverALice);
+        driverService.getAll();
+        driverService.delete(1L);
     }
 }
