f33d00d92fb0f24df81f25d21bcf62812e4d2a40
MuzykaVP <muzykavp.work@gmail.com>
2022-11-13 22:10:29 +0200

    Manufacturer service was implemented, Driver model, dao and service was implemented

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -2,8 +2,18 @@ CREATE SCHEMA IF NOT EXISTS `taxi_service` DEFAULT CHARACTER SET utf8;
 USE `taxi_service`;
 
 CREATE TABLE `manufacturers` (
-                                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                        `name` VARCHAR(225) NOT NULL,
-                                        `country` VARCHAR(225) NOT NULL,
-                                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                        PRIMARY KEY (`id`));
+                                 `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                                 `name` VARCHAR(225) NOT NULL,
+                                 `country` VARCHAR(225) NOT NULL,
+                                 `is_deleted` TINYINT NOT NULL DEFAULT 0,
+                                 PRIMARY KEY (`id`));
+
+CREATE TABLE `drivers` (
+                           `id` bigint NOT NULL AUTO_INCREMENT,
+                           `name` varchar(255) NOT NULL,
+                           `license_number` varchar(255) NOT NULL,
+                           `is_deleted` tinyint NOT NULL DEFAULT '0',
+                           PRIMARY KEY (`id`),
+                           UNIQUE KEY `id_UNIQUE` (`id`),
+                           UNIQUE KEY `license_number_UNIQUE` (`license_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
