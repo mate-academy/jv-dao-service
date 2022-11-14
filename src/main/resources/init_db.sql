d32ff538150da2d9f9f17a4193b777a0f17c18d9
Sidir0m <romangolovatyi@yahoo.com>
2022-11-14 22:08:09 +0200

    Add db connections. Create new model in db

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -7,3 +7,12 @@ CREATE TABLE `manufacturers` (
                                         `country` VARCHAR(225) NOT NULL,
                                         `is_deleted` TINYINT NOT NULL DEFAULT 0,
                                         PRIMARY KEY (`id`));
+
+CREATE TABLE `drivers` (
+                           `id` bigint NOT NULL,
+                           `name` varchar(255) NOT NULL,
+                           `license_number` varchar(255) NOT NULL,
+                           `is_deleted` tinyint NOT NULL DEFAULT '0',
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
