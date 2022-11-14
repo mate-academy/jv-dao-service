c2b9ea4c8d59861db797cc4f39fe75d883f63c05
Anastasiia Romanova <ana.roma.vi@gmail.com>
2022-11-14 21:17:46 +0200

    implemented ManufacturerService, DriverService interfaces; creted dao layer for Driver class

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,29 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerService manufacturerService = (ManufacturerService)
+                injector.getInstance(ManufacturerService.class);
+        DriverService driverService = (DriverService)
+                injector.getInstance(DriverService.class);
+        System.out.println(manufacturerService.get(16L));
+        manufacturerService.delete(16L);
+        Driver savedDriverBob = driverService.create(new Driver("Bob", "AB12345"));
+        System.out.println(driverService.get(savedDriverBob.getId()));
+        Driver driverNastya = new Driver("Nastya", "AB09876");
+        Driver savedDriverNastya =
+                driverService.getByLicenseNumber(driverNastya.getLicenseNumber());
+        savedDriverNastya.setLicenseNumber("BA09876");
+        System.out.println(driverService.update(savedDriverNastya));
+        driverService.delete(savedDriverNastya.getId());
+        System.out.println(driverService.getAll());
+        System.out.println(manufacturerService.getAll());
     }
 }
---
+++ src/main/java/mate/jdbc/dao/DriverDao.java
@@ -0,0 +1,19 @@
+package mate.jdbc.dao;
+
+import java.util.List;
+import java.util.Optional;
+import mate.jdbc.model.Driver;
+
+public interface DriverDao {
+    Driver create(Driver driver);
+
+    Optional<Driver> get(Long id);
+
+    Optional<Driver> getByLicenseNumber(String licenseNumber);
+
+    List<Driver> getAll();
+
+    Driver update(Driver driver);
+
+    boolean delete(Long id);
+}
