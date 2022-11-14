11adbd6307dbc37acd948734f06d1fc4ae20dfe3
Sidir0m <romangolovatyi@yahoo.com>
2022-11-14 23:45:20 +0200

    Refactored db

---
+++ src/main/java/mate/jdbc/service/impl/ManufacturerServiceImpl.java
@@ -0,0 +1,32 @@
+package mate.jdbc.service;
+
+import mate.jdbc.model.Manufacturer;
+
+import java.util.List;
+
+public class ManufacturerServiceImpl implements ManufacturerService {
+    @Override
+    public Manufacturer create(Manufacturer manufacturer) {
+        return null;
+    }
+
+    @Override
+    public Manufacturer get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Manufacturer> getAll() {
+        return null;
+    }
+
+    @Override
+    public Manufacturer update(Manufacturer manufacturer) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+}
