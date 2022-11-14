d082c36c0429ffcf7ba19a2a2168dca5e2a1cedd
AlinaOryshak <alinaoryshak@gmail.com>
2022-11-14 17:04:38 +0200

    Remove hardcoding

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -14,27 +14,27 @@ public class Main {
                 .getInstance(ManufacturerService.class);
         System.out.println("All manufacturers:");
         manufacturerService.getAll().stream().forEach(System.out::println);
-        Manufacturer newManufacturer = manufacturerService
-                .create(new Manufacturer(0L, "Bentley", "England"));
+        Manufacturer manufacturer = new Manufacturer(0L, "Bentley", "England");
+        Manufacturer newManufacturer = manufacturerService.create(manufacturer);
         System.out.println("New manufacturer: " + System.lineSeparator()
                 + manufacturerService.get(newManufacturer.getId()));
-        Manufacturer updatedManufacturer = manufacturerService
-                .update(new Manufacturer(5L, "BMW", "Estonia"));
+        Manufacturer manufacturerToUpdate = new Manufacturer(5L, "BMW", "Estonia");
+        Manufacturer updatedManufacturer = manufacturerService.update(manufacturerToUpdate);
         System.out.println("Updated manufacturer: " + System.lineSeparator()
                 + manufacturerService.get(updatedManufacturer.getId()));
-        System.out.println("Deletion: " + manufacturerService.delete(5L));
+        System.out.println("Deletion: " + manufacturerService.delete(updatedManufacturer.getId()));
 
         DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
         System.out.println("All drivers:");
         driverService.getAll().stream().forEach(System.out::println);
-        Driver newDriver = driverService
-                .create(new Driver(3L, "Krasnov Mark", "987654"));
+        Driver driver = new Driver(3L, "Krasnov Mark", "987654");
+        Driver newDriver = driverService.create(driver);
         System.out.println("New driver: " + System.lineSeparator()
                 + driverService.get(newDriver.getId()));
-        Driver updatedDriver = driverService
-                .update(new Driver(3L, "Krasnov Alex", "987654"));
+        Driver driverToUpdete = new Driver(3L, "Krasnov Alex", "987654");
+        Driver updatedDriver = driverService.update(driverToUpdete);
         System.out.println("Updated driver: " + System.lineSeparator()
                 + driverService.get(updatedDriver.getId()));
-        System.out.println("Deletion: " + driverService.delete(3L));
+        System.out.println("Deletion: " + driverService.delete(driverToUpdete.getId()));
     }
 }
