693e427a49552d07a98003f9f2ba15882129257a
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2022-11-14 17:39:55 +0200

    created ConnectionUtil

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,10 @@
 package mate.jdbc;
 
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.dao.ManufacturerDaoImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // test your code here
+
     }
 }
