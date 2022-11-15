4e15c7b593c1138b3b19c8c0662e561f6fc75968
thebitler <yurchyknikita@gmail.com>
2022-11-15 13:02:20 +0100

    Implemented N-tier architecture

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,9 +1,52 @@
-CREATE SCHEMA IF NOT EXISTS `taxi_service` DEFAULT CHARACTER SET utf8;
-USE `taxi_service`;
+-- Database: postgres
 
-CREATE TABLE `manufacturers` (
-                                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                        `name` VARCHAR(225) NOT NULL,
-                                        `country` VARCHAR(225) NOT NULL,
-                                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                        PRIMARY KEY (`id`));
+-- DROP DATABASE IF EXISTS postgres;
+
+CREATE DATABASE postgres
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+COMMENT ON DATABASE postgres
+    IS 'default administrative connection database';
+
+-- Table: public.manufacturers
+
+-- DROP TABLE IF EXISTS public.manufacturers;
+
+CREATE TABLE IF NOT EXISTS public.manufacturers
+(
+    id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
+    name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    country character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    is_deleted boolean NOT NULL DEFAULT false,
+    CONSTRAINT "manufacturers _pkey" PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.manufacturers
+    OWNER to postgres;
+
+-- Table: public.drivers
+
+-- DROP TABLE IF EXISTS public.drivers;
+
+CREATE TABLE IF NOT EXISTS public.drivers
+(
+    id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
+    name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    license_number character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    is_deleted boolean NOT NULL DEFAULT false,
+    CONSTRAINT drivers_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.drivers
+    OWNER to postgres;
