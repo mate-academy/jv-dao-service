6b0fa9f241a336b3ba3f8471bfaa54eeb2e33523
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2022-11-15 10:33:47 +0200

    created class DriverDaoImpl

---
+++ src/main/java/mate/jdbc/service/ManufacturerServiceImpl.java
@@ -0,0 +1,33 @@
+package mate.jdbc.service;
+
+import mate.jdbc.model.Manufacturer;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ManufacturerServiceImpl implements ManufacturerService{
+    @Override
+    public Manufacturer create(Manufacturer manufacturer) {
+        return null;
+    }
+
+    @Override
+    public Optional<Manufacturer> get(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Manufacturer> getAll() {
+        return null;
+    }
+
+    @Override
+    public Manufacturer update(Manufacturer manufacturer) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+}
