1a80892d5047b81a5a427e00a76c78313964407d
Oleksandr <oleksandrkorniychuk98@gmail.com>
2022-11-15 13:19:39 +0200

    fixed get to orEleThrow

--- src/main/java/mate/jdbc/service/impl/ManufacturerServiceImpl.java
+++ src/main/java/mate/jdbc/service/impl/ManufacturerServiceImpl.java
@@ -1,6 +1,8 @@
 package mate.jdbc.service.impl;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+
 import mate.jdbc.dao.ManufacturerDao;
 import mate.jdbc.lib.Inject;
 import mate.jdbc.lib.Service;
@@ -19,7 +21,10 @@ public class ManufacturerServiceImpl implements ManufacturerService {
 
     @Override
     public Manufacturer get(Long id) {
-        return manufacturerDao.get(id).get();
+        return manufacturerDao.get(id).orElseThrow(
+                () ->  new NoSuchElementException("Could not get manufacturer "
+                        + "by id = " + id)
+        );
     }
 
     @Override
