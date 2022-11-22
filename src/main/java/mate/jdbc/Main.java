25975ce49e23513295fca5e21a463c6cc54bb991
DY-JS <delo1604@ukr.net>
2022-11-22 14:47:41 +0200

    Solution jdbc-dao-service

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,20 @@
 package mate.jdbc;
 
+import mate.jdbc.Service.DriverService;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+
 public class Main {
+    private static Injector injector = Injector.getInstance("mate.jdbc");
     public static void main(String[] args) {
-        // test your code here
+        DriverService driverService =
+                (DriverService) injector.getInstance(DriverService.class);
+        Driver sam = new Driver("Sam", "999");
+        driverService.create(sam);
+        Driver ivan = new Driver("ivan", "889");
+        driverService.create(ivan);
+        System.out.println(driverService.get(3L));
+        driverService.delete(ivan.getId());
+        driverService.getAll().stream().forEach(System.out::println);
     }
 }
---
+++ src/main/java/mate/jdbc/Service/DriverServiceImpl.java
@@ -0,0 +1,40 @@
+package mate.jdbc.Service;
+
+import mate.jdbc.dao.DriverDao;
+import mate.jdbc.lib.Inject;
+import mate.jdbc.lib.Service;
+import mate.jdbc.model.Driver;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DriverServiceImpl implements DriverService {
+    @Inject
+    DriverDao driverDao;
+
+    @Override
+    public Driver create(Driver driver) {
+        return driverDao.create(driver);
+    }
+
+    @Override
+    public Optional<Driver> get(Long id) {
+        return driverDao.get(id);
+    }
+
+    @Override
+    public List<Driver> getAll() {
+        return driverDao.getAll();
+    }
+
+    @Override
+    public Driver update(Driver driver) {
+        return driverDao.update(driver);
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return driverDao.delete(id);
+    }
+}
