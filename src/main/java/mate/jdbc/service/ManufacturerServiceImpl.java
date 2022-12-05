433f5c8b1eea118f0e60bc991aa3c406263843d3
AndriySichniy <sichniy.andriy@gmail.com>
2022-12-05 13:11:36 +0200

    added logic in ManufacturerServiceImpl

--- src/main/java/mate/jdbc/service/ManufacturerServiceImpl.java
+++ src/main/java/mate/jdbc/service/ManufacturerServiceImpl.java
@@ -1,31 +1,38 @@
 package mate.jdbc.service;
 
 import java.util.List;
+import java.util.Optional;
+import mate.jdbc.dao.ManufacturerDaoImpl;
+import mate.jdbc.lib.Inject;
+import mate.jdbc.lib.Service;
 import mate.jdbc.model.Manufacturer;
 
+@Service
 public class ManufacturerServiceImpl implements ManufacturerService {
+    @Inject
+    ManufacturerDaoImpl connectToManufacturersTable;
     @Override
     public Manufacturer create(Manufacturer manufacturer) {
-        return null;
+        return connectToManufacturersTable.create(manufacturer);
     }
 
     @Override
     public Manufacturer get(Long id) {
-        return null;
+        return connectToManufacturersTable.get(id).orElse(null);
     }
 
     @Override
     public List<Manufacturer> getAll() {
-        return null;
+        return connectToManufacturersTable.getAll();
     }
 
     @Override
     public Manufacturer update(Manufacturer manufacturer) {
-        return null;
+        return connectToManufacturersTable.update(manufacturer);
     }
 
     @Override
     public boolean delete(Long id) {
-        return false;
+        return connectToManufacturersTable.delete(id);
     }
 }
