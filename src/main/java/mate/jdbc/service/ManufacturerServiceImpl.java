8456f8969cd80e96b8353cea7bea7634ddc425e8
AndriySichniy <sichniy.andriy@gmail.com>
2022-12-05 20:58:29 +0200

    fix ManufacturerServiceImpl

--- src/main/java/mate/jdbc/service/ManufacturerServiceImpl.java
+++ src/main/java/mate/jdbc/service/ManufacturerServiceImpl.java
@@ -2,7 +2,6 @@ package mate.jdbc.service;
 
 import java.util.List;
 import mate.jdbc.dao.ManufacturerDao;
-import mate.jdbc.dao.ManufacturerDaoImpl;
 import mate.jdbc.lib.Inject;
 import mate.jdbc.lib.Service;
 import mate.jdbc.model.Manufacturer;
@@ -14,26 +13,26 @@ public class ManufacturerServiceImpl implements ManufacturerService {
 
     @Override
     public Manufacturer create(Manufacturer manufacturer) {
-        return connectToManufacturersTable.create(manufacturer);
+        return manufacturerDao.create(manufacturer);
     }
 
     @Override
     public Manufacturer get(Long id) {
-        return connectToManufacturersTable.get(id).orElse(null);
+        return manufacturerDao.get(id).orElse(null);
     }
 
     @Override
     public List<Manufacturer> getAll() {
-        return connectToManufacturersTable.getAll();
+        return manufacturerDao.getAll();
     }
 
     @Override
     public Manufacturer update(Manufacturer manufacturer) {
-        return connectToManufacturersTable.update(manufacturer);
+        return manufacturerDao.update(manufacturer);
     }
 
     @Override
     public boolean delete(Long id) {
-        return connectToManufacturersTable.delete(id);
+        return manufacturerDao.delete(id);
     }
 }
