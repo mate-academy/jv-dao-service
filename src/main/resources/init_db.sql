a7f55a4b2958cc3e1e389ee48700bf8c9146c857
Anton Kalenskyi <anton_kalenskiy@ukr.net>
2022-12-28 10:56:55 +0200

    Fixed Driver get() and Manufacturer get() methods.

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -11,7 +11,7 @@ CREATE TABLE `manufacturers` (
 CREATE TABLE `drivers` (
                                         `id` bigint NOT NULL AUTO_INCREMENT,
                                         `name` varchar(225) NOT NULL,
-                                        `licenseNumber` varchar(225) NOT NULL,
+                                        `license_number` varchar(225) NOT NULL,
                                         `is_deleted` tinyint NOT NULL DEFAULT '0',
                                          PRIMARY KEY (`id`)
                                          ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
