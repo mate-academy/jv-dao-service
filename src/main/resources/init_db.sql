7d7d57c6edb831ad94198561deec032214775e3e
Ivan Vovnyanko <mooooonkit@gmail.com>
2023-01-05 21:29:00 +0200

    full solution

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -2,8 +2,16 @@ CREATE SCHEMA IF NOT EXISTS `taxi_service` DEFAULT CHARACTER SET utf8;
 USE `taxi_service`;
 
 CREATE TABLE `manufacturers` (
-                                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                        `name` VARCHAR(225) NOT NULL,
-                                        `country` VARCHAR(225) NOT NULL,
-                                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                        PRIMARY KEY (`id`));
+`id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+`name` VARCHAR(225) NOT NULL,
+`country` VARCHAR(225) NOT NULL,
+`is_deleted` TINYINT NOT NULL DEFAULT 0,
+PRIMARY KEY (`id`));
+
+--drivers table
+CREATE TABLE `taxi_service_db`.`drivers` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(30) NULL,
+  `licence_number` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
+
