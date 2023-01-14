bcf877946bba98f461d075b9ea3609a34cf64308
Vitalina Volkivska <113260060+Vitalina-V@users.noreply.github.com>
2023-01-14 21:11:16 +0200

    objects were received instead of an optionals in service were used constant with descriptive name

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -5,24 +5,24 @@ import mate.jdbc.model.Driver;
 import mate.jdbc.service.DriverService;
 
 public class Main {
-
     private static final Injector injector = Injector.getInstance("mate.jdbc");
+    private static final long NON_EXISTING_DRIVER_ID = 3L;
+    private static final long DRIVER_ID = 4L;
 
     public static void main(String[] args) {
         DriverService driverService = (DriverService) injector
                 .getInstance(DriverService.class);
         driverService.getAll().forEach(System.out::println);
-        Driver ivan = new Driver();
-        ivan.setName("Ivan");
-        ivan.setLicenseNumber("22500335");
-        System.out.println(driverService.create(ivan));
+        Driver denis = new Driver();
+        denis.setName("Denis");
+        denis.setLicenseNumber("20114555");
+        System.out.println(driverService.create(denis));
         driverService.getAll().forEach(System.out::println);
-        ivan.setLicenseNumber("22550335");
-        ivan.setId(ivan.getId());
-        System.out.println(driverService.update(ivan));
+        denis.setLicenseNumber("20224555");
+        System.out.println(driverService.update(denis));
         driverService.getAll().forEach(System.out::println);
-        System.out.println(driverService.get(3L));
-        System.out.println(driverService.delete(1L));
+        System.out.println(driverService.get(DRIVER_ID));
+        System.out.println(driverService.delete(NON_EXISTING_DRIVER_ID));
         driverService.getAll().forEach(System.out::println);
     }
 }
