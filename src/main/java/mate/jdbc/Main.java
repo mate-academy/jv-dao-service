bfd0ac8c4cac33e94d4bb4190ab3cf1b07186ede
Uka0001 <ukraine0001@gmail.com>
2023-01-17 20:51:52 +0200

    task done except some methods test in main

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,32 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriverService;
+
+import java.util.List;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+    private static DriverService driverService;
+    static List<Driver> driverList;
+
     public static void main(String[] args) {
-        // test your code here
+        driverService = (DriverService) injector.getInstance(DriverService.class);
+        createListOfDrivers();
+        driverList.forEach(driver -> driverService.create(driver));
+        System.out.println(driverService.getAll());
+        driverList.get(1).setLicenseNumber("7777");
+        driverList.get(1).setName("Blatnoy:)");
+        driverService.update(driverList.get(1));
+
+    }
+
+    private static void createListOfDrivers() {
+        driverList = List.of(
+                new Driver(null, "First", "1111"),
+                new Driver(null, "Second", "2222"),
+                new Driver(null, "Third", "3333")
+        );
     }
 }
