7a295685df697ac4514aa82dda8fd40935502005
vasyl.moshun <vasyl.moshun@gmail.com>
2023-01-18 13:39:11 +0200

    2nd

--- src/main/java/mate/jdbc/model/Driver.java
+++ src/main/java/mate/jdbc/model/Driver.java
@@ -48,23 +48,4 @@ public class Driver {
                 + ", licenseNumber='" + licenseNumber
                 + '\'' + '}';
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Driver that = (Driver) o;
-        return Objects.equals(id, that.id)
-                && Objects.equals(name, that.name)
-                && Objects.equals(licenseNumber, that.licenseNumber);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, licenseNumber);
-    }
 }
