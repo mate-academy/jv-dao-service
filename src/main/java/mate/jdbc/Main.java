e88be46654db60170df33335cd541cd8467c7c62
Anastasiia Metelska <metelska.anastasiia@gmail.com>
2023-01-19 09:34:51 +0200

    changed driver and manufacturer and tested all on one item

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -10,33 +10,32 @@ public class Main {
     private static Injector injector = Injector.getInstance("mate.jdbc");
 
     public static void main(String[] args) {
-        Driver olga = new Driver();
-        olga.setName("Olga");
-        olga.setLicenseNumber("5678");
-        olga.setId(10L);
+        Driver mykyta = new Driver();
+        mykyta.setName("Mykyta");
+        mykyta.setLicenseNumber("1111");
         DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
-        driverService.create(olga);
+        driverService.create(mykyta);
         System.out.println("Get all drivers from drivers table"
                 + driverService.getAll());
-        System.out.println("Get driver with id = 5 from drivers table "
-                + driverService.get(1L));
+        System.out.println("Get driver with id = 1 from drivers table "
+                + driverService.get(mykyta.getId()));
+        mykyta.setLicenseNumber("2222");
+        driverService.update(mykyta);
         System.out.println("Deleting driver with id = 5 from drivers table"
-                + driverService.delete(5L));
-        olga.setLicenseNumber("7788");
-        driverService.update(olga);
+                + driverService.delete(mykyta.getId()));
         driverService.getAll().forEach(System.out::println);
 
-        Manufacturer ford = new Manufacturer();
-        ford.setName("Ford");
-        ford.setCountry("China");
+        Manufacturer honda = new Manufacturer();
+        honda.setName("Honda");
+        honda.setCountry("China");
         ManufacturerService manufacturerService = (ManufacturerService)
                 injector.getInstance(ManufacturerService.class);
-        manufacturerService.create(ford);
-        manufacturerService.delete(12L);
-        ford.setCountry("German");
-        manufacturerService.update(ford);
-        System.out.println("Getting manufacturer with id = 16 "
-                + manufacturerService.get(16L));
+        manufacturerService.create(honda);
+        honda.setCountry("Japan");
+        manufacturerService.update(honda);
+        System.out.println("Getting manufacturer Ford "
+                + manufacturerService.get(honda.getId()));
+        manufacturerService.delete(honda.getId());
         manufacturerService.getAll().forEach(System.out::println);
     }
 }
