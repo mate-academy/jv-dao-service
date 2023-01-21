3b921c1de3a79f5ad3ce6f12ec52a5a5ec992424
Ihor Vynogradskyi <avassorv@gmail.com>
2023-01-21 22:24:38 +0200

    drivers 1

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,23 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriverService;
+
 public class Main {
+    private static Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        DriverService driverService = (DriverService) injector
+                .getInstance(DriverService.class);
+        Driver driver = new Driver();
+        driver.setName("Bob");
+        driver.setLicenseNumber("123");
+        driverService.create(driver);
+        driver.setName("Bobby");
+        driverService.update(driver);
+        driverService.delete(1L);
+        System.out.println(driverService.get(2L));
+        driverService.getAll().forEach(System.out::println);
     }
 }
