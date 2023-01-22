5596a58c9f3a603046eb5c213ba3927a3c242b8d
Solomiia Voloshyn <solomiiavoloshyn@gmail.com>
2023-01-22 19:38:57 +0200

    build success achieved

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,21 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriverService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        final DriverService driverService
+                = (DriverService) injector.getInstance(DriverService.class);
+
+        Driver driverOne = new Driver();
+        driverOne.setId(1L);
+        driverOne.setName("DriverOne");
+        driverOne.setLicenseNumber("12345");
+        driverService.create(driverOne);
+        driverService.getAll().forEach(System.out::println);
     }
 }
