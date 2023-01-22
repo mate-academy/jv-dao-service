87879f60ad5b12fdfdcf7d7235fa8c040f4fd7a9
Mykola Radzivon <kolian201497@gmail.com>
2023-01-22 23:13:10 +0200

    solved task

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,41 @@
 package mate.jdbc;
 
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerService manufacturerService = (ManufacturerService)
+                injector.getInstance(ManufacturerService.class);
+        List<Manufacturer> manufacturerList = List.of(
+                new Manufacturer("AUDI", "Germany"),
+                new Manufacturer("Volkswagen", "Germany"),
+                new Manufacturer("Toyota", "Japan")
+        );
+        manufacturerList.forEach(manufacturerService::create);
+        Manufacturer testManufacturer = manufacturerService.get(2L);
+        testManufacturer.setName("BMV");
+        manufacturerService.update(testManufacturer);
+        manufacturerService.delete(1L);
+        System.out.println(manufacturerService.getAll());
+        List<Driver> driverList = List.of(
+                new Driver("driver0001", "KNJ589778"),
+                new Driver("driver0002", "KMS564665"),
+                new Driver("driver0003", "SFF899865")
+        );
+        DriverService driverService = (DriverService)
+                injector.getInstance(DriverService.class);
+        driverList.forEach(driverService::create);
+        Driver findDriver = driverService.get(2L);
+        findDriver.setLicenseNumber("ABC000000");
+        driverService.update(findDriver);
+        driverService.delete(1L);
+        System.out.println(driverService.getAll());
     }
 }
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -2,8 +2,18 @@ CREATE SCHEMA IF NOT EXISTS `taxi_service` DEFAULT CHARACTER SET utf8;
 USE `taxi_service`;
 
 CREATE TABLE `manufacturers` (
-                                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                        `name` VARCHAR(225) NOT NULL,
-                                        `country` VARCHAR(225) NOT NULL,
-                                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                        PRIMARY KEY (`id`));
+                                 `id` bigint NOT NULL AUTO_INCREMENT,
+                                 `name` varchar(225) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+                                 `country` varchar(225) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+                                 `is_deleted` tinyint NOT NULL DEFAULT '0',
+                                 PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `drivers` (
+                           `id` bigint NOT NULL AUTO_INCREMENT,
+                           `name` varchar(225) NOT NULL,
+                           `licenseNumber` varchar(225) NOT NULL,
+                           `is_deleted` tinyint NOT NULL DEFAULT '0',
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
