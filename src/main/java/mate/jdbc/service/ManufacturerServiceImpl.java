8600cc0d543265c3b5421b95f37b93b67cbb5d96
Ihor Vynogradskyi <avassorv@gmail.com>
2023-01-23 10:06:02 +0200

    dao service 2

--- src/main/java/mate/jdbc/service/ManufacturerServiceImpl.java
+++ src/main/java/mate/jdbc/service/ManufacturerServiceImpl.java
@@ -1,6 +1,7 @@
 package mate.jdbc.service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import mate.jdbc.dao.ManufacturerDao;
 import mate.jdbc.lib.Inject;
 import mate.jdbc.lib.Service;
@@ -18,7 +19,9 @@ public class ManufacturerServiceImpl implements ManufacturerService {
 
     @Override
     public Manufacturer get(Long id) {
-        return manufacturerDao.get(id).get();
+        return manufacturerDao.get(id)
+                .orElseThrow(() -> new NoSuchElementException("Impossible to get "
+                        + "manufacturer with id " + id));
     }
 
     @Override
