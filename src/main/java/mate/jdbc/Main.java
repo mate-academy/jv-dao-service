aac976b889141ac30ac08352549f6833e67f3663
Serhii Berdychevskyi <sirko120690@mail.com>
2023-01-24 15:36:07 +0200

    Added first version jv-dao-service

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,25 @@
 package mate.jdbc;
 
+import java.util.ArrayList;
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriverService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        List<Driver> driverList = new ArrayList<>();
+        driverList.add(new Driver(4L, "Den", "19875"));
+        driverList.add(new Driver(5L, "Karl", "20060"));
+        driverList.add(new Driver(6L, "Kate", "00100"));
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        driverList.stream()
+                .map(driverService::create)
+                .forEach(System.out::println);
+        System.out.println(driverService.getAll());
+        List<Driver> allDriversFromDB = driverService.getAll();
+        System.out.println(allDriversFromDB);
     }
 }
