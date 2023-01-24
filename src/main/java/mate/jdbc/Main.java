91f8278553a9b3e6020e6e05a24e13048136a268
Leonid Topal <leonvil093@gmail.com>
2023-01-24 15:19:59 +0200

    first impl

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,35 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        Manufacturer citroen = new Manufacturer();
+        citroen.setCountry("France");
+        citroen.setName("Citroen");
+        Manufacturer alfaRomeo = new Manufacturer();
+        alfaRomeo.setName("Alfa Romeo");
+        alfaRomeo.setCountry("China");
+        ManufacturerService manufacturerService = (ManufacturerService)
+                injector.getInstance(ManufacturerService.class);
+        manufacturerService.create(citroen);
+        manufacturerService.create(alfaRomeo);
+        alfaRomeo.setCountry("Italy");
+        System.out.println(manufacturerService.update(alfaRomeo));
+        System.out.println(manufacturerService.getAll());
+
+        Driver sebastienLoeb = new Driver();
+        sebastienLoeb.setName("Sebastien Loeb");
+        sebastienLoeb.setLicenseNumber("2332");
+        DriverService driverService = (DriverService)
+                injector.getInstance(DriverService.class);
+        driverService.create(sebastienLoeb);
+        System.out.println(driverService.getAll());
     }
 }
