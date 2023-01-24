0f9069c9ba5099a8594b93014788710817915a9b
Дмитро <dima.smyk99.19@gmail.com>
2023-01-24 14:01:16 +0200

    add service level for drivers and manufacturers

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,47 @@
 package mate.jdbc;
 
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+
+        ManufacturerService manufacturerService = (ManufacturerService)
+                injector.getInstance(ManufacturerService.class);
+        List<Manufacturer> manufacturerList = List.of(
+                new Manufacturer("Dodge", "USA"),
+                new Manufacturer("Ford", "USA"),
+                new Manufacturer("Subaru", "Japan"),
+                new Manufacturer("Audi", "German")
+        );
+        Manufacturer manufacturerToAdd = new Manufacturer("Fiat", "France");
+        manufacturerList.forEach(manufacturerService::create);
+        manufacturerService.create(manufacturerToAdd);
+        manufacturerService.update(manufacturerToAdd);
+        manufacturerService.get(manufacturerList.get(1).getId());
+        manufacturerService.delete(manufacturerList.get(0).getId());
+        manufacturerService.getAll().forEach(System.out::println);
+
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        List<Driver> driverList = List.of(
+                new Driver("Vasiliy", "АХ3123ВС"),
+                new Driver("Petya", "АХ5123СВ"),
+                new Driver("Misha", "АН1821ВС"),
+                new Driver("Vladislav", "КХ7777КХ")
+        );
+        Driver driverToAdd = new Driver("Taras", "АХ4123НВ");
+        driverList.forEach(driverService::create);
+        driverService.create(driverToAdd);
+        driverService.update(driverToAdd);
+        driverService.get(driverList.get(1).getId());
+        driverService.delete(driverList.get(0).getId());
+        driverService.getAll().forEach(System.out::println);
     }
 }
