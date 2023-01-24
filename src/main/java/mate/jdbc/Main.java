c4bc6725f8ec749bf9e3711f798ac89b047f0edd
Solomiia Voloshyn <solomiiavoloshyn@gmail.com>
2023-01-24 08:58:55 +0200

    fixed

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -2,15 +2,31 @@ package mate.jdbc;
 
 import mate.jdbc.lib.Injector;
 import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
 import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
 
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.jdbc");
 
     public static void main(String[] args) {
-        final DriverService driverService
+        ManufacturerService manufacturerService =
+                (ManufacturerService) injector.getInstance(ManufacturerService.class);
+        Manufacturer manufacturerKia = new Manufacturer();
+        manufacturerKia.setName("KIA");
+        manufacturerKia.setCountry("Japan");
+        System.out.println(manufacturerService.create(manufacturerKia));
+        Manufacturer manufacturerAlfaRomeo = new Manufacturer();
+        manufacturerAlfaRomeo.setName("Alfa Romeo");
+        manufacturerAlfaRomeo.setCountry("italy");
+        System.out.println(manufacturerService.create(manufacturerAlfaRomeo));
+        System.out.println(manufacturerService.delete(manufacturerAlfaRomeo.getId()));
+        manufacturerKia.setCountry("South Korea");
+        manufacturerService.update(manufacturerKia);
+        System.out.println(manufacturerService.get(manufacturerKia.getId()));
+        manufacturerService.getAll().forEach(System.out::println);
+        DriverService driverService
                 = (DriverService) injector.getInstance(DriverService.class);
-
         Driver driverOne = new Driver();
         driverOne.setName("DriverOne");
         driverOne.setLicenseNumber("12345");
