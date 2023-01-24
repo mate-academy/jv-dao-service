5ce6305d80d5195d43e15a48d6e604c733150716
Your NameMykola <mykola.osolinskyi@gmail.com>
2023-01-24 14:37:19 +0100

    add some classes

---
+++ src/main/java/mate/jdbc/dao/impl/DriverDaoImpl.java
@@ -0,0 +1,34 @@
+package mate.jdbc.dao.impl;
+
+import mate.jdbc.dao.DriverDao;
+import mate.jdbc.model.Driver;
+
+import java.util.List;
+import java.util.Optional;
+
+public class DriverDaoImpl implements DriverDao {
+    @Override
+    public Driver create(Driver driver) {
+        return null;
+    }
+
+    @Override
+    public Optional<Driver> get(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Driver> getAll() {
+        return null;
+    }
+
+    @Override
+    public Driver update(Driver driver) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+}
