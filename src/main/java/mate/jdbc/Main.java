b6b21494cb89b63e7e90e2f2420c3c6f44f4bf69
Alexander <liero2D@gmail.com>
2023-02-02 14:18:51 +0200

    Added dao/impl and services/impl. Also class driver and table on DB.

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,29 @@
 package mate.jdbc;
 
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriverService;
+
 public class Main {
+    private static final Injector injector =
+            Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        Driver driverFirst = new Driver();
+        Driver driverSecond = new Driver();
+        driverFirst.setName("John");
+        driverFirst.setLicenseNumber("ua12311");
+        driverSecond.setName("Billy");
+        driverSecond.setLicenseNumber("ua76135");
+        DriverService driverService =
+                (DriverService) injector.getInstance(DriverService.class);
+        Driver createDriverFirst = driverService.create(driverFirst);
+        Driver createDriverSecond = driverService.create(driverSecond);
+        Driver updateDriverFirst = driverService.update(createDriverFirst);
+        Driver updateDriverSecond = driverService.update(createDriverSecond);
+        driverService.delete(updateDriverSecond.getId());
+        List<Driver> driverList = driverService.getAll();
+        driverList.forEach(System.out::println);
     }
 }
