77ee361f9ad58f6a764cbda43499d75ab5c63a4c
Andrii Malenko <andmalenko@gmail.com>
2023-02-16 13:40:17 +0200

    Fixed2

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,8 +1,6 @@
 package mate.jdbc;
 
 import mate.jdbc.lib.Injector;
-import mate.jdbc.model.Driver;
-import mate.jdbc.model.Manufacturer;
 import mate.jdbc.service.DriverService;
 import mate.jdbc.service.ManufacturerService;
 
@@ -14,33 +12,6 @@ public class Main {
                 (ManufacturerService) injector.getInstance(ManufacturerService.class);
         DriverService driverService =
                 (DriverService) injector.getInstance(DriverService.class);
-        Manufacturer manufacturer = new Manufacturer();
-        /*manufacturer.setName("Honda");
-        manufacturer.setCountry("Japan");
-        manufacturerService.create(manufacturer);
-        manufacturer.setName("Ford");
-        manufacturer.setCountry("USA");
-        manufacturerService.create(manufacturer);
-        System.out.println(manufacturerService.get(8L));
-        manufacturer.setId(8L);
-        manufacturer.setName("Dodge");
-        manufacturer.setCountry("USA");
-        manufacturerService.update(manufacturer);*/
-        manufacturerService.delete(8L);
-
-        Driver driver = new Driver();
-        /*driver.setName("Andrii");
-        driver.setLicenseNumber("1234");
-        driverService.create(driver);
-        driver.setName("Oleg");
-        driver.setLicenseNumber("5678");
-        driverService.create(driver);
-        System.out.println(driverService.get(2L));
-        driver.setId(2L);
-        driver.setName("Stepan");
-        driver.setLicenseNumber("9999");
-        driverService.update(driver);*/
-        driverService.delete(2L);
 
         manufacturerService.getAll().forEach(System.out::println);
         driverService.getAll().forEach(System.out::println);
--- src/main/java/mate/jdbc/dao/DriverDaoImpl.java
+++ src/main/java/mate/jdbc/dao/DriverDaoImpl.java
@@ -15,7 +15,6 @@ import mate.jdbc.util.ConnectionUtil;
 
 @Dao
 public class DriverDaoImpl implements DriverDao {
-
     @Override
     public Driver create(Driver driver) {
         String query = "INSERT INTO drivers (name, license_number) "
