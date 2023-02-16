4632677527f35a7668e21de4b52102a08282c07c
Andrii Malenko <andmalenko@gmail.com>
2023-02-16 12:41:47 +0200

    Here is my solution for hw-jdbc-taxi-service3

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,48 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerService manufacturerService =
+                (ManufacturerService) injector.getInstance(ManufacturerService.class);
+        DriverService driverService =
+                (DriverService) injector.getInstance(DriverService.class);
+        Manufacturer manufacturer = new Manufacturer();
+        /*manufacturer.setName("Honda");
+        manufacturer.setCountry("Japan");
+        manufacturerService.create(manufacturer);
+        manufacturer.setName("Ford");
+        manufacturer.setCountry("USA");
+        manufacturerService.create(manufacturer);
+        System.out.println(manufacturerService.get(8L));
+        manufacturer.setId(8L);
+        manufacturer.setName("Dodge");
+        manufacturer.setCountry("USA");
+        manufacturerService.update(manufacturer);*/
+        manufacturerService.delete(8L);
+
+        Driver driver = new Driver();
+        /*driver.setName("Andrii");
+        driver.setLicenseNumber("1234");
+        driverService.create(driver);
+        driver.setName("Oleg");
+        driver.setLicenseNumber("5678");
+        driverService.create(driver);
+        System.out.println(driverService.get(2L));
+        driver.setId(2L);
+        driver.setName("Stepan");
+        driver.setLicenseNumber("9999");
+        driverService.update(driver);*/
+        driverService.delete(2L);
+
+        manufacturerService.getAll().forEach(System.out::println);
+        driverService.getAll().forEach(System.out::println);
     }
 }
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,9 +1,18 @@
-CREATE SCHEMA IF NOT EXISTS `taxi_service` DEFAULT CHARACTER SET utf8;
-USE `taxi_service`;
+CREATE DATABASE `taxi_db` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
-CREATE TABLE `manufacturers` (
-                                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                        `name` VARCHAR(225) NOT NULL,
-                                        `country` VARCHAR(225) NOT NULL,
-                                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                        PRIMARY KEY (`id`));
+CREATE TABLE `taxi_manufacturers` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT 'null',
+  `country` varchar(255) DEFAULT 'null',
+  `is_deleted` tinyint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `taxi_drivers` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT 'null',
+  `license_number` varchar(255) DEFAULT 'null',
+  `is_deleted` tinyint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
