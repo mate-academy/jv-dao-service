68ae8d95d30860ac9f070ab18e27cd8d274baffa
o-shyshkan <o.shyshkan@gmail.com>
2023-02-18 22:38:17 +0200

    Solved jv-dao-service

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,30 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriverService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
         // test your code here
+        DriverService driverService = (DriverService) injector
+                .getInstance(DriverService.class);
+        Driver peter = new Driver("Peter","1234567");
+        System.out.println("Create driver with name=Peter, licence number=1234567: "
+                + driverService.create(peter));;
+        Driver bob = new Driver("Bob","98765432");
+        System.out.println("Create driver with name=Bob, licence number=98765432: "
+                + driverService.create(bob));;
+        System.out.println("Get driver peter: " + driverService
+                .get(peter.getId()).toString());;
+        System.out.println("Get all drivers: " + driverService.getAll().toString());;
+        peter.setName("Mark");
+        peter.setLicenseNumber("12131415");
+        System.out.println("Update driver with name=Mark, licence number=12131415: "
+                + driverService.update(peter).toString());;
+        System.out.println("Delete driver peter: "
+                + driverService.delete(peter.getId()));
     }
 }
