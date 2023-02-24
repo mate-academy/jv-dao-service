cf9825b5923972b5561d4f8c3c254405d6bde033
sasha <radovietsoleksandr@gmail.com>
2023-02-24 17:30:42 +0200

    driver2

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,11 +1,27 @@
 package mate.jdbc;
 
+import mate.jdbc.dao.DriverDaoImpl;
 import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.impl.DriverServiceImpl;
 
 public class Main {
     private static final Injector injector = Injector.getInstance("mate");
 
     public static void main(String[] args) {
+
+        final Driver driver = new Driver();
+        driver.setName("Roman");
+        driver.setLicenseNumber("22");
+
+        DriverService driverService = new DriverServiceImpl(new DriverDaoImpl());
+
+        driverService.createDriver(driver);
+        driverService.getDriver(2L);
+        driverService.getAllDriver();
+        driverService.updateDriver(driver);
+        driverService.deleteDriver(2L);
 
     }
 }
