3fb6a1b4e0cd230b899afb4de509841f658cc3a2
Sergii Uglovyi <s.uglovyi@gmail.com>
2023-02-27 10:02:36 +0200

    fixed service impl

--- src/main/java/mate/jdbc/service/DriverServiceImpl.java
+++ src/main/java/mate/jdbc/service/DriverServiceImpl.java
@@ -1,6 +1,8 @@
 package mate.jdbc.service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+
 import mate.jdbc.dao.DriverDao;
 import mate.jdbc.lib.Inject;
 import mate.jdbc.lib.Service;
@@ -18,7 +20,9 @@ public class DriverServiceImpl implements DriverService {
 
     @Override
     public Driver get(Long id) {
-        return manufacturerDao.get(id).orElseThrow();
+        return manufacturerDao.get(id)
+                .orElseThrow(() -> new NoSuchElementException("Could not get driver "
+                + "by id = " + id));
     }
 
     @Override
--- src/main/java/mate/jdbc/service/ManufacturerServiceImpl.java
+++ src/main/java/mate/jdbc/service/ManufacturerServiceImpl.java
@@ -1,6 +1,8 @@
 package mate.jdbc.service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+
 import mate.jdbc.dao.ManufacturerDao;
 import mate.jdbc.lib.Inject;
 import mate.jdbc.lib.Service;
@@ -18,7 +20,9 @@ public class ManufacturerServiceImpl implements ManufacturerService {
 
     @Override
     public Manufacturer get(Long id) {
-        return manufacturerDao.get(id).orElseThrow();
+        return manufacturerDao.get(id)
+                .orElseThrow(() -> new NoSuchElementException("Could not get manufacturer "
+                        + "by id = " + id));
     }
 
     @Override
