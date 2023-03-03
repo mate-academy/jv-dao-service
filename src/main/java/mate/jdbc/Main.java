12d5c9451ea4ef2dacf63ab56173416fcb5f40d6
Yevhenii Kilovyi <118058456+YevheniiKilovyi@users.noreply.github.com>
2023-03-03 14:10:16 +0200

    implemented manufacturer service, added model of driver, implemented driver dao with its implementation, and implemeneted driverservice

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,27 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriverService;
+
 public class Main {
+    private static Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        final DriverService driverService =
+                (DriverService) injector.getInstance(DriverService.class);
+        Driver bob = new Driver();
+        bob.setName("Bob");
+        bob.setLicenceNumber("3968722");
+        Driver alice = new Driver();
+        alice.setName("Alice");
+        alice.setLicenceNumber("4472652");
+        Driver john = new Driver();
+        john.setName("John");
+        john.setLicenceNumber("88833162");
+        driverService.create(bob);
+        driverService.create(alice);
+        driverService.create(john);
+        driverService.getAll().forEach(System.out::println);
     }
 }
