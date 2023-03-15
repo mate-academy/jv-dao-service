432d9577ff9066a95df6186962a431d3c8f593d6
Ilia Budenkov <i.budenkov@gmail.com>
2023-03-15 12:10:12 +0200

    Added a solution to the task

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,61 @@
 package mate.jdbc;
 
+import java.util.Optional;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
     public static void main(String[] args) {
-        // test your code here
+        Injector injector = Injector.getInstance("mate.jdbc");
+        ManufacturerService manufacturerService =
+                (ManufacturerService) injector.getInstance(ManufacturerService.class);
+        testManufactureService(manufacturerService,
+                new Manufacturer("testManufacturer", "testCountry"));
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        testDriverService(driverService,
+                new Driver("test driver", "test licence number"));
     }
+
+    public static void testManufactureService(ManufacturerService service,
+                                              Manufacturer testManufacturer) {
+        System.out.println("*************** list all manufacturers *******************");
+        service.getAll().forEach(System.out::println);
+        System.out.println("*************** create new manufacturers *******************");
+        Manufacturer newManufacturer = service.create(testManufacturer);
+        service.get(newManufacturer.getId()).ifPresent(System.out::println);
+        System.out.println("*************** get&update manufacturers *******************");
+        Optional<Manufacturer> optionalManufacturer = service.get(newManufacturer.getId());
+        if (optionalManufacturer.isPresent()) {
+            testManufacturer = optionalManufacturer.get();
+            testManufacturer.setCountry("Ukraine");
+            testManufacturer = service.update(testManufacturer);
+            service.get(testManufacturer.getId()).ifPresent(System.out::println);
+        }
+        System.out.println("*************** delete manufacturers *******************");
+        service.delete(testManufacturer.getId());
+        service.getAll().forEach(System.out::println);
+    }
+
+    public static void testDriverService(DriverService service, Driver testDriver) {
+        System.out.println("*************** list all drivers *******************");
+        service.getAll().forEach(System.out::println);
+        System.out.println("*************** create new driver *******************");
+        Driver newDriver = service.create(testDriver);
+        service.get(newDriver.getId()).ifPresent(System.out::println);
+        System.out.println("*************** get&update driver *******************");
+        Optional<Driver> optionalDriver = service.get(newDriver.getId());
+        if (optionalDriver.isPresent()) {
+            testDriver = optionalDriver.get();
+            testDriver.setLicenseNumber("new license number");
+            testDriver = service.update(testDriver);
+            service.get(testDriver.getId()).ifPresent(System.out::println);
+        }
+        System.out.println("*************** delete driver *******************");
+        service.delete(testDriver.getId());
+        service.getAll().forEach(System.out::println);
+    }
+
 }
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -7,3 +7,19 @@ CREATE TABLE `manufacturers` (
                                         `country` VARCHAR(225) NOT NULL,
                                         `is_deleted` TINYINT NOT NULL DEFAULT 0,
                                         PRIMARY KEY (`id`));
+
+INSERT INTO `taxi_service`.`manufacturers` (`name`, `country`) VALUES ('Toyota', 'Japan'),
+ ('Hyundai', 'South Korea'), ('Volkswagen Group', 'Germany'), ('General Motors', 'United States'),
+ ('Ford', 'United States'), ('Nissan', 'Japan'), ('Fiat Chrysler Automobiles', 'Italy/United States'),
+ ('Renault', 'France'), ('PSA Group', 'France'), ('Suzuki', 'Japan'), ('SAIC', 'China'),
+ ('Daimler', 'Germany'), ('BMW', 'Germany'), ('Geely', 'China');
+
+CREATE TABLE `drivers` (
+                                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+                                        `name` VARCHAR(225) NOT NULL,
+                                        `licenseNumber` VARCHAR(225) NOT NULL,
+                                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
+                                        PRIMARY KEY (`id`));
+
+INSERT INTO `taxi_service`.`drivers` (`name`, `licenseNumber`)
+                            VALUES ('Bob', '55484823'), ('Alice', '6587426');
