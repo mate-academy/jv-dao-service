74aad651014d9abfb073e401c6b6d8c5dc9f4e59
Volodymyr Barasiuk <barmalej23@gmail.com>
2023-03-16 20:10:24 +0200

    ManufacturerService & impl created

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,23 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+
+        ManufacturerService manufacturerService =
+                (ManufacturerService) injector.getInstance(ManufacturerService.class);
+
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturer.setName("Opel");
+        manufacturer.setCountry("Germany");
+
+        System.out.println(manufacturerService.create(manufacturer));
+
     }
 }
