847444d7a82b575c10205d7583d35c97c82031a4
Yehor-Cherkashyn <gooooodvin1995@gmail.com>
2023-03-21 18:17:43 +0200

    Fixed some mistakes

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,6 +1,9 @@
 package mate.jdbc;
 
+import java.util.List;
 import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
 import mate.jdbc.service.DriverService;
 import mate.jdbc.service.ManufacturerService;
 
@@ -8,11 +11,23 @@ public class Main {
     private static final Injector injector = Injector.getInstance("mate.jdbc");
 
     public static void main(String[] args) {
-        final DriverService driverService
-                   = (DriverService) injector.getInstance(DriverService.class);
         final ManufacturerService manufacturerService
-                   = (ManufacturerService) injector.getInstance(ManufacturerService.class);
-        System.out.println(driverService.getAll());
-        System.out.println(manufacturerService.getAll());
+                = (ManufacturerService) injector.getInstance(ManufacturerService.class);
+        final DriverService driverService
+                = (DriverService) injector.getInstance(DriverService.class);
+
+        manufacturerService.create(new Manufacturer(null, "Hyundai", "Korea"));
+        manufacturerService.get(11L);
+        manufacturerService.update(new Manufacturer(11L, "Hyundai", "South Korea"));
+        manufacturerService.delete(5L);
+        List<Manufacturer> manufacturers = manufacturerService.getAll();
+        manufacturers.forEach(System.out::println);
+
+        driverService.create(new Driver(null, "Frodo", "9379992"));
+        driverService.get(4L);
+        driverService.update(new Driver(4L, "Frodo Beggins", "9379992"));
+        driverService.delete(1L);
+        List<Driver> drivers = driverService.getAll();
+        drivers.forEach(System.out::println);
     }
 }
