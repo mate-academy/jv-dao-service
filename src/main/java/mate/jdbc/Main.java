9a38bd4d59feec9ea7f0b1983d277a1bcd1aa550
Denys Dominskyi <dominskyj@gmail.com>
2023-03-21 16:16:16 +0200

    added testing in main

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -2,6 +2,7 @@ package mate.jdbc;
 
 import mate.jdbc.lib.Injector;
 import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
 import mate.jdbc.service.DriverService;
 import mate.jdbc.service.ManufacturerService;
 
@@ -9,12 +10,22 @@ public class Main {
     private static Injector injector = Injector.getInstance("mate.jdbc");
 
     public static void main(String[] args) {
-        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
         ManufacturerService manufacturerService
                 = (ManufacturerService) injector.getInstance(ManufacturerService.class);
+        Manufacturer manufacturerToyota = new Manufacturer("Toyota", "Japan");
+        Manufacturer manufacturerLexus = new Manufacturer(1L,"Lexus", "Japan");
+        manufacturerService.create(manufacturerToyota);
+        System.out.println(manufacturerService.get(1L));
+        manufacturerService.update(manufacturerLexus);
+        manufacturerService.delete(1L);
         System.out.println(manufacturerService.getAll());
-        Driver driver = new Driver("Petro", "545454");
-        driverService.create(driver);
+        Driver driverPetro = new Driver("Petro", "545454");
+        Driver driverDmytro = new Driver(1L,"Dmytro", "545454");
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        driverService.create(driverPetro);
+        System.out.println(driverService.get(1L));
+        driverService.update(driverDmytro);
+        driverService.delete(1L);
         System.out.println(driverService.getAll());
     }
 }
