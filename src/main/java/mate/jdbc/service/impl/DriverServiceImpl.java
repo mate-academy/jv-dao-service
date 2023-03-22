9d80c1a61a27610cd3ce76cce6d0610c39834761
Yehor-Cherkashyn <gooooodvin1995@gmail.com>
2023-03-22 12:52:30 +0200

    Added informative message to throw an exception in get by id methods in the services. Changed case to Upper case in the init_db file

--- src/main/java/mate/jdbc/service/impl/DriverServiceImpl.java
+++ src/main/java/mate/jdbc/service/impl/DriverServiceImpl.java
@@ -1,7 +1,10 @@
 package mate.jdbc.service.impl;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.zip.DataFormatException;
 import mate.jdbc.dao.DriverDao;
+import mate.jdbc.exception.DataProcessingException;
 import mate.jdbc.lib.Inject;
 import mate.jdbc.lib.Service;
 import mate.jdbc.model.Driver;
@@ -19,7 +22,8 @@ public class DriverServiceImpl implements DriverService {
 
     @Override
     public Driver get(Long id) {
-        return driverDao.get(id).orElseThrow();
+        return driverDao.get(id).orElseThrow(() ->
+                new NoSuchElementException("Can't get driver by id: " + id));
     }
 
     @Override
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,17 +1,17 @@
 CREATE DATABASE `taxi_service` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 CREATE TABLE `drivers` (
-                           `id` bigint NOT NULL AUTO_INCREMENT,
-                           `name` varchar(255) NOT NULL,
-                           `licenseNumber` varchar(255) NOT NULL,
-                           `is_deleted` tinyint DEFAULT '0',
+                           `id` BIGINT NOT NULL AUTO_INCREMENT,
+                           `name` VARCHAR(255) NOT NULL,
+                           `licenseNumber` VARCHAR(255) NOT NULL,
+                           `is_deleted` TINYINT DEFAULT '0',
                            PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
 
 CREATE TABLE `manufacturers` (
                                  `id` bigint NOT NULL AUTO_INCREMENT,
-                                 `name` varchar(255) NOT NULL,
-                                 `country` varchar(255) NOT NULL,
-                                 `is_deleted` tinyint NOT NULL DEFAULT '0',
+                                 `name` VARCHAR(255) NOT NULL,
+                                 `country` VARCHAR(255) NOT NULL,
+                                 `is_deleted` TINYINT NOT NULL DEFAULT '0',
                                  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3;
