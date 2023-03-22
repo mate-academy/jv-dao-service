d5a7993f21a4d29bc9572647f222439f93fba48f
kromchk <kromchyk@yahoo.com>
2023-03-22 11:48:29 +0200

    realization dao service, amended all (minor changes2)

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,14 +1,14 @@
-CREATE SCHEMA IF NOT EXISTS `taxi_service` DEFAULT CHARACTER SET utf8;
-USE `taxi_service`;
+CREATE SCHEMA IF NOT EXISTS taxi_service_database DEFAULT CHARACTER SET utf8;
+USE taxi_service_database;
 
-CREATE TABLE `taxi_service`.`manufacturer` (
+CREATE TABLE taxi_service_database.`manufacturer`(
             `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
             `name` VARCHAR(255) NULL,
             `country` VARCHAR(255) NULL,
             `is_deleted` TINYINT NOT NULL DEFAULT 0,
             PRIMARY KEY (`id`));
 
-CREATE TABLE `driver` (
+CREATE TABLE taxi_service_database.`driver` (
             `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
             `name` VARCHAR(225) NOT NULL,
             `licenseNumber` VARCHAR(225) NOT NULL,
