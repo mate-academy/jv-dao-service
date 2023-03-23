3e01b555a892a26cb4c2bae23c2a5a72f8e2a74f
Nickolay Voloshchuk <124300295+NickolayVoloshchuk@users.noreply.github.com>
2023-03-23 20:21:17 +0200

    HomeWork/JDBC DAO vs Service

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,80 @@
 package mate.jdbc;
 
+import static mate.jdbc.util.TestUtil.printAllDriver;
+import static mate.jdbc.util.TestUtil.printAllManufacturer;
+import static mate.jdbc.util.TestUtil.testCreateDriver;
+import static mate.jdbc.util.TestUtil.testCreateManufacturer;
+import static mate.jdbc.util.TestUtil.testDeleteDriver;
+import static mate.jdbc.util.TestUtil.testDeleteManufacturer;
+import static mate.jdbc.util.TestUtil.testGetDriver;
+import static mate.jdbc.util.TestUtil.testGetManufacturer;
+import static mate.jdbc.util.TestUtil.testUpdateDriver;
+import static mate.jdbc.util.TestUtil.testUpdateManufacturer;
+
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+import mate.jdbc.util.ConnectionUtil;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance(Main.class.getPackageName());
+
     public static void main(String[] args) {
-        // test your code here
+        System.out.println("INFO           DB: " + ConnectionUtil.getUrl());
+        System.out.println("INFO         User: " + ConnectionUtil.getUserName());
+        ManufacturerService manufacturerService = (ManufacturerService)
+                injector.getInstance(ManufacturerService.class);
+        //Start test Manufacturers functional
+        startTestManufacturer(manufacturerService);
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        //Start test Drivers functional
+        startTestDriver(driverService);
+    }
+
+    private static void startTestManufacturer(ManufacturerService manufacturerService) {
+        final Manufacturer audi = new Manufacturer("AUDI", "Germany");
+        final Manufacturer bmw = new Manufacturer("BMW", "Germany");
+        final Manufacturer chevrolet = new Manufacturer("Chevrolet", "Germany");
+        System.out.println("\n#################################################################");
+        System.out.println("#################### MANUFACTURES TEST ##########################");
+        System.out.println("#################################################################");
+        printAllManufacturer(manufacturerService);
+        //create
+        testCreateManufacturer(manufacturerService, audi, bmw, chevrolet);
+        printAllManufacturer(manufacturerService);
+        //get
+        testGetManufacturer(manufacturerService);
+        printAllManufacturer(manufacturerService);
+        //update
+        testUpdateManufacturer(manufacturerService);
+        printAllManufacturer(manufacturerService);
+        //delete
+        testDeleteManufacturer(manufacturerService);
+        printAllManufacturer(manufacturerService);
+    }
+
+    private static void startTestDriver(DriverService driverService) {
+        final Driver andry = new Driver("Andry", "123456789");
+        final Driver oksana = new Driver("Oksana", "666");
+        final Driver oleg = new Driver("Oleg", "987654321");
+        System.out.println("\n################################################################");
+        System.out.println("####################### DRIVER TEST ############################");
+        System.out.println("################################################################");
+        //getAll
+        printAllDriver(driverService);
+        //create
+        testCreateDriver(driverService, andry, oksana, oleg);
+        printAllDriver(driverService);
+        //get
+        testGetDriver(driverService);
+        printAllDriver(driverService);
+        //update
+        testUpdateDriver(driverService);
+        printAllDriver(driverService);
+        //delete
+        testDeleteDriver(driverService);
+        printAllDriver(driverService);
     }
 }
--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -4,25 +4,39 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
+import mate.jdbc.exception.DataProcessingException;
 
 public class ConnectionUtil {
-    private static final String URL = "YOUR DATABASE URL";
-    private static final String USERNAME = "YOUR USERNAME";
-    private static final String PASSWORD = "YOUR PASSWORD";
-    private static final String JDBC_DRIVER = "YOUR DRIVER";
+    private static final String URL = "jdbc:mysql://localhost:3306/taxi_service";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "11111";
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 
     static {
         try {
             Class.forName(JDBC_DRIVER);
         } catch (ClassNotFoundException e) {
-            throw new RuntimeException("Can't find SQL Driver", e);
+            throw new DataProcessingException("Can't find SQL Driver: " + JDBC_DRIVER, e);
         }
     }
 
+    public static String getUrl() {
+        return URL;
+    }
+
+    public static String getUserName() {
+        return USERNAME;
+    }
+
     public static Connection getConnection() throws SQLException {
-        Properties dbProperties = new Properties();
-        dbProperties.setProperty("user", USERNAME);
-        dbProperties.setProperty("password", PASSWORD);
-        return DriverManager.getConnection(URL, dbProperties);
+        try {
+            Properties dbProperties = new Properties();
+            dbProperties.setProperty("user", USERNAME);
+            dbProperties.setProperty("password", PASSWORD);
+            return DriverManager.getConnection(URL, dbProperties);
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can't create connection to DB: "
+                    + URL, e);
+        }
     }
 }
