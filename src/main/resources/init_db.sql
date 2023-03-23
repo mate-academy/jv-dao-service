35488d79e5177fc02b1551040e36b46eaf8540f2
Nadiia Huryk <nadia11huryk@gmail.com>
2023-03-23 20:06:07 +0200

    DAO service solution

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -15,6 +15,3 @@ CREATE TABLE `drivers` (
                            `is_deleted` TINYINT NOT NULL DEFAULT '0',
                            PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
-
-
-
