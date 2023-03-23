47047d4f478b253fa00c8e3e84cf9c1b5e3bb607
Vasyl Vasylchuk <v.vasylchuk18@gmail.com>
2023-03-23 15:29:12 +0200

    add new table, model Driver and fix connection

---
+++ src/main/java/mate/jdbc/model/Driver.java
@@ -0,0 +1,43 @@
+package mate.jdbc.model;
+
+public class Driver {
+    private Long id;
+    private String name;
+    private String licenseNumber;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Driver setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Driver setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public Driver setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Driver{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + ", licenseNumber='" + licenseNumber + '\''
+                + '}';
+    }
+}
