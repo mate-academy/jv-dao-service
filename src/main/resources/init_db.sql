985dcbdde3c118226885028d67463282f1d59aa9
karpo <alexkarpiuk99@gmail.com>
2023-03-23 17:34:09 +0200

    make 50%

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -7,3 +7,13 @@ CREATE TABLE `manufacturers` (
                                         `country` VARCHAR(225) NOT NULL,
                                         `is_deleted` TINYINT NOT NULL DEFAULT 0,
                                         PRIMARY KEY (`id`));
+CREATE TABLE `taxi_service`.`drivers` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `licenseNumber` VARCHAR(45) NOT NULL,
+  `is_deleted` TINYINT NOT NULL,
+  PRIMARY KEY (`id`));
+ALTER TABLE `taxi_service`.`drivers`
+CHANGE COLUMN `name` `name` VARCHAR(255) NOT NULL ,
+CHANGE COLUMN `licenseNumber` `licenseNumber` VARCHAR(255) NOT NULL ,
+CHANGE COLUMN `is_deleted` `is_deleted` TINYINT NOT NULL DEFAULT 0 ;
