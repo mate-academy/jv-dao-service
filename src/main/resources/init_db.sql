1d86b78abe2b015fa8df75077309d3b42189a57e
Olena Matus <matus_ak16@nuwm.edu.ua>
2023-03-24 10:40:37 +0200

    changed get() method in DriverService

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -8,7 +8,7 @@ CREATE TABLE `manufacturers` (
                                         `is_deleted` TINYINT NOT NULL DEFAULT 0,
                                         PRIMARY KEY (`id`));
 CREATE TABLE `drivers` (
-                                      `id` bigint NOT NULL AUTO_INCREMENT,
+                                      `id` BIGINT NOT NULL AUTO_INCREMENT,
                                       `name` VARCHAR(255) NOT NULL,
                                       `license_number` VARCHAR(255) NOT NULL,
                                       `is_deleted` TINYINT NOT NULL DEFAULT 0,
