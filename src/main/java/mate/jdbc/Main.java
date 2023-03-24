03d2b3d1f5a31eb6f6c4c9013fea370611cc8ae2
Maksym Horielikov <hmv.fermats@gmail.com>
2023-03-24 13:32:29 +0200

    added test all methods ManufacturerServiceImpl, change name database in init_db.sql

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -21,11 +21,32 @@ public class Main {
                 new Manufacturer("Citroen", "France"),
                 new Manufacturer("Toyota", "Japan")
         );
+
+        System.out.println("\n\n---------------------TEST MANUFACTURER SERVICE-------------------");
         System.out.println("\nTest manufacturerService create:");
         for (Manufacturer manufacturer : manufacturers) {
             manufacturerService.create(manufacturer);
         }
         System.out.println(manufacturers);
+
+        Long id = 1L;
+        System.out.println("\nTest manufacturerService delete by id = " + id + " :");
+        System.out.println("result = " + manufacturerService.delete(id));
+
+        id = 2L;
+        System.out.println("\nTest manufacturerService get by id = " + id + " :");
+        System.out.println("result = " + manufacturerService.get(id));
+
+        System.out.println("\nTest manufacturerService update manufacturer:");
+        Manufacturer manufacturerFromDb = manufacturerService.get(id);
+        System.out.println("before: " + manufacturerFromDb);
+        String newName = "Ford";
+        String newCountry = "USA";
+        manufacturerFromDb.setName(newName);
+        manufacturerFromDb.setCountry(newCountry);
+        System.out.println("Change name to " + newName);
+        System.out.println("Change country to " + newCountry);
+        System.out.println("after: " + manufacturerService.update(manufacturerFromDb));
 
         System.out.println("\nTest manufacturerService getAll:");
         manufacturers = manufacturerService.getAll();
@@ -37,24 +58,31 @@ public class Main {
                 new Driver("Donald Trump", "999666999")
         );
 
+        System.out.println("\n\n---------------------TEST DRIVER SERVICE-------------------");
         System.out.println("\nTest driverService create:");
         for (Driver driver : drivers) {
             driverService.create(driver);
         }
         System.out.println(manufacturers);
 
-        Long id = 1L;
+        id = 1L;
         System.out.println("\nTest driverService delete by id = " + id + " :");
-        System.out.println(driverService.delete(id));
+        System.out.println("result = " + driverService.delete(id));
 
         id = 2L;
         System.out.println("\nTest driverService get by id = " + id + " :");
         System.out.println(driverService.get(id));
 
-        Driver driver = new Driver(id, "Hary Crishna", "777777777777");
         System.out.println("\nTest driverService update driver:");
-        System.out.println(driver);
-        System.out.println(driverService.update(driver));
+        Driver driverFromDb = driverService.get(id);
+        System.out.println("before: " + driverFromDb);
+        newName = "Hary Crishna";
+        String newLicenseNumber = "777777777777777";
+        driverFromDb.setName(newName);
+        driverFromDb.setLicenseNumber(newLicenseNumber);
+        System.out.println("Change name to " + newName);
+        System.out.println("Change licenseNumber to " + newLicenseNumber);
+        System.out.println("after: " + driverService.update(driverFromDb));
 
         System.out.println("\nTest driverService getAll:");
         drivers = driverService.getAll();
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,6 +1,5 @@
-CREATE SCHEMA IF NOT EXISTS `taxi_service` DEFAULT CHARACTER SET utf8;
-USE
-`taxi_service`;
+CREATE SCHEMA IF NOT EXISTS `taxi_db` DEFAULT CHARACTER SET utf8;
+USE `taxi_db`;
 
 CREATE TABLE `manufacturers`
 (
