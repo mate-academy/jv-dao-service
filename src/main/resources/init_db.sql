7c9799af47bab4e2a464125bc4e428ae8aa244bf
Gleb Iaroshevych <randomeastecho@gmail.com>
2023-03-24 13:53:51 +0200

    Removed redundant empty lines

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -14,5 +14,3 @@ CREATE TABLE `drivers` (
                                       `is_deleted` TINYINT NOT NULL DEFAULT '0',
                                       PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
-
