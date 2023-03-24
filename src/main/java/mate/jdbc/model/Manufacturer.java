5391c942f55ce9df0a942c30597612a7440184a2
Sushko Roman <formatesus@gmail.com>
2023-03-24 13:25:45 +0200

    added equals and hashcode in models

--- src/main/java/mate/jdbc/model/Manufacturer.java
+++ src/main/java/mate/jdbc/model/Manufacturer.java
@@ -1,5 +1,7 @@
 package mate.jdbc.model;
 
+import java.util.Objects;
+
 public class Manufacturer {
     private Long id;
     private String name;
@@ -44,9 +46,30 @@ public class Manufacturer {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Manufacturer that = (Manufacturer) o;
+        return Objects.equals(id, that.id)
+                && Objects.equals(name, that.name)
+                && Objects.equals(country, that.country);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, country);
+    }
+
+    @Override
     public String toString() {
         return "Manufacturer{"
-                + "id=" + id + ", name='" + name + '\''
-                + ", country='" + country + '\'' + '}';
+                + "id=" + id
+                + ", name='" + name + '\''
+                + ", country='" + country + '\''
+                + '}';
     }
 }
