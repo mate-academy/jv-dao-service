3f306f2eb31a7cdd4e95e133b58b31e627b9aa4e
Denys <denchik9026@gmail.com>
2023-03-24 15:45:00 +0200

    refactor: replace RuntimeException to NoSuchElementException in service method

--- src/main/java/mate/jdbc/service/impl/DriverServiceImpl.java
+++ src/main/java/mate/jdbc/service/impl/DriverServiceImpl.java
@@ -1,6 +1,7 @@
 package mate.jdbc.service.impl;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import mate.jdbc.dao.DriverDao;
 import mate.jdbc.lib.Inject;
 import mate.jdbc.lib.Service;
@@ -20,7 +21,7 @@ public class DriverServiceImpl implements DriverService {
     @Override
     public Driver get(Long id) {
         return driverDao.get(id).orElseThrow(
-                () -> new RuntimeException("No driver found by id " + id));
+                () -> new NoSuchElementException("No driver found by id " + id));
     }
 
     @Override
