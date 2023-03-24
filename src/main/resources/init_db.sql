fa44aa58e1e91e564ad5805b5ddaa6dfe437da64
Trach Vika <vikaa@Oksanas-MacBook-Pro.local>
2023-03-24 13:44:45 +0200

    hw-jv-dao-service-fixed

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,19 +1,21 @@
 CREATE DATABASE `taxi_db` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 CREATE TABLE `manufacturers` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `country` varchar(255) DEFAULT NULL,
-  `is_deleted` tinyint NOT NULL DEFAULT '0',
+  `id` BIGINT NOT NULL,
+  `name` VARCHAR(255) DEFAULT NULL,
+  `country` VARCHAR(255) DEFAULT NULL,
+  `is_deleted` TINYINT NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
+
 CREATE TABLE `drivers` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) DEFAULT NULL,
   `license_number` varchar(255) DEFAULT NULL,
-  `is_deleted` tinyint NOT NULL DEFAULT '0',
+  `is_deleted` TINYINT NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
 
 
 
