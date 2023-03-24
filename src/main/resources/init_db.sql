4e705e4be66dcf40edb3a68781a63e43cd13915c
Trach Vika <vikaa@Oksanas-MacBook-Pro.local>
2023-03-24 15:13:01 +0200

    hw-jv-dao-service-fixed

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -7,7 +7,6 @@ CREATE TABLE `manufacturers` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
-
 CREATE TABLE `drivers` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) DEFAULT NULL,
@@ -15,7 +14,3 @@ CREATE TABLE `drivers` (
   `is_deleted` TINYINT NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
-
-
-
