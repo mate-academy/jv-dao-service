634a7c37486cfb23e475281f1178598b859ea61c
Your Name <you@example.com>
2023-03-24 16:43:46 +0200

    implement N-tier architecture

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -9,7 +9,7 @@ CREATE TABLE `manufacturers`(
  `is_deleted` TINYINT NOT NULL DEFAULT 0,
  PRIMARY KEY (`id`));
 
-CREATE TABLE `taxi_service_db`.`drivers` (
+CREATE TABLE `drivers` (
  `id` INT NOT NULL AUTO_INCREMENT,
  `name` VARCHAR(255) NULL DEFAULT NULL,
  `license_number` VARCHAR(255) NULL DEFAULT NULL,
