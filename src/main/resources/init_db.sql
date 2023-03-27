715e6990798d626545eac6575eb2b9bf4f3a6e64
Ditkovskyi Pasha <pashkadit@gmail.com>
2023-03-27 13:53:01 +0300

    after mentor review

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,5 +1,5 @@
-CREATE SCHEMA IF NOT EXISTS `taxi_service` DEFAULT CHARACTER SET utf8;
-USE `taxi_service`;
+CREATE SCHEMA IF NOT EXISTS `taxi_service_db` DEFAULT CHARACTER SET utf8;
+USE `taxi_service_db`;
 
 CREATE TABLE `manufacturers` (
                                         `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
@@ -8,7 +8,7 @@ CREATE TABLE `manufacturers` (
                                         `is_deleted` TINYINT NOT NULL DEFAULT 0,
                                         PRIMARY KEY (`id`));
 
-CREATE TABLE `taxi_service_db`.`drivers` (
+CREATE TABLE `drivers` (
                                           `id` BIGINT NOT NULL AUTO_INCREMENT,
                                           `name` VARCHAR(255) NULL,
                                           `license_number` VARCHAR(255) NULL,
