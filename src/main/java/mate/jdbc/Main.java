3cec97798d56e62e21cf27acbf3514e14c371428
alexeyukrainets <qaaq.com.ua@gmail.com>
2023-04-07 15:35:30 +0200

    Version 1

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,29 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.driver.DriverService;
+import mate.jdbc.service.manufacturer.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+    private static final DriverService driverService
+            = (DriverService) injector.getInstance(DriverService.class);
+    public static final ManufacturerService manufacturerService
+            = (ManufacturerService) injector.getInstance(ManufacturerService.class);
+
     public static void main(String[] args) {
-        String i = "0";
+        System.out.println(manufacturerService.getAll());
+        System.out.println(manufacturerService.get(1L));
+        manufacturerService.create(new Manufacturer(null, "Lina", "Ukraine"));
+        manufacturerService.update(new Manufacturer(10L, "Lina", "Ukraine"));
+        manufacturerService.delete(1L);
+
+        driverService.create(new Driver(null, "Oleksii", "12345"));
+        System.out.println(driverService.get(1L));
+        System.out.println(driverService.getAll());
+        driverService.update(new Driver(1L, "Oleksii", "123456"));
+        driverService.delete(1L);
     }
 }
