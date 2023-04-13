5002319ec75a3cc922ee9b01c00cd17ae67aef4c
oleksandr-petrunovskyi <102808605+oleksandr-petrunovskyi@users.noreply.github.com>
2023-04-13 09:43:13 +0300

    ManufacturerService and ManufacturerServiceImpl created

---
+++ src/main/java/mate/jdbc/service/ManufacturerService.java
@@ -0,0 +1,17 @@
+package mate.jdbc.service;
+
+import mate.jdbc.model.Manufacturer;
+import java.util.List;
+import java.util.Optional;
+
+public interface ManufacturerService {
+    Manufacturer create(Manufacturer manufacturer);
+
+    Manufacturer get(Long id);
+
+    List<Manufacturer> getAll();
+
+    Manufacturer update(Manufacturer manufacturer);
+
+    boolean delete(Long id);
+}
