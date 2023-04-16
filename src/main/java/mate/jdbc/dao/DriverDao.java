60f380cc98424d5318620a9b9d4aaa3c98da6e6c
oleksandr-petrunovskyi <102808605+oleksandr-petrunovskyi@users.noreply.github.com>
2023-04-16 13:04:15 +0300

    DriverService tested

--- src/main/java/mate/jdbc/dao/DriverDao.java
+++ src/main/java/mate/jdbc/dao/DriverDao.java
@@ -1,18 +1,17 @@
 package mate.jdbc.dao;
 
-import mate.jdbc.model.Driver;
-import mate.jdbc.model.Manufacturer;
 import java.util.List;
 import java.util.Optional;
+import mate.jdbc.model.Driver;
 
 public interface DriverDao {
-    Manufacturer create(Driver driver);
+    Driver create(Driver driver);
 
     Optional<Driver> get(Long id);
 
     List<Driver> getAll();
 
-    Manufacturer update(Driver driver);
+    Driver update(Driver driver);
 
     boolean delete(Long id);
 }
