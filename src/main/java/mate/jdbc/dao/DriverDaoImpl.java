d19f70ecf0fc915ecbb12691bb0312bb98f6e986
Andrii First <andre_first@ukr.net>
2023-05-02 13:06:02 +0100

    Apply requested changes.

--- src/main/java/mate/jdbc/dao/DriverDaoImpl.java
+++ src/main/java/mate/jdbc/dao/DriverDaoImpl.java
@@ -16,22 +16,22 @@ import mate.jdbc.util.ConnectionUtil;
 @Dao
 public class DriverDaoImpl implements DriverDao {
     @Override
-    public Driver create(Driver manufacturer) {
+    public Driver create(Driver driver) {
         String query = "INSERT INTO drivers (name, license_number) "
                 + "VALUES (?, ?)";
         try (Connection connection = ConnectionUtil.getConnection();
                 PreparedStatement statement
                         = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
-            statement.setString(1, manufacturer.getName());
-            statement.setString(2, manufacturer.getLicenseNumber());
+            statement.setString(1, driver.getName());
+            statement.setString(2, driver.getLicenseNumber());
             statement.executeUpdate();
             ResultSet resultSet = statement.getGeneratedKeys();
             if (resultSet.next()) {
-                manufacturer.setId(resultSet.getObject(1, Long.class));
+                driver.setId(resultSet.getObject(1, Long.class));
             }
-            return manufacturer;
+            return driver;
         } catch (SQLException e) {
-            throw new DataProcessingException("Couldn't create driver. " + manufacturer, e);
+            throw new DataProcessingException("Couldn't create driver. " + driver, e);
         }
     }
 
