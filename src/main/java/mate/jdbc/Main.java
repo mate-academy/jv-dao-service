c6b93254acedadadd81b75ff62c234b1f083b28e
Sergey Zavalniuk <sergzavalnyuk@gmail.com>
2023-05-03 23:53:56 +0300

    Realised: DriverDao; Driver; DriverService; ManufacturerService; etc.

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,54 @@
 package mate.jdbc;
 
+import java.util.ArrayList;
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerService manufacturerService = (ManufacturerService)
+                injector.getInstance(ManufacturerService.class);
+        manufacturerList().forEach(manufacturerService::create);
+        manufacturerService.update(new Manufacturer(1L,"Ford", "USA"));
+        manufacturerService.getAll().forEach(System.out::println);
+        System.out.println(manufacturerService.delete(1L));
+        System.out.println(manufacturerService.get(2L));
+
+        DriverService driverService = (DriverService)
+                injector.getInstance(DriverService.class);
+        driverList().forEach(driverService::create);
+        driverService.update(new Driver(1L,"Kate", "B10"));
+        driverService.getAll().forEach(System.out::println);
+        System.out.println(driverService.delete(1L));
+        System.out.println(driverService.get(5L));
+    }
+
+    private static List<Driver> driverList() {
+        List<Driver> drivers = new ArrayList<>();
+        drivers.add(new Driver("Maxim", "A1"));
+        drivers.add(new Driver("Jon", "A2"));
+        drivers.add(new Driver("Bob", "A3"));
+        drivers.add(new Driver("Karl", "A4"));
+        drivers.add(new Driver("Mark", "B1"));
+        drivers.add(new Driver("Bill", "B2"));
+        drivers.add(new Driver("Eva", "B3"));
+        return drivers;
+    }
+
+    private static List<Manufacturer> manufacturerList() {
+        List<Manufacturer> manufacturers = new ArrayList<>();
+        manufacturers.add(new Manufacturer("Fo", "USA"));
+        manufacturers.add(new Manufacturer("Chevrolet", "USA"));
+        manufacturers.add(new Manufacturer("Porsche", "Germany"));
+        manufacturers.add(new Manufacturer("BMW", "Germany"));
+        manufacturers.add(new Manufacturer("Subaru", "Japan"));
+        manufacturers.add(new Manufacturer("Toyota", "Japan"));
+        return manufacturers;
     }
 }
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,9 +1,18 @@
-CREATE SCHEMA IF NOT EXISTS `taxi_service` DEFAULT CHARACTER SET utf8;
-USE `taxi_service`;
+CREATE DATABASE `taxiservice_db` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+CREATE TABLE `drivers` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb3_general_mysql500_ci DEFAULT NULL,
+  `licenseNumber` varchar(255) COLLATE utf8mb3_general_mysql500_ci DEFAULT NULL,
+  `is_deleted` tinyint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_mysql500_ci;
 
 CREATE TABLE `manufacturers` (
-                                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                        `name` VARCHAR(225) NOT NULL,
-                                        `country` VARCHAR(225) NOT NULL,
-                                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                        PRIMARY KEY (`id`));
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `country` varchar(255) NOT NULL,
+  `is_deleted` tinyint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3;
+
