967212fe6f25ebf1a06accbfeb2700e1b382a15f
AndriiBohun <poikka69@gmail.com>
2023-05-17 13:41:20 +0300

    jv-dao-service_C003

--- src/main/java/mate/jdbc/service/DriverServiceImpl.java
+++ src/main/java/mate/jdbc/service/DriverServiceImpl.java
@@ -1,7 +1,6 @@
 package mate.jdbc.service;
 
 import java.util.List;
-import java.util.Optional;
 import mate.jdbc.dao.DriverDao;
 import mate.jdbc.lib.Inject;
 import mate.jdbc.lib.Service;
@@ -19,8 +18,9 @@ public class DriverServiceImpl implements DriverService {
     }
 
     @Override
-    public Optional<Driver> get(Long id) {
-        return driverDao.get(id);
+    public Driver get(Long id) {
+        return driverDao.get(id)
+                .orElseThrow(() -> new RuntimeException("No driver with this id"));
     }
 
     @Override
