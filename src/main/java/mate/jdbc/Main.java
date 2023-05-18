6d485d090f8d04e9b336b648684536b7be63e11a
Stas Zlishchev <zlishchev@gmail.com>
2023-05-18 09:57:10 +0300

    first version

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,30 @@
 package mate.jdbc;
 
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriveService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        DriveService driveService = (DriveService) injector.getInstance(DriveService.class);
+        printAll(driveService);
+        Driver petro = new Driver("Petro", "68724");
+        driveService.create(petro);
+        printAll(driveService);
+        System.out.println("Driver with id 2 :" + driveService.get(2L));
+        Driver newDriver = new Driver(1L, "Ivan", "87986");
+        driveService.update(newDriver);
+        printAll(driveService);
+        driveService.delete(3L);
+        printAll(driveService);
+    }
+
+    private static void printAll(DriveService driveService) {
+        System.out.println("All drivers:");
+        List<Driver> drivers = driveService.getAll();
+        drivers.forEach(System.out::println);
     }
 }
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -7,3 +7,4 @@ CREATE TABLE `manufacturers` (
                                         `country` VARCHAR(225) NOT NULL,
                                         `is_deleted` TINYINT NOT NULL DEFAULT 0,
                                         PRIMARY KEY (`id`));
+
