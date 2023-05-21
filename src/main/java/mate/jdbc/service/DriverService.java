f4dc6c18919d26c609a8a06e52714bf500a69dfa
Nikita Ostrenskyi <nikostrik2003@gmail.com>
2023-05-21 19:05:28 +0300

    created dao and service for driver with implementations

---
+++ src/main/java/mate/jdbc/service/DriverService.java
@@ -0,0 +1,18 @@
+package mate.jdbc.service;
+
+import mate.jdbc.model.Driver;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DriverService {
+    Driver create(Driver driver);
+
+    Optional<Driver> get(Long id);
+
+    List<Driver> getAll();
+
+    Driver update(Driver driver);
+
+    boolean delete(Long id);
+}
