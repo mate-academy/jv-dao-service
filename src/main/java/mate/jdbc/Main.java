00de7c605d26e9cccd4bc5f797b08a78fdbc3d97
Nikita <katkovnikitka@gmail.com>
2023-05-29 13:33:46 +0200

    update one more time

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -12,6 +12,7 @@ public class Main {
     public static void main(String[] args) {
         ManufacturerService manufacturerService
                 = (ManufacturerService) injector.getInstance(ManufacturerService.class);
+
         Manufacturer manufacturer = new Manufacturer("Audi","Germany");
         manufacturerService.create(manufacturer);
         manufacturer.setName("Skoda");
