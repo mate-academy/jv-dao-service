55ea223734509d66d56e31625d1e75974d41858b
V-Solianyk <112614843+V-Solianyk@users.noreply.github.com>
2023-06-05 18:13:25 +0300

    created a new driver model and also divided the entire program into 3 layers, tested the work in the main.

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,46 @@
 package mate.jdbc;
 
+import java.util.List;
+import java.util.Optional;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerService manufacturerService = (ManufacturerService) injector
+                .getInstance(ManufacturerService.class);
+        Manufacturer manufacturerBmw = new Manufacturer("BMW", "Germany");
+        manufacturerService.create(manufacturerBmw);
+
+        Optional<Manufacturer> manufacturer = manufacturerService.get(7L);
+        manufacturer.ifPresent(System.out::println);
+
+        List<Manufacturer> allManufacturers = manufacturerService.getAll();
+        allManufacturers.forEach(System.out::println);
+
+        Manufacturer manufacturerZazUpdate = new Manufacturer(8L, "Lanos", "Ukraine");
+        System.out.println(manufacturerService.update(manufacturerZazUpdate));
+
+        System.out.println(manufacturerService.delete(11L));
+
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        Driver driverVitaliy = new Driver("Vitaliy", "0122567");
+        driverService.create(driverVitaliy);
+
+        Optional<Driver> driver = driverService.get(2L);
+        driver.ifPresent(System.out::println);
+
+        List<Driver> allDrivers = driverService.getAll();
+        allDrivers.forEach(System.out::println);
+
+        Driver updateDriverBob = new Driver(1L, "Bob", "1234567");
+        System.out.println(driverService.update(updateDriverBob));
+
+        System.out.println(driverService.delete(2L));
     }
 }
