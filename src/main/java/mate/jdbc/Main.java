713a79459605452a21e31cddff815c0e6e66a20a
Tim <tymurprokhorov@gmail.com>
2023-06-06 19:08:48 +0300

    Manufacturer entity is a part of 3-tier arc.pattern as well as Driver, all logic is called from service

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,25 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriverService;
+
 public class Main {
+    private static Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        DriverService driverService =
+                (DriverService) injector.getInstance(DriverService.class);
+        Driver vitya = new Driver();
+        vitya.setName("Vitya");
+        vitya.setLicenseNumber("2121");
+
+        driverService.create(vitya);
+        vitya.setName("Viktor");
+        vitya.setLicenseNumber("9090");
+        driverService.update(vitya);
+        System.out.println(driverService.get(vitya.getId()));
+        driverService.getAll().forEach(System.out::println);
+        System.out.println("deletion successful: " + driverService.delete(vitya.getId()));
     }
 }
