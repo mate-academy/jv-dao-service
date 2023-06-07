4b4753183f6ebf7ebdfd85a01632c8640922fc41
V-Solianyk <112614843+V-Solianyk@users.noreply.github.com>
2023-06-07 14:50:39 +0300

    removed the Optional return from service methods and refactored the Main method

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,6 @@
 package mate.jdbc;
 
 import java.util.List;
-import java.util.Optional;
 import mate.jdbc.lib.Injector;
 import mate.jdbc.model.Driver;
 import mate.jdbc.model.Manufacturer;
@@ -15,32 +14,31 @@ public class Main {
         ManufacturerService manufacturerService = (ManufacturerService) injector
                 .getInstance(ManufacturerService.class);
         Manufacturer manufacturerBmw = new Manufacturer("BMW", "Germany");
-        manufacturerService.create(manufacturerBmw);
+        Manufacturer manufacturer = manufacturerService.create(manufacturerBmw);
 
-        Optional<Manufacturer> manufacturer = manufacturerService.get(7L);
-        manufacturer.ifPresent(System.out::println);
+        System.out.println(manufacturerService.get(manufacturerBmw.getId()));
 
         List<Manufacturer> allManufacturers = manufacturerService.getAll();
         allManufacturers.forEach(System.out::println);
 
-        Manufacturer manufacturerZazUpdate = new Manufacturer(8L, "Lanos", "Ukraine");
+        Manufacturer manufacturerZazUpdate = new Manufacturer(manufacturer.getId(),
+                "Lanos", "Ukraine");
         System.out.println(manufacturerService.update(manufacturerZazUpdate));
 
-        System.out.println(manufacturerService.delete(11L));
+        System.out.println(manufacturerService.delete(manufacturerBmw.getId()));
 
         DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
         Driver driverVitaliy = new Driver("Vitaliy", "0122567");
-        driverService.create(driverVitaliy);
+        Driver driver = driverService.create(driverVitaliy);
 
-        Optional<Driver> driver = driverService.get(2L);
-        driver.ifPresent(System.out::println);
+        System.out.println(driverService.get(driverVitaliy.getId()));
 
         List<Driver> allDrivers = driverService.getAll();
         allDrivers.forEach(System.out::println);
 
-        Driver updateDriverBob = new Driver(1L, "Bob", "1234567");
+        Driver updateDriverBob = new Driver(driver.getId(), "Bob", "1234567");
         System.out.println(driverService.update(updateDriverBob));
 
-        System.out.println(driverService.delete(2L));
+        System.out.println(driverService.delete(driverVitaliy.getId()));
     }
 }
