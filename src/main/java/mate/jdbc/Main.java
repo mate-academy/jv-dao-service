3a0d1296acdead34fb80889abed38d142df67ef3
Mykyta Naumov <nneed4use@gmail.com>
2023-06-11 20:09:34 +0300

    method main() works, checkstyle done

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,47 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerService manufacturerService
+                = (ManufacturerService) injector.getInstance(ManufacturerService.class);
+
+        System.out.println(manufacturerService.getAll());
+
+        Manufacturer manufacturer = new Manufacturer("Chevrolet", "USA");
+        System.out.println(manufacturerService.create(manufacturer));
+
+        manufacturer.setCountry("DAEWOO-ZAZ");
+        manufacturer.setName("Ukraine");
+        System.out.println(manufacturerService.update(manufacturer));
+
+        System.out.println(manufacturerService.delete(6L));
+        System.out.println(manufacturerService.get(1L));
+
+        System.out.println(manufacturerService.getAll());
+
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+
+        System.out.println(driverService.getAll());
+
+        Driver driver = new Driver("Sofia", "4");
+        driverService.create(driver);
+
+        driver.setName("Alice");
+        driver.setLicenseNumber("12");
+        driverService.update(driver);
+
+        driverService.delete(5L);
+        driverService.get(6L);
+
+        System.out.println(driverService.getAll());
+
     }
 }
