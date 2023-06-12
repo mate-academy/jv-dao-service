56de009619b011d095a65d2f3ba3c656efcffd21
DVArt <dvart@hotmail.com>
2023-06-12 16:35:21 +0300

    Create Dao_vs_Service

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,41 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.impl.DriverService;
+import mate.jdbc.service.impl.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        driverServise(driverService);
+        ManufacturerService manufacturerService =
+                (ManufacturerService) injector.getInstance(ManufacturerService.class);
+        manufacturerService(manufacturerService);
+    }
+
+    private static void manufacturerService(ManufacturerService manufacturerService) {
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturer.setName("Daewo");
+        manufacturer.setCountry("Sweden");
+        System.out.println(manufacturerService.create(manufacturer));
+        System.out.println(manufacturerService.get(1L));
+        System.out.println(manufacturerService.delete(2L));
+        System.out.println(manufacturerService.update(manufacturer));
+        manufacturerService.getAll().forEach(System.out::println);
+    }
+
+    private static void driverServise(DriverService driverService) {
+        Driver driver = new Driver();
+        driver.setName("Arthur");
+        driver.setLicenseNumber("666");
+        System.out.println(driverService.create(driver));
+        System.out.println(driverService.get(1L));
+        System.out.println(driverService.delete(2L));
+        System.out.println(driverService.update(driver));
+        driverService.getAll().forEach(System.out::println);
     }
 }
