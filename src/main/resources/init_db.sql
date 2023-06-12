eb3c6640014f9fb8a70901a46147faf1238924f1
Vladyslav <vladkrivec3@gmail.com>
2023-06-12 14:43:54 +0300

    dao service done

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,9 +1,17 @@
-CREATE SCHEMA IF NOT EXISTS `taxi_service` DEFAULT CHARACTER SET utf8;
-USE `taxi_service`;
+CREATE DATABASE `db_taxi_service` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 CREATE TABLE `manufacturers` (
-                                        `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-                                        `name` VARCHAR(225) NOT NULL,
-                                        `country` VARCHAR(225) NOT NULL,
-                                        `is_deleted` TINYINT NOT NULL DEFAULT 0,
-                                        PRIMARY KEY (`id`));
+                                 `id` BIGINT NOT NULL AUTO_INCREMENT,
+                                 `name` VARCHAR(45) DEFAULT NULL,
+                                 `country` VARCHAR(45) DEFAULT NULL,
+                                 `is_deleted` TINYINT DEFAULT '0',
+                                 PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `drivers` (
+                           `id` BIGINT NOT NULL AUTO_INCREMENT,
+                           `name` VARCHAR(45) DEFAULT NULL,
+                           `license_number` VARCHAR(45) DEFAULT NULL,
+                           `is_deleted` TINYINT NOT NULL DEFAULT '0',
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
