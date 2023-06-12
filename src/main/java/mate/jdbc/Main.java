f06d46723da9ba5e60577eaa1592465f43ed2e49
ViktoriiaSemenchenko <vickysemenchenko@gmail.com>
2023-06-12 11:54:24 +0300

    done dao-service task

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,45 @@
 package mate.jdbc;
 
+import java.util.List;
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector
+            .getInstance("mate.jdbc");
+    private static final ManufacturerService manufacturerService = (ManufacturerService) injector
+            .getInstance(ManufacturerService.class);
+    public static final DriverService driverService = (DriverService) injector
+            .getInstance(DriverService.class);
+
     public static void main(String[] args) {
-        // test your code here
+        Manufacturer toyotaManufacturer = new Manufacturer("Toyota", "Japan");
+        manufacturerService.create(toyotaManufacturer);
+
+        System.out.println(manufacturerService
+                .get(toyotaManufacturer.getId()));
+
+        List<Manufacturer> manufacturers = manufacturerService.getAll();
+        manufacturers.forEach(System.out::println);
+
+        toyotaManufacturer.setCountry("USA");
+        manufacturerService.update(toyotaManufacturer);
+        boolean isDeleted = manufacturerService.delete(toyotaManufacturer.getId());
+        System.out.println(isDeleted);
+
+        Driver driver = new Driver("Ivan", "92748dnsy37");
+        driverService.create(driver);
+
+        List<Driver> drivers = driverService.getAll();
+        drivers.forEach(System.out::println);
+
+        driver.setName("Semen");
+        driverService.update(driver);
+        System.out.println(driverService.get(driver.getId()));
+        boolean isDeletedDriver = driverService.delete(driver.getId());
+        System.out.println(isDeletedDriver);
     }
 }
