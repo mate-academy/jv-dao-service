fd51c064225f1cdb06105c402412a5cf10de7012
Володимир <vova.malyarchuk2@gmail.com>
2023-06-12 16:09:26 +0300

    Implemented task daoHw-3

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,38 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.serviceimpl.DriverService;
+import mate.jdbc.serviceimpl.ManufacturerService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        ManufacturerService manufacturerService
+                = (ManufacturerService) injector.getInstance(ManufacturerService.class);
+
+        Manufacturer manufacturer = new Manufacturer("Lanos","Ukraine");
+        manufacturerService.create(manufacturer);
+        manufacturer.setName("Mitsubishi");
+        manufacturer.setCountry("Japan");
+        manufacturerService.update(manufacturer);
+        manufacturerService.get(1L);
+        manufacturerService.delete(2L);
+
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        Driver driver = new Driver();
+        driver.setName("Petro");
+        driver.setLicenseNumber("123");
+        driverService.create(driver);
+        driver.setName("Stefania");
+        driver.setLicenseNumber("321");
+        driverService.update(driver);
+        driverService.get(2L);
+        driverService.delete(1L);
+
+        System.out.println(driverService.getAll());
+        System.out.println(manufacturerService.getAll());
     }
 }
