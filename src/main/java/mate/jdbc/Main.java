63d1c300b63e73dabb67d3551a573599978da114
Petro Kobevko <kobevkopi@gmail.com>
2023-06-12 11:15:16 +0200

    Added hw solution

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,25 @@
 package mate.jdbc;
 
+import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriverService;
+
 public class Main {
+    private static Injector injector = Injector.getInstance("mate.jdbc");
+
     public static void main(String[] args) {
-        // test your code here
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+        Driver driver = new Driver();
+        driver.setName("Petro Poroshenko");
+        driver.setLicenseNumber("7777777");
+        driverService.create(driver);
+        Driver driver1 = driverService.get(1L).get();
+        System.out.println(driver1);
+        driverService.getAll().forEach(System.out::println);
+        driver.setName("Petro Poroshenko Chocolate King");
+        driverService.update(driver);
+        driverService.getAll().forEach(System.out::println);
+        driverService.delete(1L);
+        driverService.getAll().forEach(System.out::println);
     }
 }
