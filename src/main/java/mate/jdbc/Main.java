accdffbdc96cf9da6dfe6c61788a68a9b70d4dc0
Pasha <pavlokrasota@gmail.com>
2023-06-12 10:40:21 +0300

    Update solution

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -7,16 +7,15 @@ import mate.jdbc.service.DriverService;
 import mate.jdbc.service.ManufacturerService;
 
 public class Main {
-
     private static final Injector injector = Injector.getInstance("mate.jdbc");
 
     public static void main(String[] args) {
         DriverService driverService =
                 (DriverService) injector.getInstance(DriverService.class);
         Driver driver = new Driver("Alex", "51252");
-        System.out.println(driverService.create(driver));
-        System.out.println(driverService.get(1L));
-        System.out.println(driverService.delete(2L));
+        Driver driverId = driverService.create(driver);
+        System.out.println(driverService.get(driverId.getId()));
+        System.out.println(driverService.delete(driverId.getId()));
         System.out.println(driverService.update(driver));
         System.out.println(driverService.getAll());
         driverService.getAll().forEach(System.out::println);
@@ -24,9 +23,9 @@ public class Main {
         ManufacturerService manufacturerService =
                 (ManufacturerService) injector.getInstance(ManufacturerService.class);
         Manufacturer manufacturer = new Manufacturer("Toyota", "Japan");
-        System.out.println(manufacturerService.create(manufacturer));
-        System.out.println(manufacturerService.get(1L));
-        System.out.println(manufacturerService.delete(2L));
+        Manufacturer manufacturerId = manufacturerService.create(manufacturer);
+        System.out.println(manufacturerService.get(manufacturerId.getId()));
+        System.out.println(manufacturerService.delete(manufacturerId.getId()));
         System.out.println(manufacturerService.update(manufacturer));
         System.out.println(manufacturerService.getAll());
         driverService.getAll().forEach(System.out::println);
