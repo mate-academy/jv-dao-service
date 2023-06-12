9d3b8f1461a53fa77664433272400d64336b4a95
Yaroslav Khmaruk <yarik9656422@gmail.com>
2023-06-12 10:22:15 +0300

    add fixes after student review

--- src/main/java/mate/jdbc/service/impl/DriverServiceImpl.java
+++ src/main/java/mate/jdbc/service/impl/DriverServiceImpl.java
@@ -1,6 +1,7 @@
 package mate.jdbc.service.impl;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import mate.jdbc.dao.DriverDao;
 import mate.jdbc.lib.Inject;
 import mate.jdbc.lib.Service;
@@ -19,7 +20,9 @@ public class DriverServiceImpl implements DriverService {
 
     @Override
     public Driver get(Long id) {
-        return driverDao.get(id).get();
+        return driverDao.get(id).orElseThrow(
+                () -> new NoSuchElementException("Couldn't get driver by id:" + id)
+        );
     }
 
     @Override
