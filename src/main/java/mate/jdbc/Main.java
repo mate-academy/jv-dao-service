38f7f82084f179b5cb42c04a3e51c5b7c94376ef
Igor Maksimov <maksigors@gmail.com>
2023-06-13 12:18:30 +0300

    build 1.1 after std rev

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,9 @@
 package mate.jdbc;
 
 import mate.jdbc.lib.Injector;
+import mate.jdbc.model.Driver;
 import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
 import mate.jdbc.service.ManufacturerService;
 
 public class Main {
@@ -9,7 +11,25 @@ public class Main {
         Injector injector = Injector.getInstance("mate.jdbc");
         ManufacturerService manufacturerService =
                 (ManufacturerService) injector.getInstance(ManufacturerService.class);
+
         Manufacturer porshe = new Manufacturer("Porshe", "Italy");
-        manufacturerService.create(porshe);
+        Manufacturer ford = new Manufacturer("Ford", "USA");
+        System.out.println(manufacturerService.create(porshe));
+        System.out.println(manufacturerService.create(ford));
+        System.out.println(manufacturerService.create(porshe));
+        System.out.println(manufacturerService.get(porshe.getId()));
+        System.out.println(manufacturerService.delete(porshe.getId()));
+        System.out.println(manufacturerService.update(new Manufacturer(2L,"Toyota","Korea")));
+
+        DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+
+        Driver dilan = new Driver("Dilan", "DL00923");
+        Driver alice = new Driver("Alice", "DL00211");
+        System.out.println(driverService.create(dilan));
+        System.out.println(driverService.create(alice));
+        System.out.println(driverService.get(dilan.getId()));
+        System.out.println(driverService.delete(alice.getId()));
+        System.out.println(driverService.update(new Driver(2L,"Billy","DL00001")));
+        driverService.getAll().forEach(System.out::println);
     }
 }
