55d04cc4a8c2bcda63dd12b68aea691be1c31445
Taras Machyta <taras.machyta.sa@gmail.com>
2023-06-14 21:42:35 +0300

    Updates

--- src/main/java/mate/jdbc/service/ManufacturerServiceImpl.java
+++ src/main/java/mate/jdbc/service/ManufacturerServiceImpl.java
@@ -1,7 +1,7 @@
 package mate.jdbc.service;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.NoSuchElementException;
 import mate.jdbc.dao.ManufacturerDao;
 import mate.jdbc.lib.Inject;
 import mate.jdbc.lib.Service;
@@ -18,8 +18,10 @@ public class ManufacturerServiceImpl implements ManufacturerService {
     }
 
     @Override
-    public Optional<Manufacturer> get(Long id) {
-        return manufacturerDao.get(id);
+    public Manufacturer get(Long id) {
+        return manufacturerDao.get(id)
+                .orElseThrow(() -> new NoSuchElementException(
+                        "Can't find manufacturer by id: " + id));
     }
 
     @Override
