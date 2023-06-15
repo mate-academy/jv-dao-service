38c22096ab91cab7cdeb12349de41f88c1b53c5e
Volodymyr Davidov <volodymyrdavidov1@gmail.com>
2023-06-15 17:01:59 +0300

    hw-jv-dao-service

--- src/main/java/mate/jdbc/model/Driver.java
+++ src/main/java/mate/jdbc/model/Driver.java
@@ -52,7 +52,7 @@ public class Driver {
     @Override
     public boolean equals(Object o) {
         if (this == o) {
-            return true ;
+            return true;
         }
         if (o == null || getClass() != o.getClass()) {
             return false;
