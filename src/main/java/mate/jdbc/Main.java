862a24734393de402691316be5f05982ab677f90
Vershynin Kostia <bigvini@gmail.com>
2023-07-06 23:24:41 +0300

    realization jv-jbdc homwork

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,29 @@
 package mate.jdbc;
 
+import mate.jdbc.dao.DriverDao;
+import mate.jdbc.dao.impl.DriverDaoImpl;
+import mate.jdbc.model.Driver;
+import mate.jdbc.model.Manufacturer;
+import mate.jdbc.service.DriverService;
+import mate.jdbc.service.impl.DriverServiceImpl;
+import mate.jdbc.util.ConnectionUtil;
+
+import java.awt.print.Printable;
+
 public class Main {
     public static void main(String[] args) {
-        // test your code here
+        Driver driver = new Driver();
+        driver.setId(9L);
+        driver.setLicenseNumber("854565");
+        driver.setName("B5ob");
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturer.setCountry("Ukraine");
+        manufacturer.setName("Zaz");
+        DriverService driverService = new DriverServiceImpl();
+        driverService.create(driver);
+        driverService.update(driver);
+        driverService.getAll().forEach(System.out ::println);
+
+
     }
 }
--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -1,15 +1,17 @@
 package mate.jdbc.util;
 
+import mate.jdbc.exception.DataProcessingException;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL = "YOUR DATABASE URL";
-    private static final String USERNAME = "YOUR USERNAME";
-    private static final String PASSWORD = "YOUR PASSWORD";
-    private static final String JDBC_DRIVER = "YOUR DRIVER";
+    private static final String URL = "taxi_service";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "1984";
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 
     static {
         try {
@@ -19,10 +21,14 @@ public class ConnectionUtil {
         }
     }
 
-    public static Connection getConnection() throws SQLException {
-        Properties dbProperties = new Properties();
-        dbProperties.setProperty("user", USERNAME);
-        dbProperties.setProperty("password", PASSWORD);
-        return DriverManager.getConnection(URL, dbProperties);
+    public static Connection getConnection() {
+        try {
+            Properties dbProperties = new Properties();
+            dbProperties.setProperty("user", USERNAME);
+            dbProperties.setProperty("password", PASSWORD);
+           return DriverManager.getConnection("jdbc:mysql://localhost:3306/taxi_service",dbProperties);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't create connection to Db: taxi_service ",e);
+        }
     }
 }
