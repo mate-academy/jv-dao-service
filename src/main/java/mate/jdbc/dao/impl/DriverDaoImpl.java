94df4c9f808e52e1c3a2fba1f1759c6a875eff4a
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-07-14 00:33:23 +0300

    added files

---
+++ src/main/java/mate/jdbc/dao/impl/DriverDaoImpl.java
@@ -0,0 +1,33 @@
+package mate.jdbc.dao.impl;
+
+import mate.jdbc.dao.DriverDao;
+import mate.jdbc.model.Driver;
+
+import java.util.List;
+
+public class DriverDaoImpl implements DriverDao {
+    @Override
+    public Driver create(Driver driver) {
+        return null;
+    }
+
+    @Override
+    public Driver get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Driver> getAll() {
+        return null;
+    }
+
+    @Override
+    public Driver update(Driver driver) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+}
---
+++ src/main/java/mate/jdbc/service/impl/DriverServiceImpl.java
@@ -0,0 +1,36 @@
+package mate.jdbc.service.impl;
+
+import mate.jdbc.lib.Service;
+import mate.jdbc.model.Driver;
+import mate.jdbc.service.DriverService;
+
+import java.util.List;
+
+@Service
+public class DriverServiceImpl implements DriverService {
+
+    @Override
+    public Driver create(Driver driver) {
+        return null;
+    }
+
+    @Override
+    public Driver get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Driver> getAll() {
+        return null;
+    }
+
+    @Override
+    public Driver update(Driver driver) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+}
