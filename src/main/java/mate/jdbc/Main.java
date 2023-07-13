d265db4bd1f4c4ed815c9a8b99e5e929869c6c96
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-07-14 00:23:01 +0300

    added connection settings, driver model and prepared dao and service for it

--- src/main/java/mate/jdbc/Main.java
+++ src/main/java/mate/jdbc/Main.java
@@ -1,7 +1,13 @@
 package mate.jdbc;
 
+import mate.jdbc.dao.ManufacturerDao;
+import mate.jdbc.dao.impl.ManufacturerDaoImpl;
+import mate.jdbc.model.Manufacturer;
+
 public class Main {
     public static void main(String[] args) {
-        // test your code here
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturer.setCountry("ukraine");
+        new ManufacturerDaoImpl().create(manufacturer);
     }
 }
