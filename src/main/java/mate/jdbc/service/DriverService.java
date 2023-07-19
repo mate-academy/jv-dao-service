4ed6a15464ab10ba282a60eb682c748a5bc804b1
Artem <artempharm83@gmail.com>
2023-07-19 21:16:01 +0300

    Create class Driver, create interface ManufacturerService and all CRUD methods ,create interface DriverService  and all CRUD methods

---
+++ src/main/java/mate/jdbc/service/DriverService.java
@@ -0,0 +1,17 @@
+package mate.jdbc.service;
+
+import java.util.List;
+import mate.jdbc.model.Driver;
+
+public interface DriverService {
+    List<Driver> getAll();
+
+    Driver get(Long id);
+
+    Driver create(Driver driver);
+
+    boolean delete(Long id);
+
+    Driver update(Driver driver);
+
+}
