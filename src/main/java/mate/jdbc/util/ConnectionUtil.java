91b8df2e6169f3c60588cc699dd60bc1fc284513
Stanislav Hrynus <stanislav.hrynus@gmail.com>
2023-07-23 00:11:22 +0300

    implemented singleton pattern, added all necessary annotation and realized n-tier architecture

--- src/main/java/mate/jdbc/util/ConnectionUtil.java
+++ src/main/java/mate/jdbc/util/ConnectionUtil.java
@@ -4,25 +4,30 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
+import mate.jdbc.exception.DataProcessingException;
 
 public class ConnectionUtil {
-    private static final String URL = "YOUR DATABASE URL";
-    private static final String USERNAME = "YOUR USERNAME";
-    private static final String PASSWORD = "YOUR PASSWORD";
-    private static final String JDBC_DRIVER = "YOUR DRIVER";
+    private static final String URL = "jdbc:mysql://localhost:3306/library_db";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "stas-4ydik*1992";
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 
     static {
         try {
             Class.forName(JDBC_DRIVER);
         } catch (ClassNotFoundException e) {
-            throw new RuntimeException("Can't find SQL Driver", e);
+            throw new DataProcessingException("Can't find SQL Driver", e);
         }
     }
 
-    public static Connection getConnection() throws SQLException {
+    public static Connection getConnection() {
         Properties dbProperties = new Properties();
         dbProperties.setProperty("user", USERNAME);
         dbProperties.setProperty("password", PASSWORD);
-        return DriverManager.getConnection(URL, dbProperties);
+        try {
+            return DriverManager.getConnection(URL, dbProperties);
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can't create connection to DB", e);
+        }
     }
 }
